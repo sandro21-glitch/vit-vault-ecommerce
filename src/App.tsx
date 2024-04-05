75b5a04e8a61899e27507682d4c1ac32a8a2077b
Sandro <vipsandro11@gmail.com>
2024-04-05 20:57:23 +0400

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./ui/Header";
+import MainPage from "./pages/MainPage";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
