d18aa74fd78eaef348beb0f97188997e271395f6
Sandro <vipsandro11@gmail.com>
2024-04-06 22:13:02 +0400

    update SingleNavItem.tsx

--- src/features/header/navbar/SingleNavItem.tsx
+++ src/features/header/navbar/SingleNavItem.tsx
@@ -4,7 +4,7 @@ type SingleNavItemTypes = {
 };
 const SingleNavItem = ({ icon, name }: SingleNavItemTypes) => {
   return (
-    <li className="flex items-center gap-1 cursor-pointer text-[14px] font-semibold">
+    <li className="flex items-center gap-1 cursor-pointer text-[14px] font-semibold hover:text-primaryGreen transition-colors ease-in duration-150">
       <img src={icon} alt={name} className="w-[24px] h-[24px]" />
       <span>{name}</span>
     </li>
