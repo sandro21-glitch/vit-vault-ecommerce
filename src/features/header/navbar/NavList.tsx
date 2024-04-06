7b6c429abe62d03decfba345268562b112b2e18a
Sandro <vipsandro11@gmail.com>
2024-04-06 20:25:16 +0400

    feat: create NavList.tsx

---
+++ src/features/header/navbar/NavList.tsx
@@ -0,0 +1,16 @@
+import { navItems } from "../../../utils/navItems";
+import SingleNavItem from "./SingleNavItem";
+
+const NavList = () => {
+  return (
+    <ul className="flex items-center gap-5 w-full">
+      {navItems.map((item) => {
+        return (
+          <SingleNavItem key={item.id} name={item.name} icon={item.icon} />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default NavList;
