0e45368c34e7b16272cb72187468bd772f9ee1ee
Sandro <vipsandro11@gmail.com>
2024-04-06 19:23:07 +0400

    feat: create SearchProduct.tsx

---
+++ src/features/header/general/SearchProduct.tsx
@@ -0,0 +1,26 @@
+
+import { CiSearch } from "react-icons/ci";
+
+const SearchProduct = () => {
+  return (
+    <li className="flex-1">
+    <form className="relative">
+      <input
+        type="text"
+        name="search"
+        id="search"
+        placeholder="მოძებნე პროდუქცია"
+        className="min-w-[290px] w-full border-2 outline-none p-5 h-[46px]"
+      />
+      <button
+        type="submit"
+        className="absolute right-0 bottom-0 top-0 w-[50px] bg-transparent text-[#777] flex-center"
+      >
+        <CiSearch size={25} />
+      </button>
+    </form>
+  </li>
+  )
+}
+
+export default SearchProduct
