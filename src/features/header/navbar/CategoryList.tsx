c3a8f2dc814bbdf1e96b9cb97e54a563c2ddd8b0
Sandro <vipsandro11@gmail.com>
2024-04-06 22:01:40 +0400

    feat: create CategoryList.tsx

---
+++ src/features/header/navbar/CategoryList.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import { IoIosArrowForward } from "react-icons/io";
+const CategoryList = () => {
+  return (
+    <div className="absolute left-0 top-full w-full">
+      <ul className="border border-t-transparent">
+        <li className="border border-b text-[13px] cursor-pointer font-medium flex justify-between items-center px-3 py-1">
+          <Link to="">საკვები დანამატები</Link>
+          <IoIosArrowForward />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default CategoryList;
