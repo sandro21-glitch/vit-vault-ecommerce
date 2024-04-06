4274fe7cd197952ae9ed2bf56d7cdd1bb504f21c
Sandro <vipsandro11@gmail.com>
2024-04-06 22:01:32 +0400

    update MainNavbar.tsx

--- src/features/header/MainNavbar.tsx
+++ src/features/header/MainNavbar.tsx
@@ -1,9 +1,11 @@
+import Categories from "./navbar/Categories";
 import NavList from "./navbar/NavList";
 
 const MainNavbar = () => {
   return (
     <nav className="border border-b-gray-200 h-[52px]">
       <div className="section-center section-x flex items-center h-full">
+        <Categories />
         <NavList />
       </div>
     </nav>
