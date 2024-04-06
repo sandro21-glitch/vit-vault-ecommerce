9f94cd54391fb900a3c7da880fa1c74a493d8f97
Sandro <vipsandro11@gmail.com>
2024-04-06 20:25:11 +0400

    feat: create MainNavbar.tsx

---
+++ src/features/header/MainNavbar.tsx
@@ -0,0 +1,13 @@
+import NavList from "./navbar/NavList";
+
+const MainNavbar = () => {
+  return (
+    <nav className="border border-b-gray-200 h-[52px]">
+      <div className="section-center section-x flex items-center h-full">
+        <NavList />
+      </div>
+    </nav>
+  );
+};
+
+export default MainNavbar;
