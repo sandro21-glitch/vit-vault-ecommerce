1c07c83ea4ff8e262336bcc70a8de6842aebd0b2
Sandro <vipsandro11@gmail.com>
2024-04-07 17:44:14 +0400

    feat: create HomeSlider.tsx

---
+++ src/features/mainPage/HomeSlider.tsx
@@ -0,0 +1,11 @@
+import SliderContent from "./SliderContent";
+
+const HomeSlider = () => {
+  return (
+    <article className="max-w-[900px] float-right w-full  mx-auto select-none">
+      <SliderContent />
+    </article>
+  );
+};
+
+export default HomeSlider;
