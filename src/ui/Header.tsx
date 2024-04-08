8b1c9637e424d8af719c21fb24a50314721b29ca
Sandro <vipsandro11@gmail.com>
2024-04-08 20:18:12 +0400

    update Header.tsx

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from "react";
 import GeneralHeader from "../features/header/GeneralHeader";
 import MainNavbar from "../features/header/MainNavbar";
 import TopBar from "../features/header/TopBar";
 
 const Header = () => {
+  const [isMobileNav, setIsMobileNav] = useState<boolean>(false);
+  const [isOpen, setOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileNav(window.innerWidth < 1280);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <header>
       <TopBar />
-      <GeneralHeader />
-      <MainNavbar />
+      <GeneralHeader
+        isMobileNav={isMobileNav}
+        isOpen={isOpen}
+        setOpen={setOpen}
+      />
+      <MainNavbar isMobileNav={isMobileNav} />
     </header>
   );
 };
