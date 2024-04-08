571aa5737f6514e2f7912d560d713df455a32974
Sandro <vipsandro11@gmail.com>
2024-04-08 20:35:07 +0400

    update MobileNav.tsx

--- src/features/header/navbar/mobile/MobileNav.tsx
+++ src/features/header/navbar/mobile/MobileNav.tsx
@@ -1,9 +1,13 @@
-
+import SearchProduct from "../../general/SearchProduct";
 
 const MobileNav = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="fixed bg-white top-0 left-0 w-[60%] right-0 bottom-0 z-[99999]">
+      <ul className="overflow-hidden">
+        <SearchProduct />
+      </ul>
+    </div>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
