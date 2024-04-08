8d3a14f0c0c9474cd84751926ea6411e8ac81459
Sandro <vipsandro11@gmail.com>
2024-04-08 20:18:02 +0400

    refactor: change folder path

---
+++ src/features/header/navbar/desktop/DesktopNav.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import Categories from "./Categories";
+import NavList from "./NavList";
+
+const DesktopNav = () => {
+  return (
+    <div className="section-center section-x flex items-center h-full">
+      <Categories />
+      <NavList />
+      <li className="list-none cursor-pointer text-[14px] font-semibold text-primaryGreen">
+        <Link to="/">ფასდაკლებები</Link>
+      </li>
+    </div>
+  );
+};
+
+export default DesktopNav;
