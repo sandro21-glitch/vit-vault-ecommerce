0f241233a76fcf5f03119e21f264c41ba6a6916a
Sandro <vipsandro11@gmail.com>
2024-04-08 20:53:08 +0400

    update MobileNav.tsx

--- src/features/header/navbar/mobile/MobileNav.tsx
+++ src/features/header/navbar/mobile/MobileNav.tsx
@@ -1,8 +1,16 @@
 import SearchProduct from "../../general/SearchProduct";
 
-const MobileNav = () => {
+type MobileNavTypes = {
+  isOpen: boolean;
+};
+
+const MobileNav = ({ isOpen }: MobileNavTypes) => {
   return (
-    <div className="fixed bg-white top-0 left-0 w-[60%] right-0 bottom-0 z-[99999]">
+    <div
+      className={`fixed ${
+        isOpen ? "translate-x-0" : "translate-x-[-200%]"
+      } bg-white top-0 left-0 w-[60%] right-0 bottom-0 z-[9999] transition-all ease-in duration-300 delay-100`}
+    >
       <ul className="overflow-hidden">
         <SearchProduct />
       </ul>
