8828c43113234da935d7764dac99513b729cee02
Sandro <vipsandro11@gmail.com>
2024-04-08 20:17:30 +0400

    update GeneralHeader.tsx

--- src/features/header/GeneralHeader.tsx
+++ src/features/header/GeneralHeader.tsx
@@ -1,20 +1,39 @@
+import Hamburger from "hamburger-react";
 import Cart from "./general/Cart";
 import Logo from "./general/Logo";
 import SearchProduct from "./general/SearchProduct";
 import UserAuth from "./general/UserAuth";
-const GeneralHeader = () => {
+
+type GenHeaderTypes = {
+  isMobileNav: boolean;
+  isOpen: boolean;
+  setOpen: (isOpen: boolean) => void;
+};
+
+const GeneralHeader = ({ isMobileNav, isOpen, setOpen }: GenHeaderTypes) => {
+  const handleToggle = () => {
+    setOpen(!isOpen);
+  };
   return (
     <div className="bg-white border border-b-gray-200 font-mono">
       <div className="section-center section-x">
         <ul className="h-[105px] flex items-center justify-between gap-5">
-          <Logo />
-          <SearchProduct />
-          <UserAuth />
+          {isMobileNav ? (
+            <>
+              <Hamburger size={20} toggled={isOpen} toggle={handleToggle} />
+              <Logo />
+            </>
+          ) : (
+            <>
+              <Logo />
+              <SearchProduct />
+              <UserAuth />
+            </>
+          )}
           <Cart />
         </ul>
       </div>
     </div>
   );
 };
-
 export default GeneralHeader;
