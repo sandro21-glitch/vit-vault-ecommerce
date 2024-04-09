361c3754cc8736c817185ca607b9b8699a956781
Sandro <vipsandro11@gmail.com>
2024-04-09 19:39:07 +0400

    update MobileNav.tsx

--- src/features/header/navbar/mobile/MobileNav.tsx
+++ src/features/header/navbar/mobile/MobileNav.tsx
@@ -1,19 +1,26 @@
+import { useState } from "react";
 import SearchProduct from "../../general/SearchProduct";
+import MobileNavList from "./MobileNavList";
+import NavTabs from "./NavTabs";
 
 type MobileNavTypes = {
   isOpen: boolean;
 };
 
 const MobileNav = ({ isOpen }: MobileNavTypes) => {
+  const [activeTab, setActiveTab] = useState<string>("menu");
+
   return (
     <div
       className={`fixed ${
         isOpen ? "translate-x-0" : "translate-x-[-200%]"
       } bg-white top-0 left-0 w-[60%] right-0 bottom-0 z-[9999] transition-all ease-in duration-300 delay-100`}
     >
-      <ul className="overflow-hidden">
+      <div className="overflow-hidden">
         <SearchProduct />
-      </ul>
+        <NavTabs setActiveTab={setActiveTab} activeTab={activeTab} />
+        {activeTab === "menu" ? <MobileNavList /> : <p>Categories</p>}
+      </div>
     </div>
   );
 };
