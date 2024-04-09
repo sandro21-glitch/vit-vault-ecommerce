f60eca0ccdcd1c982b4da7b835d5081bd0666323
Sandro <vipsandro11@gmail.com>
2024-04-09 18:40:14 +0400

    feat: create HeaderText.tsx

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,11 @@
-import HomeSlider from "../features/mainPage/HomeSlider";
+import PopularCategories from "../features/mainPage/popularCategory/PopularCategories";
+import HomeSlider from "../features/mainPage/sliderPage/HomeSlider";
 
 const MainPage = () => {
   return (
     <section className="section-center section-x">
       <HomeSlider />
+      <PopularCategories />
     </section>
   );
 };
