0d924c502e8521333369d4f124f4a7c78dcab11a
Sandro <vipsandro11@gmail.com>
2024-04-09 18:40:26 +0400

    feat: create HomeSlider.tsx

---
+++ src/features/mainPage/sliderPage/HomeSlider.tsx
@@ -0,0 +1,11 @@
+import SliderContent from "./SliderContent";
+
+const HomeSlider = () => {
+  return (
+    <article className=" mx-auto select-none mb-[8vh]">
+      <SliderContent />
+    </article>
+  );
+};
+
+export default HomeSlider;
