3ebb436a40de66bfce37d965ca5defdcae90ebe0
Sandro <vipsandro11@gmail.com>
2024-04-09 18:39:16 +0400

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
         primaryGreen: "#2E854B",
         secondaryGreen: "#5CB85C",
       },
+      boxShadow: {
+        mainShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+      },
     },
   },
   plugins: [],
