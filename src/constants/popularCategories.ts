d9618d90167078e5be7d90f23890f32e0af6394d
Sandro <vipsandro11@gmail.com>
2024-04-09 18:39:54 +0400

    feat: create popularCategories.ts

---
+++ src/constants/popularCategories.ts
@@ -0,0 +1,42 @@
+export const popularCategory = [
+  {
+    id: 1,
+    name: "სახსრების ჯანმრთელობა",
+    image: "https://vitamini.ge/storage/products/categories/16/fix2.jpg",
+  },
+  {
+    id: 2,
+    name: "ჯანსაღი საკვები",
+    image: "https://vitamini.ge/storage/products/categories/16/fix2.jpg",
+  },
+  {
+    id: 3,
+    name: "ვეგანური",
+    image: "https://vitamini.ge/storage/products/categories/16/fix2.jpg",
+  },
+  {
+    id: 4,
+    name: "გულის ჯანმრთელობა",
+    image: "https://vitamini.ge/storage/products/categories/16/fix2.jpg",
+  },
+  {
+    id: 5,
+    name: "ფამფი",
+    image: "https://vitamini.ge/storage/products/categories/16/fix2.jpg",
+  },
+  {
+    id: 6,
+    name: "სექსუალური ჯანმრთელობა",
+    image: "https://vitamini.ge/storage/products/categories/16/fix2.jpg",
+  },
+  {
+    id: 7,
+    name: "გონებრივი შესაძლებლობების გაუმჯობესება",
+    image: "https://vitamini.ge/storage/products/categories/16/fix2.jpg",
+  },
+  {
+    id: 8,
+    name: "კანი, თმა, ფრჩხილები",
+    image: "https://vitamini.ge/storage/products/categories/16/fix2.jpg",
+  },
+];
