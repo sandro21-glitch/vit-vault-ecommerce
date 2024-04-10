aff388a55785e25157d368e19c104ac2a86e1080
Sandro <vipsandro11@gmail.com>
2024-04-10 23:58:41 +0400

    update Footer.tsx

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,9 +1,9 @@
-
-
 const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
-}
+    <footer className="border border-t">
+      <div className="section-center section-x py-5">footer</div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
