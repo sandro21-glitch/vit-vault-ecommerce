91cf6c8ee9095357b764ee683de4e57cf3de65c4
Sandro <vipsandro11@gmail.com>
2024-04-10 23:59:30 +0400

    update Footer.tsx

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="border border-t">
-      <div className="section-center section-x py-5">footer</div>
+    <footer className="border border-t py-5">
+      <div className="section-center section-x">Footer</div>
     </footer>
   );
 };
