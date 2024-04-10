129b3efe079d2ef9b2d64e0664a9e1a66de60cbe
Sandro <vipsandro11@gmail.com>
2024-04-10 23:28:52 +0400

    feat: create DiscountedProductList.tsx

---
+++ src/features/mainPage/discountedProducts/DiscountedProductList.tsx
@@ -0,0 +1,58 @@
+import SingleDiscountedProduct from "./SingleDiscountedProduct";
+
+const tempList = [
+  {
+    id: "1",
+    name: "Kevin Levrone - Gold Whey",
+    image: "https://vitamini.ge/storage/products/11822/123.jpg",
+    price: "210.00₾",
+  },
+  {
+    id: "11",
+    name: "Kevin Levrone - Gold Whey",
+    image: "https://vitamini.ge/storage/products/11822/123.jpg",
+    price: "210.00₾",
+  },
+  {
+    id: "1111",
+    name: "Kevin Levrone - Gold Whey",
+    image: "https://vitamini.ge/storage/products/11822/123.jpg",
+    price: "210.00₾",
+  },
+  {
+    id: "11111",
+    name: "Kevin Levrone - Gold Whey",
+    image: "https://vitamini.ge/storage/products/11822/123.jpg",
+    price: "210.00₾",
+  },
+  {
+    id: "241",
+    name: "Kevin Levrone - Gold Whey",
+    image: "https://vitamini.ge/storage/products/11822/123.jpg",
+    price: "210.00₾",
+  },
+  {
+    id: "11414111",
+    name: "Kevin Levrone - Gold Whey",
+    image: "https://vitamini.ge/storage/products/11822/123.jpg",
+    price: "210.00₾",
+  },
+  {
+    id: "1111414111",
+    name: "Kevin Levrone - Gold Whey",
+    image: "https://vitamini.ge/storage/products/11822/123.jpg",
+    price: "210.00₾",
+  },
+];
+
+const DiscountedProductList = () => {
+  return (
+    <ul className="grid grid-cols-4 w-full gap-2">
+      {tempList.map((product) => {
+        return <SingleDiscountedProduct key={product.id} product={product} />;
+      })}
+    </ul>
+  );
+};
+
+export default DiscountedProductList;
