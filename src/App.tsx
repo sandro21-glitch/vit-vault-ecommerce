1f40f6da52eb52cd4bb8ed15ce70c1d5c280e84b
Sandro <vipsandro11@gmail.com>
2024-04-11 15:48:17 +0400

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./ui/Header";
 import MainPage from "./pages/MainPage";
 import Footer from "./pages/Footer";
+import { useEffect } from "react";
+import { useAppDispatch } from "./hooks/hooks";
+import { fetchDataFromFirebase } from "./features/slices/productsSlice";
+
 const App = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDataFromFirebase());
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <Header />
