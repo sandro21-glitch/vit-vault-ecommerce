6447ea7b41e4342ac47e5458ce33a8e35ac1d154
Sandro <vipsandro11@gmail.com>
2024-04-11 19:47:49 +0400

    feat: create Categories.tsx and CategoryList.tsx

---
+++ src/features/header/navbar/desktop/category/CategoryList.tsx
@@ -0,0 +1,23 @@
+import { useAppSelector } from "../../../../../hooks/hooks";
+import SingleCategory from "./SingleCategory";
+const CategoryList = () => {
+  const { productData } = useAppSelector((store) => store.product);
+  const uniqueCategories = Array.from(
+    new Set(
+      productData?.map((product) => {
+        return product.category;
+      })
+    )
+  );
+  return (
+    <div className="absolute z-[9999] left-0 top-full w-full">
+      <ul className="border-l border-b border-r ">
+        {uniqueCategories.map((category, index) => {
+          return <SingleCategory key={index} category={category} />;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default CategoryList;
