62be07957ade3ac23f6ea099806415d4e4d4ab43
Sandro <vipsandro11@gmail.com>
2024-04-11 15:48:30 +0400

    update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,10 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB9tG_d1aGPXhJ5jU8UToebGsOTcMNqdmg",
   authDomain: "vitvault-47c5f.firebaseapp.com",
   projectId: "vitvault-47c5f",
+  databaseURL: "https://vitvault-47c5f-default-rtdb.firebaseio.com/",
   storageBucket: "vitvault-47c5f.appspot.com",
   messagingSenderId: "97791255698",
   appId: "1:97791255698:web:20efe72793a152e46a724b",
@@ -12,4 +14,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
+export const database = getDatabase(app);
