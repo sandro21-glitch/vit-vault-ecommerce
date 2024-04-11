98bfa4418cdf2fd6b7f5511478edfd3b21df43ef
Sandro <vipsandro11@gmail.com>
2024-04-11 19:48:16 +0400

    feat: create TypesDropdown.tsx

---
+++ src/features/header/navbar/desktop/category/TypesDropdown.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+type TypesDropdownProps = {
+  categoryTypes: string[] | undefined;
+};
+const TypesDropdown = ({ categoryTypes }: TypesDropdownProps) => {
+  return (
+    <div className="absolute z-[9999] hidden group-hover:block right-0 top-0 translate-x-full bg-white shadow-mainShadow">
+      {categoryTypes?.map((type) => {
+        return (
+          <div className="flex flex-col w-[10rem] h-auto p-3 hover:bg-veryLightGray">
+            <Link to={"products/category/type"}>
+              <p className="font-medium">{type}</p>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default TypesDropdown;
