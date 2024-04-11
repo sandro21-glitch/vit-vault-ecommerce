32a1ce0eb52f296856aea7649c01078cd6f1ee8a
Sandro <vipsandro11@gmail.com>
2024-04-11 23:41:23 +0400

    update SingleDiscountedProduct.tsx

--- src/features/mainPage/discountedProducts/SingleDiscountedProduct.tsx
+++ src/features/mainPage/discountedProducts/SingleDiscountedProduct.tsx
@@ -1,23 +1,23 @@
+import { Product } from "../../../types/ProductTypes";
+import CheckDiscount from "../../../ui/CheckDiscount";
+
 type DiscountedProductListTypes = {
-  product: {
-    id: string;
-    name: string;
-    image: string;
-    price: string;
-  };
+  product: Product;
 };
 const SingleDiscountedProduct = ({
-  product: { image, name, price },
+  product: { images, name, price, discount },
 }: DiscountedProductListTypes) => {
   return (
-    <li className="group flex flex-col items-center text-center hover:shadow-mainShadow hover:scale-105 transition-all ease-in duration-150 cursor-pointer">
-      <img
-        src={image}
-        alt=""
-        className="max-h-[268px] w-full object-cover max-w-[250px]"
-      />
+    <li className="h-[380px] group flex flex-col items-center text-center hover:shadow-mainShadow hover:scale-105 transition-all ease-in duration-150 cursor-pointer">
+      <div className="w-full h-[268px] overflow-hidden flex justify-center items-center pt-10 mb-2">
+        <img
+          src={images[0]}
+          alt=""
+          className="object-cover w-[150px]  h-auto "
+        />
+      </div>
       <h6 className="mb-5">{name}</h6>
-      <p className="mb-2">{price}</p>
+      <CheckDiscount discount={discount} price={price} />
       <button className="invisible group-hover:visible mb-5 bg-secondaryGreen text-white font-semibold text-[11px] py-[8px] px-[12px]">
         <span>კალათაში დამატება</span>
       </button>
