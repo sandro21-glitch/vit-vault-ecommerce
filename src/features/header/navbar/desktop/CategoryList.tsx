3b89ed1ff9d3dbf72203709c23c01385dc790ed7
Sandro <vipsandro11@gmail.com>
2024-04-11 15:56:50 +0400

    feat: :sparkles: Update CategoryList component to dynamically render unique categories
    - Utilize Redux store to retrieve product data - Filter out duplicate categories and display them dynamically in the CategoryList component

--- src/features/header/navbar/desktop/CategoryList.tsx
+++ src/features/header/navbar/desktop/CategoryList.tsx
@@ -1,65 +1,25 @@
 import { Link } from "react-router-dom";
 import { IoIosArrowForward } from "react-icons/io";
+import { useAppSelector } from "../../../../hooks/hooks";
 const CategoryList = () => {
+  const { productData } = useAppSelector((store) => store.product);
+  const uniqueCategories = Array.from(
+    new Set(productData?.map((product) => product.category))
+  );
   return (
-    <div className="absolute left-0 top-full w-full">
+    <div className="absolute z-[9999] left-0 top-full w-full">
       <ul className="border-l border-b border-r ">
-        <li className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-          <Link to="/">საკვები დანამატები</Link>
-          <IoIosArrowForward />
-        </li>
-        <li className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-          <Link to="/">საკვები დანამატები</Link>
-          <IoIosArrowForward />
-        </li>
-        <li className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-          <Link to="/">საკვები დანამატები</Link>
-          <IoIosArrowForward />
-        </li>
-        <li className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-          <Link to="/">საკვები დანამატები</Link>
-          <IoIosArrowForward />
-        </li>
-        <li className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-          <Link to="/">საკვები დანამატები</Link>
-          <IoIosArrowForward />
-        </li>
-        <li className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-          <Link to="/">საკვები დანამატები</Link>
-          <IoIosArrowForward />
-        </li>
-        <li className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-          <Link to="/">საკვები დანამატები</Link>
-          <IoIosArrowForward />
-        </li>
-        <li className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-          <Link to="/">საკვები დანამატები</Link>
-          <IoIosArrowForward />
-        </li>
-        <li className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-          <Link to="/">საკვები დანამატები</Link>
-          <IoIosArrowForward />
-        </li>
-        <li className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-          <Link to="/">საკვები დანამატები</Link>
-          <IoIosArrowForward />
-        </li>
-        <li className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-          <Link to="/">საკვები დანამატები</Link>
-          <IoIosArrowForward />
-        </li>
-        <li className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-          <Link to="/">საკვები დანამატები</Link>
-          <IoIosArrowForward />
-        </li>
-        <li className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-          <Link to="/">საკვები დანამატები</Link>
-          <IoIosArrowForward />
-        </li>
-        <li className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-          <Link to="/">საკვები დანამატები</Link>
-          <IoIosArrowForward />
-        </li>
+        {uniqueCategories.map((data, index) => {
+          return (
+            <li
+              key={index}
+              className="border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2"
+            >
+              <Link to="/">{data}</Link>
+              <IoIosArrowForward />
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
