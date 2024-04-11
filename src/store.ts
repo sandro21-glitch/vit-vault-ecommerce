c49fc1eca58aa0c12cfb423ced8cadecb9cc66c2
Sandro <vipsandro11@gmail.com>
2024-04-11 15:48:27 +0400

    feat: create store.ts

---
+++ src/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from "./features/slices/productsSlice";
+export const store = configureStore({
+  reducer: {
+    product: productReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
