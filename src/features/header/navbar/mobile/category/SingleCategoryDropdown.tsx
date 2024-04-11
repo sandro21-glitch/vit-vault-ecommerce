327bec9b3c38dcf20a9254d6eedfa1b9877c7d61
Sandro <vipsandro11@gmail.com>
2024-04-11 22:30:28 +0400

    feat: create SingleCategoryDropdown.tsx

---
+++ src/features/header/navbar/mobile/category/SingleCategoryDropdown.tsx
@@ -0,0 +1,11 @@
+type SingleCategoryDropdownTypes = {
+  type: string | null;
+};
+
+const SingleCategoryDropdown = ({ type }: SingleCategoryDropdownTypes) => {
+  return <div className="h-[3rem] flex items-center border-b">
+    <span className="pl-5 text-primaryGray">{type}</span>
+  </div>;
+};
+
+export default SingleCategoryDropdown;
