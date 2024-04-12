e0550a68beda216a0ad1f6e0728251f7b820863d
Sandro <vipsandro11@gmail.com>
2024-04-12 20:54:24 +0400

    update Footer.tsx

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,7 +1,18 @@
 const Footer = () => {
   return (
     <footer className="border border-t py-5">
-      <div className="section-center section-x">Footer</div>
+      <div className="section-center section-x flex justify-between items-center">
+        <p>© 2024 VitVault. All rights reserved</p>
+        <p>
+          <a
+            href="https://www.top.ge/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="https://www.top.ge/source/img/logo.gif" alt="top link" />
+          </a>
+        </p>
+      </div>
     </footer>
   );
 };
