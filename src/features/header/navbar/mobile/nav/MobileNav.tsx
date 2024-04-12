3c58472eebf15436a3e91133b8d7c2330df0604f
Sandro <vipsandro11@gmail.com>
2024-04-12 17:55:05 +0400

    update MobileNav.tsx

--- src/features/header/navbar/mobile/nav/MobileNav.tsx
+++ src/features/header/navbar/mobile/nav/MobileNav.tsx
@@ -15,7 +15,7 @@ const MobileNav = ({ isOpen }: MobileNavTypes) => {
     <div
       className={`fixed ${
         isOpen ? "translate-x-0" : "translate-x-[-200%]"
-      } bg-white top-0 left-0 w-[60%] right-0 bottom-0 z-[9999] transition-all ease-in duration-300 delay-100`}
+      } overflow-y-scroll bg-white top-0 left-0 w-[60%] right-0 bottom-0 z-[9999] transition-all ease-in duration-300 delay-100`}
     >
       <div className="overflow-hidden">
         <SearchProduct />
