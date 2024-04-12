24486a388c6071f18c5395208d44beb58c627277
Sandro <vipsandro11@gmail.com>
2024-04-12 22:15:47 +0400

    feat: create CartOItemCount.tsx

---
+++ src/features/header/general/cart/CartOItemCount.tsx
@@ -0,0 +1,12 @@
+const CartOItemCount = () => {
+  return (
+    <div
+      className="w-[1rem] h-[1rem] flex items-center justify-center
+   rounded-full text-[.7rem] text-white bg-primaryGreen absolute left-5 top-0"
+    >
+      1
+    </div>
+  );
+};
+
+export default CartOItemCount;
