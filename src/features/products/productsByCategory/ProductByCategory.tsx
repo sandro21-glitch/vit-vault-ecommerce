14ade0a482699cd3f1894eff25d4b48e657c4f18
Sandro <vipsandro11@gmail.com>
2024-04-13 19:33:11 +0400

    update ProductByCategory.tsx

--- src/features/products/productsByCategory/ProductByCategory.tsx
+++ src/features/products/productsByCategory/ProductByCategory.tsx
@@ -1,14 +1,6 @@
-import { useParams } from "react-router-dom";
-import { useAppSelector } from "../../../hooks/hooks";
-import CheckDiscount from "../../../ui/CheckDiscount";
+import ProductsByCategoryList from "./ProductsByCategoryList";
 
 const ProductByCategory = () => {
-  const { productData } = useAppSelector((store) => store.product);
-  const { category } = useParams();
-
-  const selectedCategory = productData?.filter(
-    (product) => product.category === category?.replace(/-/g, " ")
-  );
 
   return (
     <section className="section-x section-center">
@@ -16,26 +8,7 @@ const ProductByCategory = () => {
         <div className="w-full max-w-[25%] h-auto">
           testtttffffffffffffffffffffff
         </div>
-        <ul className="grid grid-cols-4 gap-5 w-full max-w-[75%] mt-20">
-          {selectedCategory?.map((categoryItem) => {
-            const { name, images, price, id, discount } = categoryItem;
-            return (
-              <li
-                key={id}
-                className="w-full h-[350px] flex flex-col items-center text-center bg-white hover:shadow-mainShadow hover:scale-105 transition-all ease-in duration-200 cursor-pointer"
-              >
-                <img
-                  src={images[0]}
-                  alt={name}
-                  loading="lazy"
-                  className="h-[200px] w-auto object-cover mt-10"
-                />
-                <h6 className="text-[14px]">{name}</h6>
-                <p>{CheckDiscount({ price, discount })}</p>
-              </li>
-            );
-          })}
-        </ul>
+        <ProductsByCategoryList />
       </article>
     </section>
   );
