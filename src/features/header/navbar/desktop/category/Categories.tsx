df1bbecdceb469e9bfcb627f14230ccc5339e488
Sandro <vipsandro11@gmail.com>
2024-04-13 21:17:46 +0400

    update Categories.tsx

--- src/features/header/navbar/desktop/category/Categories.tsx
+++ src/features/header/navbar/desktop/category/Categories.tsx
@@ -1,18 +1,29 @@
 import { IoIosArrowForward } from "react-icons/io";
 import { GiHamburgerMenu } from "react-icons/gi";
 import CategoryList from "./CategoryList";
+import { useState } from "react";
 const Categories = () => {
+  const [isLocationChanged, setIsLocationChanged] = useState<boolean>(false);
   return (
     <div className="h-full mr-[15px]">
-      <div className="relative min-w-[270px] max-w-[320px] h-full bg-secondaryGreen flex items-center justify-between">
+      <div className="relative group min-w-[270px] max-w-[320px] h-full bg-secondaryGreen flex items-center justify-between">
         <div className="flex items-center gap-1 pl-5 text-white text-[15px] font-bold">
           <GiHamburgerMenu className="text-[20px]" />
           <span>კატეგორიები</span>
         </div>
-        <span className="pr-3 text-white">
+        <div
+          className={`mr-2 text-white ${
+            isLocationChanged
+              ? "group-hover:rotate-[-90deg] transform origin-center"
+              : ""
+          } rotate-90 transform origin-center transition-all ease-in duration-200`}
+        >
           <IoIosArrowForward />
-        </span>
-        <CategoryList />
+        </div>
+        <CategoryList
+          isLocationChanged={isLocationChanged}
+          setIsLocationChanged={setIsLocationChanged}
+        />
       </div>
     </div>
   );
