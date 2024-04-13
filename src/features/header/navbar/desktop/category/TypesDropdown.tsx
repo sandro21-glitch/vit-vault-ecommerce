b2dd029fccf8fb78f3aa582c5390233d4b2c4f65
Sandro <vipsandro11@gmail.com>
2024-04-13 16:28:33 +0400

    update TypesDropdown.tsx

--- src/features/header/navbar/desktop/category/TypesDropdown.tsx
+++ src/features/header/navbar/desktop/category/TypesDropdown.tsx
@@ -6,10 +6,13 @@ type TypesDropdownProps = {
 const TypesDropdown = ({ categoryTypes }: TypesDropdownProps) => {
   return (
     <div className="absolute z-[9999] hidden group-hover:block right-0 top-0 translate-x-full bg-white shadow-mainShadow">
-      {categoryTypes?.map((type,index) => {
+      {categoryTypes?.map((type, index) => {
         return (
-          <div key={index} className="flex flex-col w-[10rem] h-auto p-3 hover:bg-veryLightGray">
-            <Link to={"products/category/type"}>
+          <div
+            key={index}
+            className="flex flex-col w-[10rem] h-auto p-3 hover:bg-veryLightGray"
+          >
+            <Link to={`product/${type}`}>
               <p className="font-medium">{type}</p>
             </Link>
           </div>
