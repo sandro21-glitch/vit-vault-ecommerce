915bf62a9c7e2c88bcf5fa24990b98248e823eb4
Sandro <vipsandro11@gmail.com>
2024-04-13 16:28:30 +0400

    update SingleCategory.tsx

--- src/features/header/navbar/desktop/category/SingleCategory.tsx
+++ src/features/header/navbar/desktop/category/SingleCategory.tsx
@@ -10,13 +10,13 @@ const SingleCategory = ({ category }: SingleCategoryTypes) => {
   const { productData } = useAppSelector((store) => store.product);
 
   const categoryTypes = productData
-    ?.filter((data) => data.category === category) 
-    .flatMap((data) => data.type ?? []) 
-    .filter((value, index, self) => self.indexOf(value) === index); 
+    ?.filter((data) => data.category === category)
+    .flatMap((data) => data.type ?? [])
+    .filter((value, index, self) => self.indexOf(value) === index);
 
   return (
     <li className="relative group border border-b text-[13px] cursor-pointer font-semibold flex justify-between items-center px-3 py-2">
-      <Link to="/">{category}</Link>
+      <Link to={`/product/${category}`}>{category}</Link>
       {categoryTypes && categoryTypes.length !== 0 && <IoIosArrowForward />}
       <TypesDropdown categoryTypes={categoryTypes} />
     </li>
