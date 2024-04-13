6c7fd40302dedfa653f9265737068913f0209dfb
Sandro <vipsandro11@gmail.com>
2024-04-13 21:17:49 +0400

    update CategoryList.tsx

--- src/features/header/navbar/desktop/category/CategoryList.tsx
+++ src/features/header/navbar/desktop/category/CategoryList.tsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 import { useAppSelector } from "../../../../../hooks/hooks";
 import SingleCategory from "./SingleCategory";
-const CategoryList = () => {
+import { useLocation } from "react-router-dom";
+
+type CategoryListTypes = {
+  isLocationChanged: boolean;
+  setIsLocationChanged: (isOpen: boolean) => void;
+};
+
+const CategoryList = ({
+  isLocationChanged,
+  setIsLocationChanged,
+}: CategoryListTypes) => {
   const { productData } = useAppSelector((store) => store.product);
+
   const uniqueCategories = Array.from(
     new Set(
       productData?.map((product) => {
@@ -9,8 +21,22 @@ const CategoryList = () => {
       })
     )
   );
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setIsLocationChanged(false);
+    } else {
+      setIsLocationChanged(true);
+    }
+  }, [location.pathname]);
+
   return (
-    <div className="absolute z-[9999] left-0 top-full w-full bg-white">
+    <div
+      className={`${
+        isLocationChanged ? "hidden" : ""
+      }  group-hover:block absolute z-[9999] left-0 top-full w-full bg-white`}
+    >
       <ul className="border-l border-b border-r ">
         {uniqueCategories.map((category, index) => {
           return <SingleCategory key={index} category={category} />;
