2200d6bddc113c5ceac0d95a32ba001de799f2f6
Sandro <vipsandro11@gmail.com>
2024-04-13 23:05:22 +0400

    update ProductByCategory.tsx

--- src/features/products/productsByCategory/ProductByCategory.tsx
+++ src/features/products/productsByCategory/ProductByCategory.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import SectionHeader from "../../../ui/SectionHeader";
 import ProductsByCategoryList from "./ProductsByCategoryList";
 import ProductsFilter from "./ProductsFilter";
+import ListHeader from "./ListHeader";
 
 const ProductByCategory = () => {
   const { category } = useParams();
@@ -10,7 +11,10 @@ const ProductByCategory = () => {
       <SectionHeader path={category ? category : ""} />
       <article className="section-x section-center flex">
         <ProductsFilter />
-        <ProductsByCategoryList />
+        <div className=" w-full max-w-[75%]">
+          <ListHeader category={category} />
+          <ProductsByCategoryList />
+        </div>
       </article>
     </section>
   );
