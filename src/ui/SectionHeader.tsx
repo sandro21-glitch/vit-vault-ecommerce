4204d254c03e0040ed64c899d6fe3dc5806a6cc7
Sandro <vipsandro11@gmail.com>
2024-04-13 19:45:41 +0400

    feat: Add navigation functionality to SectionHeader component

--- src/ui/SectionHeader.tsx
+++ src/ui/SectionHeader.tsx
@@ -1,9 +1,31 @@
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 type SectionHeaderTypes = {
   path: string;
 };
 
 const SectionHeader = ({ path }: SectionHeaderTypes) => {
-  return <header>{path}</header>;
+  if (!path) return null;
+  const categoryName = path.replace(/-/g, " ");
+  const navigate = useNavigate();
+
+  const navigateBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <header className="bg-primaryGreen">
+      <div className="section-center py-[15px] flex items-center justify-center">
+        <button
+          onClick={navigateBack}
+          className="mr-5 rotate-180 text-[2rem] text-white"
+        >
+          <FaLongArrowAltRight />
+        </button>
+        <h1 className="text-white font-bold text-[2rem]">{categoryName}</h1>
+      </div>
+    </header>
+  );
 };
 
 export default SectionHeader;
