678b98fbf56ee9be14eed8bde4e973927d7bc20f
Sandro <vipsandro11@gmail.com>
2024-04-14 19:08:52 +0400

    feat: create FilterByCategory.tsx

---
+++ src/features/products/productsByCategory/FilterByCategory.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { useAppSelector } from "../../../hooks/hooks";
+
+const FilterByCategory = () => {
+  const { productData } = useAppSelector((store) => store.product);
+
+  const categories = productData?.map((item) => item.category);
+
+  const uniqueCategories = [...new Set(categories)];
+  return (
+    <div>
+      <h5 className="mb-[10px] font-semibold tracking-wider">კატეგორიები</h5>
+      <ul>
+        {uniqueCategories?.map((category, index) => {
+          const formattedCategory = category.replace(/ /g, "-");
+          return (
+            <li
+              key={index}
+              className="py-[8px] text-primaryGray text-[14px] hover:text-black transition-colors ease-in duration-150"
+            >
+              <Link to={`/product/${formattedCategory}`}>{category}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default FilterByCategory;
