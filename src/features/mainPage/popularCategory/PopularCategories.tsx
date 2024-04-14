10dcc5d7b8b82b5c93e2989e1356b05b3c3d03e8
Sandro <vipsandro11@gmail.com>
2024-04-14 22:37:24 +0400

    update PopularCategories.tsx

--- src/features/mainPage/popularCategory/PopularCategories.tsx
+++ src/features/mainPage/popularCategory/PopularCategories.tsx
@@ -5,7 +5,7 @@ const PopularCategories = () => {
   return (
     <article className="mb-[8vh]">
       <HeaderText text="პოპულარული კატეგორიები" />
-      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
+      <ul className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-4 gap-5">
         {popularCategory.map((item) => {
           return <SinglePopularCategory key={item.id} item={item} />;
         })}
