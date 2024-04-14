c9ff248f2545438a6a3b19dfa9b8da9ad750c7e9
Sandro <vipsandro11@gmail.com>
2024-04-14 17:42:14 +0400

    update ProductsByCategoryList.tsx

--- src/features/products/productsByCategory/ProductsByCategoryList.tsx
+++ src/features/products/productsByCategory/ProductsByCategoryList.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useAppSelector } from "../../../hooks/hooks";
-import SingleProductByCategory from "./SingleProductByCategory";
+import ProductCard from "../../../ui/ProductCard";
 
 const ProductsByCategoryList = () => {
   const { productData } = useAppSelector((store) => store.product);
@@ -14,10 +14,7 @@ const ProductsByCategoryList = () => {
     <ul className="grid grid-cols-4 gap-5 mt-10">
       {selectedCategory?.map((categoryItem) => {
         return (
-          <SingleProductByCategory
-            key={categoryItem.id}
-            categoryItem={categoryItem}
-          />
+          <ProductCard key={categoryItem.id} categoryItem={categoryItem} />
         );
       })}
     </ul>
