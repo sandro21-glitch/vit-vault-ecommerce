f06d85ce85a13935e4ce1ae4fed58304e4fffae6
Sandro <vipsandro11@gmail.com>
2024-04-14 19:09:01 +0400

    update ProductsByCategoryList.tsx

--- src/features/products/productsByCategory/ProductsByCategoryList.tsx
+++ src/features/products/productsByCategory/ProductsByCategoryList.tsx
@@ -2,13 +2,24 @@ import { useParams } from "react-router-dom";
 import { useAppSelector } from "../../../hooks/hooks";
 import ProductCard from "../../../ui/ProductCard";
 
-const ProductsByCategoryList = () => {
+type ProductsByCategoryList = {
+  filterPrice: number;
+};
+
+const ProductsByCategoryList = ({ filterPrice }: ProductsByCategoryList) => {
   const { productData } = useAppSelector((store) => store.product);
   const { category } = useParams();
 
-  const selectedCategory = productData?.filter(
-    (product) => product.category === category?.replace(/-/g, " ")
-  );
+  const selectedCategory = productData?.filter((product) => {
+    return (
+      product.price <= filterPrice &&
+      product.category === category?.replace(/-/g, " ")
+    );
+  });
+
+  if (selectedCategory && selectedCategory?.length < 1) {
+    return <p className="text-[1.5rem] h-full flex items-center justify-center">პროდუქტები ვერ მოიძებნა</p>;
+  }
 
   return (
     <ul className="grid grid-cols-4 gap-5 mt-10">
