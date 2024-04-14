cca1bc73c3618b295e12c48de5ef0cdd77201ca7
Sandro <vipsandro11@gmail.com>
2024-04-14 19:26:42 +0400

    Refactor: Restructure FilterByPrice component to use useState for managing price input and add a filter button for enhanced usability

--- src/features/products/productsByCategory/FilterByPrice.tsx
+++ src/features/products/productsByCategory/FilterByPrice.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useAppSelector } from "../../../hooks/hooks";
 import { formatToGeorgianLari } from "../../../utils/formatPrice";
 
@@ -12,17 +13,18 @@ const FilterByPrice: React.FC<FilterByPriceProps> = ({
 }) => {
   const { productData } = useAppSelector((store) => store.product);
   const prices = productData?.map((product) => product.price) ?? [];
-
+  const [newPrice, setNewPrice] = useState<number>(filterPrice);
   // find the maximum price
   const maxPrice = Math.max(...prices);
 
   const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newPrice = parseInt(event.target.value);
-    setFilterPrice(newPrice);
+    setNewPrice(parseInt(event.target.value));
   };
 
+  const handleFilterPrice = () => setFilterPrice(newPrice);
+
   return (
-    <div className="mt-4">
+    <div className="my-4">
       <label
         htmlFor="price-range"
         className="block mb-2 text-[1rem] font-semibold text-gray-900 dark:text-white"
@@ -34,17 +36,25 @@ const FilterByPrice: React.FC<FilterByPriceProps> = ({
         type="range"
         min={0}
         max={maxPrice}
-        value={filterPrice}
+        value={newPrice}
         onChange={handlePriceChange}
-        className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+        className="w-full h-2 mb-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
       />
-      <p className="mt-2 text-gray-700 dark:text-gray-300">
-        ფასი: <span className="font-semibold">₾0</span>
-        <span className="mx-1">—</span>
-        <span className="font-semibold">
-          {formatToGeorgianLari(filterPrice)}
-        </span>
-      </p>
+      <div className="flex items-center justify-between">
+        <p className=" text-gray-700 dark:text-gray-300 whitespace-nowrap">
+          ფასი: <span className="font-semibold text-[14px]">₾0</span> —
+          <span className="font-semibold text-[14px]">
+            {formatToGeorgianLari(newPrice)}
+          </span>
+        </p>
+        <button
+          type="button"
+          onClick={handleFilterPrice}
+          className="py-[10px] px-[14px] text-[12px] font-semibold text-[#3E3E3E] bg-[#F3F3F3] hover:bg-[#efefef] transition-all ease-in duration-150"
+        >
+          გაფილტვრა
+        </button>
+      </div>
     </div>
   );
 };
