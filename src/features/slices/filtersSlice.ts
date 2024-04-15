881bb98046fd41d0342ae628ff12f891548106db
Sandro <vipsandro11@gmail.com>
2024-04-15 20:24:13 +0400

    feat:Add sort functionality to filtersSlice

--- src/features/slices/filtersSlice.ts
+++ src/features/slices/filtersSlice.ts
@@ -2,10 +2,12 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface FiltersState {
   productsPerPage: number;
+  sort: string;
 }
 
 const initialState: FiltersState = {
   productsPerPage: 24,
+  sort: "normal",
 };
 
 export const filtersSlice = createSlice({
@@ -15,9 +17,12 @@ export const filtersSlice = createSlice({
     setProductPerPage: (state, action: PayloadAction<number>) => {
       state.productsPerPage = action.payload;
     },
+    setPriceSort: (state, action: PayloadAction<string>) => {
+      state.sort = action.payload;
+    },
   },
 });
 
-export const { setProductPerPage } = filtersSlice.actions;
+export const { setProductPerPage, setPriceSort } = filtersSlice.actions;
 
 export default filtersSlice.reducer;
