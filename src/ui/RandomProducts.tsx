29912c1f4cda8ba80d9133c2839139dab45f3f28
Sandro <vipsandro11@gmail.com>
2024-04-15 23:18:40 +0400

    feat: create RandomProducts.tsx

---
+++ src/ui/RandomProducts.tsx
@@ -0,0 +1,12 @@
+import RandomProductList from "../features/productPages/randomProduct/RandomProductList";
+
+const RandomProducts = () => {
+  return (
+    <div>
+      <h5 className="mb-[15px] font-semibold tracking-wider">პროდუქტები</h5>
+      <RandomProductList />
+    </div>
+  );
+};
+
+export default RandomProducts;
