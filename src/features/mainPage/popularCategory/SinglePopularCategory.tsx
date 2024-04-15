353c48bbc8da55a4dcee20d61efbb214686db09a
Sandro <vipsandro11@gmail.com>
2024-04-15 20:46:14 +0400

    refactor: include Link from react-router-dom in SinglePopularCategory

--- src/features/mainPage/popularCategory/SinglePopularCategory.tsx
+++ src/features/mainPage/popularCategory/SinglePopularCategory.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import { useAppSelector } from "../../../hooks/hooks";
 
 type SinglePopCategoryTypes = {
   item: {
     image: string;
     name: string;
+    path: string;
   };
 };
 
@@ -19,17 +21,19 @@ const SinglePopularCategory = ({ item }: SinglePopCategoryTypes) => {
 
   return (
     <li className="group cursor-pointer rounded-full">
-      <div className="shadow-mainShadow bg-white overflow-hidden rounded-full mb-4">
-        <img
-          src={item.image}
-          alt={item.name}
-          className="w-full p-5 border-2 border-primaryGreen  object-center h-auto group-hover:scale-105 transition-all ease-in duration-300 rounded-full"
-        />
-      </div>
-      <div className="text-center mt-2">
-        <h4 className="mb-1">{item.name}</h4>
-        <p>{handleCheckCategory(item.name)} პროდუქტი</p>
-      </div>
+      <Link to={`/product/${item.path}`}>
+        <div className="shadow-mainShadow bg-white overflow-hidden rounded-full mb-4">
+          <img
+            src={item.image}
+            alt={item.name}
+            className="w-full p-5 border-2 border-primaryGreen  object-center h-auto group-hover:scale-105 transition-all ease-in duration-300 rounded-full"
+          />
+        </div>
+        <div className="text-center mt-2">
+          <h4 className="mb-1">{item.name}</h4>
+          <p>{handleCheckCategory(item.name)} პროდუქტი</p>
+        </div>
+      </Link>
     </li>
   );
 };
