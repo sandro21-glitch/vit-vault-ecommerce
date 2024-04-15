8d9625564b037312797c77d17127c1432e4dc317
Sandro <vipsandro11@gmail.com>
2024-04-15 23:18:33 +0400

    feat: create SingleRandomProduct.tsx

---
+++ src/features/productPages/randomProduct/SingleRandomProduct.tsx
@@ -0,0 +1,31 @@
+import CheckDiscount from "../../../ui/CheckDiscount";
+
+type SingleRandomProdTypes = {
+  product: {
+    name: string;
+    images: string[];
+    discount: number;
+    price: number;
+  };
+};
+
+const SingleRandomProduct = ({
+  product: { name, images, discount, price },
+}: SingleRandomProdTypes) => {
+  return (
+    <li className="flex items-center gap-5 border-b pb-[15px] mb-[15px]">
+      <img
+        src={images[0]}
+        alt={name}
+        loading="lazy"
+        className="min-w-[50px] max-w-[50px]"
+      />
+      <div>
+        <p>{name}</p>
+        <CheckDiscount discount={discount} price={price} />
+      </div>
+    </li>
+  );
+};
+
+export default SingleRandomProduct;
