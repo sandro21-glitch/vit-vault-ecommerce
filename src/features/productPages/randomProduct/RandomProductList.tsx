fb9a271d799ce49a100662aa188fbb127d81254c
Sandro <vipsandro11@gmail.com>
2024-04-15 23:26:43 +0400

    update RandomProductList.tsx

--- src/features/productPages/randomProduct/RandomProductList.tsx
+++ src/features/productPages/randomProduct/RandomProductList.tsx
@@ -1,16 +1,28 @@
 import { useAppSelector } from "../../../hooks/hooks";
+import { useEffect, useState } from "react";
 import SingleRandomProduct from "./SingleRandomProduct";
+import { Product } from "../../../types/ProductTypes";
+import { useLocation } from "react-router-dom";
 
 const RandomProductList = () => {
   const { productData } = useAppSelector((store) => store.product);
+  const [randomProducts, setRandomProducts] = useState<Product[]>([]);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (productData) {
+      const copiedProductData = [...productData];
+      const shuffledProducts = copiedProductData.sort(
+        () => Math.random() - 0.5
+      );
+      const products = shuffledProducts.slice(0, 10);
+      setRandomProducts(products);
+    }
+  }, [location]);
 
   if (!productData) {
     return <div>Loading...</div>;
   }
-
-  const copiedProductData = [...productData];
-  const shuffledProducts = copiedProductData.sort(() => Math.random() - 0.5);
-  const randomProducts = shuffledProducts.slice(0, 10);
 
   return (
     <ul className="flex flex-col">
