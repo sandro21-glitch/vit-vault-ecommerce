0817f0aba383e99e4b988737de9d09d8efa5f6f9
Sandro <vipsandro11@gmail.com>
2024-04-15 19:42:53 +0400

    Updated store configuration to include filtersReducer

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./features/slices/productsSlice";
+import filtersReducer from "./features/slices/filtersSlice"; // Correct import
 export const store = configureStore({
   reducer: {
     product: productReducer,
+    filters: filtersReducer,
   },
 });
 
