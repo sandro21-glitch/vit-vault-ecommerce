e7789f6ca00ff905a21083c3fafed1655be52c9c
Sandro <vipsandro11@gmail.com>
2024-04-15 18:18:45 +0400

    feat: :sparkles: Implement pagination in ProductsByCategoryList component
    - Added pagination functionality to the ProductsByCategoryList component - Utilized React useState hook for managing current page state - Implemented dynamic calculation of products per page based on user input - Integrated CustomPagination component for improved user navigation - Enhanced user experience by enabling seamless browsing through product categories

--- src/features/products/productsByCategory/ProductsByCategoryList.tsx
+++ src/features/products/productsByCategory/ProductsByCategoryList.tsx
@@ -1,15 +1,22 @@
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useAppSelector } from "../../../hooks/hooks";
 import ProductCard from "../../../ui/ProductCard";
+import CustomPagination from "../../../ui/CustomPagination";
 
-type ProductsByCategoryList = {
+type ProductsByCategoryListProps = {
   filterPrice: number;
 };
 
-const ProductsByCategoryList = ({ filterPrice }: ProductsByCategoryList) => {
+const ProductsByCategoryList: React.FC<ProductsByCategoryListProps> = ({
+  filterPrice,
+}) => {
   const { productData } = useAppSelector((store) => store.product);
-  const { category } = useParams();
+  const { category } = useParams<{ category: string }>();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(8);
 
+  // filter products based on category and price
   const selectedCategory = productData?.filter((product) => {
     return (
       product.price <= filterPrice &&
@@ -17,18 +24,40 @@ const ProductsByCategoryList = ({ filterPrice }: ProductsByCategoryList) => {
     );
   });
 
-  if (selectedCategory && selectedCategory?.length < 1) {
-    return <p className="text-[1.5rem] h-full flex items-center justify-center">პროდუქტები ვერ მოიძებნა</p>;
+  // handle page change
+  const handlePageChange = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  if (!selectedCategory || selectedCategory.length < 1) {
+    return (
+      <p className="text-[1.5rem] h-full flex items-center justify-center">
+        პროდუქტები ვერ მოიძებნა
+      </p>
+    );
   }
 
+  // calculate index range for current page
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+
+  // get current products for the current page
+  const currentProducts = selectedCategory.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
   return (
-    <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-10">
-      {selectedCategory?.map((categoryItem) => {
-        return (
+    <div>
+      <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-10">
+        {currentProducts.map((categoryItem) => (
           <ProductCard key={categoryItem.id} categoryItem={categoryItem} />
-        );
-      })}
-    </ul>
+        ))}
+      </ul>
+      <CustomPagination
+        currentPage={currentPage}
+        totalPages={Math.ceil(selectedCategory.length / productsPerPage)}
+        onPageChange={handlePageChange}
+      />
+    </div>
   );
 };
 
