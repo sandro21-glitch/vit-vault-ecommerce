c06f98c31d17f6d7dca89246984a08bdb421a847
Sandro <vipsandro11@gmail.com>
2024-04-17 00:35:29 +0400

    update ProductCard.tsx

--- src/ui/ProductCard.tsx
+++ src/ui/ProductCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import soldOutIcon from "/icons/sold-out-sales-svgrepo-com.svg";
 import CheckDiscount from "./CheckDiscount";
+import { Link } from "react-router-dom";
 
 type ProductCardProps = {
   categoryItem: {
@@ -13,27 +14,29 @@ type ProductCardProps = {
   };
 };
 
-const ProductCard: React.FC<ProductCardProps> = ({
-  categoryItem: { discount, price, id, images, inStock, name },
-}) => {
+const ProductCard: React.FC<ProductCardProps> = ({ categoryItem }) => {
+  const { discount, price, id, images, inStock, name } = categoryItem;
   return (
-    <li
-      key={id}
-      className="relative w-full h-[350px] flex flex-col items-center text-center bg-white hover:shadow-mainShadow hover:scale-105 transition-all ease-in duration-200 cursor-pointer"
-    >
-      <img
-        src={images[0]}
-        alt={name}
-        loading="lazy"
-        className="h-[200px] w-auto object-cover mt-10 mb-2"
-      />
-      <h6 className="text-[14px]">{name}</h6>
-      <div>{CheckDiscount({ price, discount })}</div>
-      {!inStock && (
-        <div className="absolute top-0 right-0">
-          <img src={soldOutIcon} alt="sold out" className="w-[3rem] h-auto" />
-        </div>
-      )}
+    <li key={id}>
+      <Link
+        to={`/product/${name}`}
+        state={{ id }}
+        className="relative w-full h-[350px] flex flex-col items-center text-center bg-white hover:shadow-mainShadow hover:scale-105 transition-all ease-in duration-200 cursor-pointer"
+      >
+        <img
+          src={images[0]}
+          alt={name}
+          loading="lazy"
+          className="h-[200px] w-auto object-cover mt-10 mb-2"
+        />
+        <h6 className="text-[14px]">{name}</h6>
+        <div>{CheckDiscount({ price, discount })}</div>
+        {!inStock && (
+          <div className="absolute top-0 right-0">
+            <img src={soldOutIcon} alt="sold out" className="w-[3rem] h-auto" />
+          </div>
+        )}
+      </Link>
     </li>
   );
 };
