1620325b4082b62e90ff63cfba52e74b17cf697c
Sandro <vipsandro11@gmail.com>
2024-04-17 15:09:46 +0400

    update CheckDiscount.tsx

--- src/ui/CheckDiscount.tsx
+++ src/ui/CheckDiscount.tsx
@@ -3,17 +3,18 @@ import { formatToGeorgianLari } from "../utils/formatPrice";
 type CheckDiscountTypes = {
   price: number;
   discount: number;
+  classname?: string;
 };
 
-const CheckDiscount = ({ price, discount }: CheckDiscountTypes) => {
+const CheckDiscount = ({ price, discount, classname }: CheckDiscountTypes) => {
   if (discount > 0) {
     const discountedPrice = price - (price * discount) / 100;
     return (
       <div className="flex items-center gap-1">
-        <p className="line-through text-primaryGray">
+        <p className={`line-through text-primaryGray ${classname}`}>
           {formatToGeorgianLari(price)}
         </p>
-        <p className="text-secondaryGreen font-bold">
+        <p className={`text-secondaryGreen font-bold ${classname}`}>
           {formatToGeorgianLari(discountedPrice)}
         </p>
       </div>
@@ -21,7 +22,7 @@ const CheckDiscount = ({ price, discount }: CheckDiscountTypes) => {
   } else {
     return (
       <div>
-        <p className="text-secondaryGreen font-bold">
+        <p className={`text-secondaryGreen font-bold ${classname}`}>
           {formatToGeorgianLari(price)}
         </p>
       </div>
