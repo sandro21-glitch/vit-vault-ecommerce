00ab6f6af174f839af691484c166bc65368bf014
Sandro <vipsandro11@gmail.com>
2024-04-17 21:32:46 +0400

    feat: create AddToCartBtn.tsx

---
+++ src/features/productPages/singleProduct/productTopContent/rightSide/addToCart/AddToCartBtn.tsx
@@ -0,0 +1,9 @@
+const AddToCartBtn = () => {
+  return (
+    <button className="bg-secondaryGreen h-[42px] hover:bg-primaryGreen transition-all ease-in duration-150 px-[20px] text-[14px] text-white font-semibold">
+      კალათაში დამატება
+    </button>
+  );
+};
+
+export default AddToCartBtn;
