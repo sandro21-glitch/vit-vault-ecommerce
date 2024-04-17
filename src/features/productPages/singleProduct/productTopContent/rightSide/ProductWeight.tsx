70ec16171d3b6b92ac1fdcab829364355091ad48
Sandro <vipsandro11@gmail.com>
2024-04-17 23:24:28 +0400

    update ProductWeight.tsx

--- src/features/productPages/singleProduct/productTopContent/rightSide/ProductWeight.tsx
+++ src/features/productPages/singleProduct/productTopContent/rightSide/ProductWeight.tsx
@@ -4,7 +4,7 @@ type ProductWeightTypes = {
 
 const ProductWeight = ({ weight }: ProductWeightTypes) => {
   return (
-    <div className="flex justify-between text-[14px] border-b py-2">
+    <div className="flex justify-between text-[14px] border-b border-dashed py-2">
       <p className="text-black font-semibold">წონა</p>
       <p className="font-sans">{weight}</p>
     </div>
