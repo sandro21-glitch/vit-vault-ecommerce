92f81c1ddc26dbfb26246d72a6a0c01ebb016e06
Sandro <vipsandro11@gmail.com>
2024-04-17 21:34:06 +0400

    refactor: Update option handling and styling in Property component

--- src/features/productPages/singleProduct/productTopContent/rightSide/Property.tsx
+++ src/features/productPages/singleProduct/productTopContent/rightSide/Property.tsx
@@ -6,15 +6,15 @@ const Property = ({ property }: PropertyTypes) => {
   return (
     <div className="flex justify-between items-center mt-5">
       <div className="text-[14px] mr-5">აირჩიეთ პარამეტრი</div>
-      <select className="border-2 flex-1 py-2 cursor-pointer outline-none">
+      <select className="border-2 flex-1 py-2 text-[14px] cursor-pointer outline-none">
         {property?.map((prop, index) => {
           return (
             <option
               key={index}
-              disabled={prop.available}
+              disabled={!prop.available}
               value={prop.name}
               className={` text-[14px] cursor-not-allowed ${
-                !prop.available ? "text-primaryGray" : "text-gray-200"
+                prop.available ? "text-primaryGray" : "text-gray-200"
               }`}
             >
               {prop.name}
