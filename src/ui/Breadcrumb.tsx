6e29d7a3d869e7f85bd7ef5abf8ace75085ae7ca
Sandro <vipsandro11@gmail.com>
2024-04-17 14:38:51 +0400

    update Breadcrumb.tsx

--- src/ui/Breadcrumb.tsx
+++ src/ui/Breadcrumb.tsx
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 
 type BreadcrumbTypes = {
   category: string;
+  classname?: string;
 };
 
-const Breadcrumb = ({ category }: BreadcrumbTypes) => {
+const Breadcrumb = ({ category, classname }: BreadcrumbTypes) => {
   return (
-    <div className="flex text-[14px]">
+    <div className={`flex text-[14px] ${classname}`}>
       <Link
         to="/"
         className="hover:text-black transition-colors ease-in duration-150 text-primaryGray"
