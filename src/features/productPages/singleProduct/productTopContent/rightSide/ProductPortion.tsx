a123a04893a53cc969c6f5bb10ff65af414dc0b0
Sandro <vipsandro11@gmail.com>
2024-04-17 23:24:11 +0400

    update ProductPortion.tsx

--- src/features/productPages/singleProduct/productTopContent/rightSide/ProductPortion.tsx
+++ src/features/productPages/singleProduct/productTopContent/rightSide/ProductPortion.tsx
@@ -3,7 +3,7 @@ type ProductPortion = {
 };
 const ProductPortion = ({ portion }: ProductPortion) => {
   return (
-    <div className="flex justify-between text-[14px] border-b py-2">
+    <div className="flex justify-between text-[14px] border-b border-dashed py-2">
       <p className="text-black font-semibold">პორცია</p>
       <p>{portion}</p>
     </div>
