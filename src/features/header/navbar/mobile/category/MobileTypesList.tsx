94278ab5d19320d2bdd235f6ed4f65c73c2302df
Sandro <vipsandro11@gmail.com>
2024-04-17 21:50:48 +0400

    update MobileTypesList.tsx

--- src/features/header/navbar/mobile/category/MobileTypesList.tsx
+++ src/features/header/navbar/mobile/category/MobileTypesList.tsx
@@ -3,19 +3,26 @@ import SingleCategoryDropdown from "./SingleCategoryDropdown";
 type MobileTypesListTypes = {
   isTypeOpen: boolean;
   categoryTypes: string[] | undefined;
+  setOpen: (isOpen: boolean) => void;
 };
 
 const MobileTypesList = ({
   isTypeOpen,
   categoryTypes = [],
+  setOpen,
 }: MobileTypesListTypes) => {
   const dynamicHeight = isTypeOpen
     ? { height: `${categoryTypes?.length * 3}rem` }
     : { height: "0" };
 
+  const handleCloseNav = () => {
+    setOpen(false);
+  };
+
   return (
     <div
       style={dynamicHeight}
+      onClick={handleCloseNav}
       className="overflow-hidden transition-height ease-in duration-300"
     >
       {categoryTypes?.map((type, index) => {
