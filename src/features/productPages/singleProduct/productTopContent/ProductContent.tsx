2c42dbaef56ff626e2834052749d360ca652b509
Sandro <vipsandro11@gmail.com>
2024-04-17 23:16:34 +0400

    refactor: Refactor ProductContent component to use Redux for managing selected product ID

--- src/features/productPages/singleProduct/productTopContent/ProductContent.tsx
+++ src/features/productPages/singleProduct/productTopContent/ProductContent.tsx
@@ -2,18 +2,31 @@ import { useLocation } from "react-router-dom";
 import { useAppSelector } from "../../../../hooks/hooks";
 import ProductImages from "./leftSide/ProductImages";
 import ProductSummary from "./rightSide/ProductSummary";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setSelectedProductId } from "../../../slices/productsSlice";
 
 const ProductContent = () => {
   const { state } = useLocation();
-
-  const { productData } = useAppSelector((store) => store.product);
-  const singleProdData = productData?.find(
-    (product) => product.id === state.id
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const { productData, selectedProductId } = useAppSelector(
+    (store) => store.product
   );
+  const singleProdData = productData?.find(
+    (product) => product.id === selectedProductId
+  );
+
+  useEffect(() => {
+    if (state && state.id) {
+      dispatch(setSelectedProductId(location.state.id));
+    }
+  }, [dispatch, location.state]);
+
   if (!singleProdData) return null;
   return (
     <article className="section-center section-x">
-      <div className="grid grid-cols-2 py-10 gap-7">
+      <div className="grid grid-cols-1 lg:grid-cols-2 py-10 gap-7">
         <ProductImages
           images={singleProdData.images || []}
           name={singleProdData.name}
