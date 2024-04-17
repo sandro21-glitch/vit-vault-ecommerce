7074156d3ef64c133d98d1ab826e5dd39816dcef
Sandro <vipsandro11@gmail.com>
2024-04-17 23:16:06 +0400

    update Property.tsx

--- src/features/productPages/singleProduct/productTopContent/rightSide/Property.tsx
+++ src/features/productPages/singleProduct/productTopContent/rightSide/Property.tsx
@@ -4,7 +4,7 @@ type PropertyTypes = {
 
 const Property = ({ property }: PropertyTypes) => {
   return (
-    <div className="flex justify-between items-center mt-5">
+    <div className="flex justify-between items-center mt-5 border-b">
       <div className="text-[14px] mr-5">აირჩიეთ პარამეტრი</div>
       <select className="border-2 flex-1 py-2 text-[14px] cursor-pointer outline-none">
         {property?.map((prop, index) => {
