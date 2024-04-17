d1f42b6bfd1f4cda7ef6e4feda9515b9d8b054fd
Sandro <vipsandro11@gmail.com>
2024-04-17 21:32:42 +0400

    feat: create AddToWhishlist.tsx

---
+++ src/features/productPages/singleProduct/productTopContent/rightSide/AddToWhishlist.tsx
@@ -0,0 +1,9 @@
+const AddToWhishlist = () => {
+  return (
+    <button className="text-secondaryGreen mt-5 text-[14px] ">
+      სურვილების სიაში დამატება
+    </button>
+  );
+};
+
+export default AddToWhishlist;
