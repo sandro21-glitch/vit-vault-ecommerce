57eb39c767238449b350ba1c63911ceb2ce30c78
Sandro <vipsandro11@gmail.com>
2024-04-18 21:08:41 +0400

    feat: create ProductUsage.tsx

---
+++ src/features/productPages/singleProduct/productDetails/use/ProductUsage.tsx
@@ -0,0 +1,16 @@
+type ProdUsageTypes = {
+  use: string | null;
+};
+
+const ProductUsage = ({ use }: ProdUsageTypes) => {
+  if (!use) return null;
+
+  return (
+    <div className="mb-5">
+      <p className="font-semibold text-black">მიღების წესი და დოზირება:</p>
+      <p className="text-[14px] ">{use}</p>
+    </div>
+  );
+};
+
+export default ProductUsage;
