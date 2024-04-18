5ae383caa4f5623af7f2e4c3b5e533775f18e00c
Sandro <vipsandro11@gmail.com>
2024-04-18 23:46:30 +0400

    update ProductDescription.tsx

--- src/features/productPages/singleProduct/productDetails/productDescription/ProductDescription.tsx
+++ src/features/productPages/singleProduct/productDetails/productDescription/ProductDescription.tsx
@@ -1,53 +1,11 @@
-import React from "react";
-
 type DescriptionTypes = {
   desc: string;
 };
 
 const ProductDescription = ({ desc }: DescriptionTypes) => {
-  let lineBreakOccurred = false;
-
-  const wrappedDesc = desc.split(" - ").map((part, index) => {
-    const parts = part.split("–");
-
-    const isFirstPartAfterLineBreak = lineBreakOccurred && index > 0;
-
-    if (!lineBreakOccurred && index > 0) {
-      lineBreakOccurred = true;
-    }
-
-    return (
-      <React.Fragment key={index}>
-        {isFirstPartAfterLineBreak && (
-          <>
-            <br />
-            <br />
-            {parts.map((subPart, subIndex) => (
-              <React.Fragment key={subIndex}>
-                {subIndex === 0 && <strong>{subPart.split(" ")[0]}</strong>}
-                {subIndex === 0 && " " + subPart.split(" ").slice(1).join(" ")}
-                {subIndex > 0 && <> – {subPart}</>}
-              </React.Fragment>
-            ))}
-          </>
-        )}
-        {!isFirstPartAfterLineBreak &&
-          parts.map((subPart, subIndex) => (
-            <React.Fragment key={subIndex}>
-              {subIndex === 0 && parts.length > 1 && (
-                <strong>{subPart.split(" ")[0]}</strong>
-              )}
-              {subIndex === 0 && parts.length === 1 && subPart}
-              {subIndex > 0 && <> – {subPart}</>}
-            </React.Fragment>
-          ))}
-      </React.Fragment>
-    );
-  });
-
   return (
     <div className="mb-5">
-      <p className="text-[14px]">{wrappedDesc}</p>
+      <p className="text-[14px]">{desc}</p>
     </div>
   );
 };
