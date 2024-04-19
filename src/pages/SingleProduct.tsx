bb96bde12b5fa85f2a11e77dd060425a0749c390
Sandro <vipsandro11@gmail.com>
2024-04-19 22:54:57 +0400

    update SingleProduct.tsx

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -1,11 +1,13 @@
 import ArticleDetails from "../features/productPages/singleProduct/article/ArticleDetails";
 import ProductContent from "../features/productPages/singleProduct/productTopContent/ProductContent";
+import RelatedProducts from "../features/productPages/singleProduct/relatedProduct/RelatedProducts";
 
 const SingleProduct = () => {
   return (
     <section>
       <ProductContent />
       <ArticleDetails />
+      <RelatedProducts />
     </section>
   );
 };
