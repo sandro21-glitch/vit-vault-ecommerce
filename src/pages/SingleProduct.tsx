ba18416582199edb449a124d6209e440f4866ad3
Sandro <vipsandro11@gmail.com>
2024-04-19 14:33:52 +0400

    feat: create SingleProduct.tsx

---
+++ src/pages/SingleProduct.tsx
@@ -0,0 +1,13 @@
+import ArticleDetails from "../features/productPages/singleProduct/article/ArticleDetails";
+import ProductContent from "../features/productPages/singleProduct/productTopContent/ProductContent";
+
+const SingleProduct = () => {
+  return (
+    <section>
+      <ProductContent />
+      <ArticleDetails />
+    </section>
+  );
+};
+
+export default SingleProduct;
