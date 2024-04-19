3e5234fa740bb2386c5da1f4e0147862fc2ae796
Sandro <vipsandro11@gmail.com>
2024-04-19 14:51:32 +0400

    update ArticleDetails.tsx

--- src/features/productPages/singleProduct/article/ArticleDetails.tsx
+++ src/features/productPages/singleProduct/article/ArticleDetails.tsx
@@ -1,8 +1,32 @@
-const ArticleDetails = () => {
-  return <article className="section-center section-x">
+import { useAppSelector } from "../../../../hooks/hooks";
+import Links from "./Links";
 
-    f
-  </article>;
+const ArticleDetails = () => {
+  const { productData, selectedProductId } = useAppSelector(
+    (store) => store.product
+  );
+  const singleProdData = productData?.find(
+    (product) => product.id === selectedProductId
+  );
+
+  return (
+    <article className="bg-veryLightGray py-[15px] mb-[40px]">
+      <div className="section-center section-x">
+        <ul className="flex justify-evenly flex-col md:flex-row">
+          <li className="text-[14px]">
+            <strong>კატეგორია:</strong> {singleProdData?.category},{" "}
+            {singleProdData?.type}
+          </li>
+          <li className="text-[14px]">
+            <strong>პროდუქტი:</strong> {singleProdData?.name}
+          </li>
+          <li className="text-[14px] flex">
+            <strong>გაზიარება:</strong> <Links />
+          </li>
+        </ul>
+      </div>
+    </article>
+  );
 };
 
 export default ArticleDetails;
