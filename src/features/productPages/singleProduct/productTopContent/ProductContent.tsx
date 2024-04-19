01cb55b678172bd76e1d099b54f71cd67b532794
Sandro <vipsandro11@gmail.com>
2024-04-19 14:34:00 +0400

    update ProductContent.tsx

--- src/features/productPages/singleProduct/productTopContent/ProductContent.tsx
+++ src/features/productPages/singleProduct/productTopContent/ProductContent.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useAppSelector } from "../../../../hooks/hooks";
 import ProductImages from "./leftSide/ProductImages";
 import ProductSummary from "./rightSide/ProductSummary";
@@ -8,6 +8,7 @@ import { setSelectedProductId } from "../../../slices/productsSlice";
 import ProductDetails from "../productDetails/ProductDetails";
 
 const ProductContent = () => {
+  const navigate = useNavigate();
   const { state } = useLocation();
   const dispatch = useDispatch();
   const location = useLocation();
@@ -21,13 +22,18 @@ const ProductContent = () => {
   useEffect(() => {
     if (state && state.id) {
       dispatch(setSelectedProductId(location.state.id));
+    } else {
+      navigate("/");
     }
   }, [dispatch, location.state]);
 
   if (!singleProdData) return null;
   return (
-    <article className="section-center section-x">
-      <div className="grid grid-cols-1 lg:grid-cols-2 py-10 gap-5">
+    <article
+      className="section-center section-x"
+      style={{ marginBottom: "40px", marginTop: "50px" }}
+    >
+      <div className="grid grid-cols-1 lg:grid-cols-2 pb-10 gap-5">
         <ProductImages
           images={singleProdData.images || []}
           name={singleProdData.name}
