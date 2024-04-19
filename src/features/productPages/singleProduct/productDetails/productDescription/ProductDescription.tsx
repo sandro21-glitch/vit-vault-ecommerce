561cee22639c0f967de5048b8512e0f5cd23af00
Sandro <vipsandro11@gmail.com>
2024-04-19 15:24:23 +0400

    refactor: :sparkles: Enhance ProductDescription component to format description
    - Split the description into parts using regex to insert line breaks - Each part is rendered with double line breaks for improved readability

--- src/features/productPages/singleProduct/productDetails/productDescription/ProductDescription.tsx
+++ src/features/productPages/singleProduct/productDetails/productDescription/ProductDescription.tsx
@@ -3,9 +3,25 @@ type DescriptionTypes = {
 };
 
 const ProductDescription = ({ desc }: DescriptionTypes) => {
+  const pattern = / - /g;
+  const parts = desc.split(pattern);
+
+  const formattedParts = parts.map((part, index) => {
+    if (index === 0 || !part.trim()) {
+      return `${part} `;
+    }
+    return (
+      <>
+        {part}
+        <br />
+        <br />
+      </>
+    );
+  });
+
   return (
     <div className="mb-5">
-      <p className="text-[14px]">{desc}</p>
+      <p className="text-[14px]">{formattedParts}</p>
     </div>
   );
 };
