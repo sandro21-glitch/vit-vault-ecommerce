df68577dce49b36bd187725b4499644325abf05e
Sandro <vipsandro11@gmail.com>
2024-04-19 14:34:05 +0400

    feat: create ProductTabs.tsx

---
+++ src/features/productPages/singleProduct/productDetails/tabs/ProductTabs.tsx
@@ -0,0 +1,17 @@
+const ProductTabs = () => {
+  return (
+    <div
+      className="  w-full
+   flex justify-center items-center absolute top-0 left-0 right-0"
+    >
+      <p
+        className=" 
+   text-center text-black font-semibold text-[14px] border-t-secondaryGreen border-t-2"
+      >
+        აღწერა
+      </p>
+    </div>
+  );
+};
+
+export default ProductTabs;
