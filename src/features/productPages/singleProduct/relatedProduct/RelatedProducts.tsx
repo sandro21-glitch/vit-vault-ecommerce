1926a5e1c193ee864966c569d9bd6c4d1847f32b
Sandro <vipsandro11@gmail.com>
2024-04-20 22:00:54 +0400

    update RelatedProducts.tsx

--- src/features/productPages/singleProduct/relatedProduct/RelatedProducts.tsx
+++ src/features/productPages/singleProduct/relatedProduct/RelatedProducts.tsx
@@ -6,7 +6,9 @@ const RelatedProducts = () => {
       className="section-center section-x"
       style={{ marginBottom: "40px", marginTop: "50px" }}
     >
-      <h6 className="font-semibold mb-5 border-b-2 border-secondaryGreen w-fit">შესაძლოა, ასევე მოგეწონოთ…</h6>
+      <h6 className="font-semibold text-[14px] mb-5 border-b-2 border-secondaryGreen w-fit">
+        შესაძლოა, ასევე მოგეწონოთ…
+      </h6>
       <RelatedProductList />
     </article>
   );
