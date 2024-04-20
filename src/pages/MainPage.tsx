5ff2fb2c4201d929f7b7b51c00df6afc97fdc283
Sandro <vipsandro11@gmail.com>
2024-04-20 19:31:55 +0400

    update MainPage.tsx

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,10 @@
 import DiscountedProducts from "../features/mainPage/discountedProducts/DiscountedProducts";
 import PopularCategories from "../features/mainPage/popularCategory/PopularCategories";
 import HomeSlider from "../features/mainPage/sliderPage/HomeSlider";
+import useScrollToTopOnPageChange from "../hooks/useScrollToTopOnPageChange";
 
 const MainPage = () => {
+  useScrollToTopOnPageChange();
   return (
     <section className="section-center section-x">
       <HomeSlider />
