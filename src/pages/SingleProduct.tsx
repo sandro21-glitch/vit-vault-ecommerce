f8c5cb0250b3e40db1301f6532614d58b9649c32
Sandro <vipsandro11@gmail.com>
2024-04-20 19:32:05 +0400

    update SingleProduct.tsx

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -1,8 +1,10 @@
 import ArticleDetails from "../features/productPages/singleProduct/article/ArticleDetails";
 import ProductContent from "../features/productPages/singleProduct/productTopContent/ProductContent";
 import RelatedProducts from "../features/productPages/singleProduct/relatedProduct/RelatedProducts";
+import useScrollToTopOnPageChange from "../hooks/useScrollToTopOnPageChange";
 
 const SingleProduct = () => {
+  useScrollToTopOnPageChange();
   return (
     <section>
       <ProductContent />
