4310c81960959bfe9f88a450843880592602eae1
Sandro <vipsandro11@gmail.com>
2024-04-21 23:24:23 +0400

    feat: create SearchProduct.tsx

---
+++ src/features/header/general/search/SearchProduct.tsx
@@ -0,0 +1,38 @@
+import { FormEvent, useState } from "react";
+import { CiSearch } from "react-icons/ci";
+import { useAppDispatch } from "../../../../hooks/hooks";
+import { setFilteredProducts } from "../../../slices/productsSlice";
+
+const SearchProduct = () => {
+  const [search, setSearch] = useState("");
+  const dispatch = useAppDispatch();
+
+  const handleSearchProduct = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    dispatch(setFilteredProducts(search));
+  };
+
+  return (
+    <div className="flex-1">
+      <form className="relative" onSubmit={handleSearchProduct}>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="მოძებნე პროდუქცია"
+          className="min-w-[290px] w-full xl:border-2 border-b outline-none p-5 h-[65px] xl:h-[46px]"
+        />
+        <button
+          type="submit"
+          className="absolute right-0 bottom-0 top-0 w-[50px] bg-transparent text-[#777] flex-center"
+        >
+          <CiSearch size={25} />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchProduct;
