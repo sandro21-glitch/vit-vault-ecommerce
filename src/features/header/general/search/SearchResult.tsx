9f5a4e9a0d27cc3d2c2bab9569c44c06396663df
Sandro <vipsandro11@gmail.com>
2024-04-21 23:56:17 +0400

    update SearchResult.tsx

--- src/features/header/general/search/SearchResult.tsx
+++ src/features/header/general/search/SearchResult.tsx
@@ -1,14 +1,34 @@
 import { useAppSelector } from "../../../../hooks/hooks";
+import CheckDiscount from "../../../../ui/CheckDiscount";
 
 const SearchResult = () => {
   const { filteredProducts } = useAppSelector((store) => store.product);
-  if (!filteredProducts || filteredProducts.length < 1)
-    return <p>NO PRODUCT FOUND</p>;
+  if (!filteredProducts || filteredProducts.length < 1) return null;
   return (
-    <div className="absolute top-full left-0 right-0 bg-red-900 z-[999999]">
-      <ul>
-        {filteredProducts?.map((data,index) => {
-          return <li key={index}>{data.name}</li>;
+    <div className="absolute top-full left-0 right-0 bg-white shadow-mainShadow z-[999999]">
+      <ul className="grid grid-cols-2 max-h-[50vh] overflow-y-scroll">
+        {filteredProducts?.map((data, index) => {
+          const { name, images, discount, price } = data;
+          return (
+            <li
+              key={index}
+              className="w-full flex items-center border p-5 cursor-pointer"
+            >
+              <div className="flex justify-center items-center">
+                <div className="w-[65px] h-[65px] overflow-hidden">
+                  <img
+                    src={images[0]}
+                    alt={name}
+                    className="w-full h-full object-contain"
+                  />
+                </div>
+              </div>
+              <div className="">
+                <h6>{name}</h6>
+                <CheckDiscount discount={discount} price={price} />
+              </div>
+            </li>
+          );
         })}
       </ul>
     </div>
