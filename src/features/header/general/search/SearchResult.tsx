bc1c32f1ecd9d6218d458b82d6b03149f5978385
Sandro <vipsandro11@gmail.com>
2024-04-22 20:31:19 +0400

    update SearchResult.tsx

--- src/features/header/general/search/SearchResult.tsx
+++ src/features/header/general/search/SearchResult.tsx
@@ -1,32 +1,40 @@
-import { useAppSelector } from "../../../../hooks/hooks";
+import { Link } from "react-router-dom";
+import { Product } from "../../../../types/ProductTypes";
 import CheckDiscount from "../../../../ui/CheckDiscount";
+import { formatPath } from "../../../../utils/formatPath";
 
-const SearchResult = () => {
-  const { filteredProducts } = useAppSelector((store) => store.product);
-  if (!filteredProducts || filteredProducts.length < 1) return null;
+type SearchResultTypes = {
+  localData: Product[] | null;
+};
+
+const SearchResult = ({ localData }: SearchResultTypes) => {
+  if (!localData || localData.length < 1) return null;
   return (
     <div className="absolute top-full left-0 right-0 bg-white shadow-mainShadow z-[999999]">
       <ul className="grid grid-cols-2 max-h-[50vh] overflow-y-scroll">
-        {filteredProducts?.map((data, index) => {
-          const { name, images, discount, price } = data;
+        {localData?.map((data, index) => {
+          const { name, images, discount, price, id } = data;
           return (
-            <li
-              key={index}
-              className="w-full flex items-center border p-5 cursor-pointer"
-            >
-              <div className="flex justify-center items-center">
-                <div className="w-[65px] h-[65px] overflow-hidden">
-                  <img
-                    src={images[0]}
-                    alt={name}
-                    className="w-full h-full object-contain"
-                  />
+            <li key={index}>
+              <Link
+                 to={`/product/${formatPath(name)}`}
+                state={{ id }}
+                className="w-full flex items-center border p-5 cursor-pointer"
+              >
+                <div className="flex justify-center items-center">
+                  <div className="w-[65px] h-[65px] overflow-hidden">
+                    <img
+                      src={images[0]}
+                      alt={name}
+                      className="w-full h-full object-contain"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="">
-                <h6>{name}</h6>
-                <CheckDiscount discount={discount} price={price} />
-              </div>
+                <div className="ml-3">
+                  <h6>{name}</h6>
+                  <CheckDiscount discount={discount} price={price} />
+                </div>
+              </Link>
             </li>
           );
         })}
