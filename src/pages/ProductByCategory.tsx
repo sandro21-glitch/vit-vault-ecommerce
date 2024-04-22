2faa91488785c4d85bee87ac7a4dc30880ba911d
Sandro <vipsandro11@gmail.com>
2024-04-22 14:27:42 +0400

    update ProductByCategory.tsx

--- src/pages/ProductByCategory.tsx
+++ src/pages/ProductByCategory.tsx
@@ -1,36 +1,11 @@
-import { useState } from "react";
-import { useAppSelector } from "../hooks/hooks";
-import { useParams } from "react-router-dom";
-import SectionHeader from "../ui/SectionHeader";
 import ProductsByCategoryList from "../features/productPages/productsByCategory/ProductsByCategoryList";
-import ProductsFilter from "../features/productPages/productsByCategory/ProductsFilter";
-import ListHeader from "../features/productPages/productsByCategory/ListHeader";
-import useScrollToTopOnPageChange from "../hooks/useScrollToTopOnPageChange";
+import ProductsPage from "../ui/ProductsPage";
 
 const ProductByCategory = () => {
-  useScrollToTopOnPageChange();
-  const { productData } = useAppSelector((store) => store.product);
-  const prices = productData?.map((product) => product.price) ?? [];
-
-  // find the maximum price
-  const maxPrice = Math.max(...prices);
-  const [filterPrice, setFilterPrice] = useState(maxPrice);
-
-  const { category } = useParams();
   return (
-    <section>
-      <SectionHeader path={category ? category : ""} />
-      <article className="section-x section-center flex flex-col-reverse lg:flex-row">
-        <ProductsFilter
-          filterPrice={filterPrice}
-          setFilterPrice={setFilterPrice}
-        />
-        <div className="w-full lg:max-w-[75%]">
-          <ListHeader category={category} />
-          <ProductsByCategoryList filterPrice={filterPrice} />
-        </div>
-      </article>
-    </section>
+    <ProductsPage>
+      <ProductsByCategoryList />
+    </ProductsPage>
   );
 };
 
