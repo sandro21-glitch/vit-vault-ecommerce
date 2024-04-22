c4c35a6e14276f668df741a5b6b78c3e08575b77
Sandro <vipsandro11@gmail.com>
2024-04-22 20:36:05 +0400

    update SearchResult.tsx

--- src/features/header/general/search/SearchResult.tsx
+++ src/features/header/general/search/SearchResult.tsx
@@ -1,7 +1,5 @@
-import { Link } from "react-router-dom";
 import { Product } from "../../../../types/ProductTypes";
-import CheckDiscount from "../../../../ui/CheckDiscount";
-import { formatPath } from "../../../../utils/formatPath";
+import SingelResult from "./SingelResult";
 
 type SearchResultTypes = {
   localData: Product[] | null;
@@ -13,30 +11,7 @@ const SearchResult = ({ localData }: SearchResultTypes) => {
     <div className="absolute top-full left-0 right-0 bg-white shadow-mainShadow z-[999999]">
       <ul className="grid grid-cols-2 max-h-[50vh] overflow-y-scroll">
         {localData?.map((data, index) => {
-          const { name, images, discount, price, id } = data;
-          return (
-            <li key={index}>
-              <Link
-                 to={`/product/${formatPath(name)}`}
-                state={{ id }}
-                className="w-full flex items-center border p-5 cursor-pointer"
-              >
-                <div className="flex justify-center items-center">
-                  <div className="w-[65px] h-[65px] overflow-hidden">
-                    <img
-                      src={images[0]}
-                      alt={name}
-                      className="w-full h-full object-contain"
-                    />
-                  </div>
-                </div>
-                <div className="ml-3">
-                  <h6>{name}</h6>
-                  <CheckDiscount discount={discount} price={price} />
-                </div>
-              </Link>
-            </li>
-          );
+          return <SingelResult key={index} data={data} />;
         })}
       </ul>
     </div>
