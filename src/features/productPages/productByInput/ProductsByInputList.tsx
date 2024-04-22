ed9159b0ec9677487946c7dc0848e56d9ea649d7
Sandro <vipsandro11@gmail.com>
2024-04-22 22:22:00 +0400

    feat: create ProductsByInputList.tsx

---
+++ src/features/productPages/productByInput/ProductsByInputList.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { useAppSelector } from "../../../hooks/hooks";
+import ProductCard from "../../../ui/ProductCard";
+import CustomPagination from "../../../ui/CustomPagination";
+
+const ProductsByInputList = () => {
+  const { productsPerPage } = useAppSelector((store) => store.filters);
+  const { filteredProducts } = useAppSelector((store) => store.product);
+  const [currentPage, setCurrentPage] = useState(1);
+    
+  // handle page change
+  const handlePageChange = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  if (!filteredProducts || filteredProducts.length < 1) {
+    return (
+      <p className="text-[1.5rem] h-full flex items-center justify-center">
+        პროდუქტები ვერ მოიძებნა
+      </p>
+    );
+  }
+
+  // calculate index range for current page
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+
+  // get current products for the current page
+  const currentProducts = filteredProducts.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+  return (
+    <div>
+      <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-10">
+        {currentProducts.map((categoryItem, index) => (
+          <ProductCard key={index} categoryItem={categoryItem} />
+        ))}
+      </ul>
+      <CustomPagination
+        currentPage={currentPage}
+        totalPages={Math.ceil(filteredProducts.length / productsPerPage)}
+        onPageChange={handlePageChange}
+      />
+    </div>
+  );
+};
+
+export default ProductsByInputList;
