90c3f7c5d02069eafebd4c6b364ed04dfd2ca70b
Sandro <vipsandro11@gmail.com>
2024-04-22 14:28:46 +0400

    feat: :sparkles: Implement ProductsPage component for managing product display
    - Created ProductsPage component to handle product display layout and functionality - Utilized SectionHeader, ProductsFilter, and ListHeader components for header and filtering - Implemented dynamic setting of filterPrice using useEffect and setFilterPriceLocal state - Used useScrollToTopOnPageChange hook for scrolling to top on page change

---
+++ src/ui/ProductsPage.tsx
@@ -0,0 +1,45 @@
+import { ReactNode, useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks/hooks";
+import useScrollToTopOnPageChange from "../hooks/useScrollToTopOnPageChange";
+import { useParams } from "react-router-dom";
+import SectionHeader from "./SectionHeader";
+import ProductsFilter from "../features/productPages/productsByCategory/ProductsFilter";
+import ListHeader from "../features/productPages/productsByCategory/ListHeader";
+import { setFilterPrice } from "../features/slices/filtersSlice";
+
+type ProductsPageTypes = {
+  children: ReactNode;
+};
+
+const ProductsPage = ({ children }: ProductsPageTypes) => {
+  useScrollToTopOnPageChange();
+  const { productData } = useAppSelector((store) => store.product);
+  const prices = productData?.map((product) => product.price) ?? [];
+  const dispatch = useAppDispatch();
+  const maxPrice = Math.max(...prices);
+  const [filterPrice, setFilterPriceLocal] = useState(maxPrice);
+
+  const { category } = useParams();
+  
+  useEffect(() => {
+    dispatch(setFilterPrice(filterPrice));
+  }, [filterPrice, dispatch]);
+
+  return (
+    <section>
+      <SectionHeader path={category ? category : "პროდუქტები"} />
+      <article className="section-x section-center flex flex-col-reverse lg:flex-row">
+        <ProductsFilter
+          filterPrice={filterPrice}
+          setFilterPrice={setFilterPriceLocal}
+        />
+        <div className="w-full lg:max-w-[75%]">
+          <ListHeader category={category} />
+          {children}
+        </div>
+      </article>
+    </section>
+  );
+};
+
+export default ProductsPage;
