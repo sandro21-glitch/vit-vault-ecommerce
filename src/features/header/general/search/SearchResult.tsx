9414e960db6691df279fc61732d266e525bbc0b4
Sandro <vipsandro11@gmail.com>
2024-04-22 20:39:46 +0400

    update SearchResult.tsx

--- src/features/header/general/search/SearchResult.tsx
+++ src/features/header/general/search/SearchResult.tsx
@@ -3,15 +3,28 @@ import SingelResult from "./SingelResult";
 
 type SearchResultTypes = {
   localData: Product[] | null;
+  setIsTyping: (typeing: boolean) => void;
+  setSearch: (search: string) => void;
 };
 
-const SearchResult = ({ localData }: SearchResultTypes) => {
+const SearchResult = ({
+  localData,
+  setIsTyping,
+  setSearch,
+}: SearchResultTypes) => {
   if (!localData || localData.length < 1) return null;
   return (
     <div className="absolute top-full left-0 right-0 bg-white shadow-mainShadow z-[999999]">
       <ul className="grid grid-cols-2 max-h-[50vh] overflow-y-scroll">
         {localData?.map((data, index) => {
-          return <SingelResult key={index} data={data} />;
+          return (
+            <SingelResult
+              key={index}
+              data={data}
+              setIsTyping={setIsTyping}
+              setSearch={setSearch}
+            />
+          );
         })}
       </ul>
     </div>
