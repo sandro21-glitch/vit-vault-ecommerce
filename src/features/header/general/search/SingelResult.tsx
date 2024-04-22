a8158e3383f292dcffec50e62a58ca529f51e041
Sandro <vipsandro11@gmail.com>
2024-04-22 20:36:08 +0400

    feat: create SingelResult.tsx

---
+++ src/features/header/general/search/SingelResult.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import { Product } from "../../../../types/ProductTypes";
+import { formatPath } from "../../../../utils/formatPath";
+import CheckDiscount from "../../../../ui/CheckDiscount";
+
+type SingleResultTypes = {
+  data: Product;
+};
+
+const SingelResult = ({ data }: SingleResultTypes) => {
+  const { name, images, discount, price, id } = data;
+
+  return (
+    <li>
+      <Link
+        to={`/product/${formatPath(name)}`}
+        state={{ id }}
+        className="w-full flex items-center border p-5 cursor-pointer"
+      >
+        <div className="flex justify-center items-center">
+          <div className="w-[65px] h-[65px] overflow-hidden">
+            <img
+              src={images[0]}
+              alt={name}
+              className="w-full h-full object-contain"
+            />
+          </div>
+        </div>
+        <div className="ml-3">
+          <h6>{name}</h6>
+          <CheckDiscount discount={discount} price={price} />
+        </div>
+      </Link>
+    </li>
+  );
+};
+
+export default SingelResult;
