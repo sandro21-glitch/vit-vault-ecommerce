06d53c179d019cb02ca101dd4a8cde927cee503d
Sandro <vipsandro11@gmail.com>
2024-04-23 19:35:27 +0400

    feat: create NoProducts.tsx

---
+++ src/ui/NoProducts.tsx
@@ -0,0 +1,11 @@
+
+
+const NoProducts = () => {
+  return (
+    <p className="text-[1.5rem] h-full text-center mt-20">
+      პროდუქტები ვერ მოიძებნა
+    </p>
+  );
+};
+
+export default NoProducts;
