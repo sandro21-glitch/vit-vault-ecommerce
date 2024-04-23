089af18c35bd5c9d15c13ac6b55ddb680cc83bec
Sandro <vipsandro11@gmail.com>
2024-04-23 21:56:43 +0400

    update SingleCharacteristic.tsx

--- src/features/productPages/singleProduct/productDetails/characteristics/SingleCharacteristic.tsx
+++ src/features/productPages/singleProduct/productDetails/characteristics/SingleCharacteristic.tsx
@@ -1,8 +1,19 @@
+import { RxDividerHorizontal } from "react-icons/rx";
+
 type SingleCharacteristicTypes = {
   char: string;
 };
+
 const SingleCharacteristic = ({ char }: SingleCharacteristicTypes) => {
-  return <li className="list-disc text-[14px]">{char}</li>;
+  return (
+    <li className="text-[14px] flex items-center gap-1">
+      <span>
+        <RxDividerHorizontal />
+      </span>
+
+      {char}
+    </li>
+  );
 };
 
 export default SingleCharacteristic;
