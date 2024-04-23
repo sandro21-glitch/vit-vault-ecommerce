d23428f88d7148e77bd286bd694f7b6d7812f1ac
Sandro <vipsandro11@gmail.com>
2024-04-23 22:07:55 +0400

    update homeSlider.ts

--- src/constants/homeSlider.ts
+++ src/constants/homeSlider.ts
@@ -1,25 +1,32 @@
-export const sliderContent = [
+export interface SliderTypes {
+  id: number;
+  name: string;
+  para: string;
+  image: string;
+  path: string;
+}
+[];
+
+export const sliderContent: SliderTypes[] = [
   {
     id: 1,
     name: "მცენარეული ექსტრაქტები",
     para: "100% ბუნებრივი წყაროდან მოპოვებული მცენარეული ექსტრაქტები",
     image: "https://vitamini.ge/storage/sliders/69/33%20(1)%20(3).jpg",
-    path: "/",
+    path: "https://realpharm.eu/en/",
   },
   {
     id: 2,
     name: "Applied Nutrition",
     para: "ბრიტანეთში შექმნილი მაღალი კლასის ბრენდი ექსკლუზიურად ვიტამინის ქსელში.",
     image: "https://vitamini.ge/storage/sliders/141/hl-33855914763.png",
-    path: "/",
+    path: "https://appliednutrition.uk/",
   },
   {
     id: 3,
     name: "დამზადებულია აშშ-ში",
     para: "ლეგენდარული, ამერიკული ბრენდის- Universal Nutrition-ის ოფიციალური წარმომადგენელი",
     image: "https://vitamini.ge/storage/sliders/126/universal.png",
-    path: "/",
+    path: "https://www.animalpak.com/pages/where-to-buy",
   },
 ];
-
-
