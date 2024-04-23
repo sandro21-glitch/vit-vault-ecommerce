a782e6bb8bf318933ee85c14bd609fa700cf6704
Sandro <vipsandro11@gmail.com>
2024-04-23 22:27:29 +0400

    update ProductCard.tsx

--- src/ui/ProductCard.tsx
+++ src/ui/ProductCard.tsx
@@ -17,11 +17,12 @@ const ProductCard: React.FC<ProductCardProps> = ({ categoryItem }) => {
   const { discount, price, id, images, inStock, name } = categoryItem;
 
   return (
-    <li key={id}>
+    <li key={id}
+     className="group flex flex-col items-center relative w-full h-[350px] bg-white hover:shadow-mainShadow hover:scale-105 transition-all ease-in duration-200 cursor-pointer">
       <Link
         to={`/product/${formatPath(name)}`}
         state={{ id }}
-        className="relative w-full h-[350px] flex flex-col items-center text-center bg-white hover:shadow-mainShadow hover:scale-105 transition-all ease-in duration-200 cursor-pointer"
+        className="flex flex-col items-center text-center"
       >
         <img
           src={images[0]}
@@ -30,13 +31,21 @@ const ProductCard: React.FC<ProductCardProps> = ({ categoryItem }) => {
           className="h-[200px] w-auto object-cover mt-10 mb-2"
         />
         <h6 className="text-[14px] px-10">{name}</h6>
-        <div>{CheckDiscount({ price, discount })}</div>
+        <div className="mb-1">{CheckDiscount({ price, discount })}</div>
         {!inStock && (
           <div className="absolute top-0 right-0">
             <img src={soldOutIcon} alt="sold out" className="w-[3rem] h-auto" />
           </div>
         )}
       </Link>
+      <button
+        type="button"
+        data-twe-ripple-init
+        data-twe-ripple-color="light"
+        className="hidden font-serif font-semibold group-hover:block rounded bg-secondaryGreen px-6 pb-2 pt-2.5 text-[12px] uppercase leading-normal text-white shadow-primary-3"
+      >
+        კალათაში დამატება
+      </button>
     </li>
   );
 };
