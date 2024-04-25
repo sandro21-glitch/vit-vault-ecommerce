8f89e81e95a36d104b72b889f4959cc16b656f9b
Sandro <vipsandro11@gmail.com>
2024-04-25 19:30:13 +0400

    update AddToCartBtn.tsx

--- src/ui/AddToCartBtn.tsx
+++ src/ui/AddToCartBtn.tsx
@@ -1,13 +1,19 @@
+import { FiShoppingCart } from "react-icons/fi";
 const AddToCartBtn = () => {
   return (
     <button
       type="button"
       data-twe-ripple-init
       data-twe-ripple-color="light"
-      className="font-serif group-hover:visible invisible hover:bg-primaryGreen transition-all ease-in duration-150 font-semibold bg-secondaryGreen
+      className="font-serif h-[2rem] overflow-hidden group/fade group-hover:visible group-hover:opacity-100 opacity-0 invisible hover:bg-primaryGreen transition-all ease-in duration-150 font-semibold bg-secondaryGreen
          px-[.5rem] py-[.4rem] min-w-[50%] text-[12px] uppercase leading-normal text-white"
     >
-      კალათაში დამატება
+      <div className="block group-hover/fade:translate-y-[-200%] transition-transform ease-in duration-200">
+        კალათაში დამატება
+      </div>
+      <div className="flex items-center justify-center translate-y-[200%] group-hover/fade:translate-y-[-100%] transition-transform ease-in duration-200">
+        <FiShoppingCart className="text-[1.2rem] font-bold" />
+      </div>
     </button>
   );
 };
