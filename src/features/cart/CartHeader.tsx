69f1b1fcb318d45e5e0b58eba2d9e3d0a9195a64
Sandro <vipsandro11@gmail.com>
2024-04-25 19:30:29 +0400

    feat: create CartHeader.tsx

---
+++ src/features/cart/CartHeader.tsx
@@ -0,0 +1,28 @@
+import { FaLongArrowAltRight } from "react-icons/fa";
+const CartHeader = () => {
+  return (
+    <header className=" bg-secondaryGreen mb-[40px]">
+      <div className=" section-center section-x">
+        <ul className="flex justify-center items-center py-[15px] text-[22px]">
+          <li className="flex items-center cursor-pointer text-white font-semibold">
+            საყიდლების კალათა
+            <span className="mx-5">
+              <FaLongArrowAltRight />
+            </span>
+          </li>
+          <li className="flex items-center cursor-pointer text-white font-semibold">
+            გადახდა
+            <span className="mx-5">
+              <FaLongArrowAltRight />
+            </span>
+          </li>
+          <li className="cursor-pointer text-white font-semibold">
+            გადახდის გვერდი
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default CartHeader;
