25358c5c133f4037b5fa8192e135117a43eaf7f7
Sandro <vipsandro11@gmail.com>
2024-04-25 22:17:54 +0400

    update Cart.tsx

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,14 @@
 import CartHeader from "../features/cart/CartHeader";
+import CartLayout from "../features/cart/CartLayout";
 
 const Cart = () => {
   return (
     <section>
       <CartHeader />
+      <div className="flex gap-5 section-center section-x ">
+        <CartLayout />
+        <div className="w-full max-w-[30%] bg-red-900">test</div>
+      </div>
     </section>
   );
 };
