33403dfc69ce2a3f82fb647c7fbd80a4de443028
Sandro <vipsandro11@gmail.com>
2024-04-25 22:18:00 +0400

    feat: create TableHeader.tsx

---
+++ src/features/cart/cartTable/TableHeader.tsx
@@ -0,0 +1,16 @@
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr className="text-left">
+        <th className="invisible w-[10px]">&nbsp</th>
+        <th className="invisible w-[40px]">&nbsp</th>
+        <th>პროდუქტი</th>
+        <th>ფასი</th>
+        <th>რაოდენობა</th>
+        <th>ჯამი</th>
+      </tr>
+    </thead>
+  );
+};
+
+export default TableHeader;
