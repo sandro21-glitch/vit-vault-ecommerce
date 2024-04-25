e968f10112150f770f42630b54b28290d4001770
Sandro <vipsandro11@gmail.com>
2024-04-25 22:17:51 +0400

    update CartHeader.tsx

--- src/features/cart/CartHeader.tsx
+++ src/features/cart/CartHeader.tsx
@@ -6,13 +6,13 @@ const CartHeader = () => {
         <ul className="flex justify-center items-center py-[15px] text-[22px]">
           <li className="flex items-center cursor-pointer text-white font-semibold">
             საყიდლების კალათა
-            <span className="mx-5">
+            <span className="mx-5 cursor-default">
               <FaLongArrowAltRight />
             </span>
           </li>
           <li className="flex items-center cursor-pointer text-white font-semibold">
             გადახდა
-            <span className="mx-5">
+            <span className="mx-5 cursor-default">
               <FaLongArrowAltRight />
             </span>
           </li>
