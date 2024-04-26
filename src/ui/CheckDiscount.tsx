6e3b6bbe7b43fc4153a6cf05eae76a6314161ada
Sandro <vipsandro11@gmail.com>
2024-04-26 19:18:48 +0400

    refactor: Update CheckDiscount component to support line break option

--- src/ui/CheckDiscount.tsx
+++ src/ui/CheckDiscount.tsx
@@ -4,13 +4,19 @@ type CheckDiscountTypes = {
   price: number;
   discount: number;
   classname?: string;
+  br?: boolean;
 };
 
-const CheckDiscount = ({ price, discount, classname }: CheckDiscountTypes) => {
+const CheckDiscount = ({
+  price,
+  discount,
+  classname,
+  br,
+}: CheckDiscountTypes) => {
   if (discount > 0) {
     const discountedPrice = price - (price * discount) / 100;
     return (
-      <div className="flex items-center gap-1">
+      <div className={`flex ${br ? "flex-col" : "gap-1"} items-center`}>
         <p className={`line-through text-primaryGray ${classname}`}>
           {formatToGeorgianLari(price)}
         </p>
