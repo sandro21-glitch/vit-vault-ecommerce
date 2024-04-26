0b3dd1061d5d84d059cef951423e3abc48ebcb2c
Sandro <vipsandro11@gmail.com>
2024-04-26 20:56:32 +0400

    update ProductCard.tsx

--- src/ui/ProductCard.tsx
+++ src/ui/ProductCard.tsx
@@ -44,7 +44,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ categoryItem }) => {
       </Link>
       <div>
         <div className="mb-1">{CheckDiscount({ price, discount })}</div>
-        <AddToCartBtn categoryItem={categoryItem} />
+        <AddToCartBtn name={name} id={id} />
       </div>
     </li>
   );
