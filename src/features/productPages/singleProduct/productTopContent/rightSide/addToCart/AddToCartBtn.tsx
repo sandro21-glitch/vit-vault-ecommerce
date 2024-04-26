30e8278447e8b42f05bb32dbd239297b3fdd922c
Sandro <vipsandro11@gmail.com>
2024-04-26 19:06:22 +0400

    update AddToCartBtn.tsx

--- src/features/productPages/singleProduct/productTopContent/rightSide/addToCart/AddToCartBtn.tsx
+++ src/features/productPages/singleProduct/productTopContent/rightSide/addToCart/AddToCartBtn.tsx
@@ -1,6 +1,41 @@
-const AddToCartBtn = () => {
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../../../../../hooks/hooks";
+import { addProductToCart } from "../../../../../slices/cartSlice";
+
+type AddToCartTypes = {
+  singleProdData: {
+    id: string;
+    name: string;
+    price: number;
+    discount: number;
+    images: string[];
+  };
+};
+
+const AddToCartBtn = ({ singleProdData }: AddToCartTypes) => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const newCartProduct = {
+    id: singleProdData.id,
+    name: singleProdData.name,
+    image: singleProdData.images[0],
+    price: singleProdData.price,
+    discount: singleProdData.discount,
+    quantity: 1,
+    totalPrice: singleProdData.price,
+  };
+
+  const handleAddProduct = () => {
+    dispatch(addProductToCart(newCartProduct));
+    navigate("/cart");
+  };
+
   return (
-    <button className="bg-secondaryGreen h-[42px] hover:bg-primaryGreen transition-all ease-in duration-150 px-[20px] text-[14px] text-white font-semibold">
+    <button
+      type="button"
+      onClick={handleAddProduct}
+      className="bg-secondaryGreen h-[42px] hover:bg-primaryGreen transition-all ease-in duration-150 px-[20px] text-[14px] text-white font-semibold"
+    >
       კალათაში დამატება
     </button>
   );
