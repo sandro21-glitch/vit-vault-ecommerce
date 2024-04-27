c988a686a100ccd0544e44b94110e0f8cfd370e7
Sandro <vipsandro11@gmail.com>
2024-04-27 21:06:22 +0400

    feat: create ClearCart.tsx

---
+++ src/features/cart/cartActions/ClearCart.tsx
@@ -0,0 +1,9 @@
+const ClearCart = () => {
+  return (
+    <button className="bg-veryLightGray hover:bg-gray-200 transition-colors ease-in duration-150 py-[12px] px-[20px] font-semibold text-black text-[13px] leading-[18px]">
+      კალათის განახლება
+    </button>
+  );
+};
+
+export default ClearCart;
