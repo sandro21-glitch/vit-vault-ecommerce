2d7eba41a9a4571d4541b422adcf9e2575e9cf50
Sandro <vipsandro11@gmail.com>
2024-04-27 23:21:36 +0400

    update CartTotal.tsx

--- src/features/cart/cartTotal/CartTotal.tsx
+++ src/features/cart/cartTotal/CartTotal.tsx
@@ -1,7 +1,9 @@
 const CartTotal = () => {
   return (
-    <div className="p-[25px] inline-block flex-1 max-w-[40%] w-full ml-7 text-[20px] border-2">
-      მთლიანი ღირებულება
+    <div className="w-full max-w-[40%]">
+      <div className="p-[25px] w-full text-[20px] border-2">
+        მთლიანი ღირებულება
+      </div>
     </div>
   );
 };
