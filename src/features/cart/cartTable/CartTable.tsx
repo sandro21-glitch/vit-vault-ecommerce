9bec3ae8ede1fa558255fe3c22dfc1f0df962ced
Sandro <vipsandro11@gmail.com>
2024-04-27 21:06:18 +0400

    update CartTable.tsx

--- src/features/cart/cartTable/CartTable.tsx
+++ src/features/cart/cartTable/CartTable.tsx
@@ -1,14 +1,17 @@
-import TableBody from "./TableBody"
-import TableHeader from "./TableHeader"
-
+import CartActions from "../cartActions/CartActions";
+import TableBody from "./TableBody";
+import TableHeader from "./TableHeader";
 
 const CartTable = () => {
   return (
-    <table className="w-full flex-1 text-[14px] border-b-2">
+    <div>
+      <table className="w-full flex-1 text-[14px] border-b-2 mb-[40px]">
         <TableHeader />
         <TableBody />
-    </table>
-  )
-}
+      </table>
+      <CartActions />
+    </div>
+  );
+};
 
-export default CartTable
+export default CartTable;
