79a38e21ef629d7bb54301ce06cb1d394df3c2ed
Sandro <vipsandro11@gmail.com>
2024-04-27 20:17:52 +0400

    refactor: Update RemoveCartItemBtn component to accept id prop and dispatch removeCartProduct action

--- src/features/cart/cartTable/RemoveCartItemBtn.tsx
+++ src/features/cart/cartTable/RemoveCartItemBtn.tsx
@@ -1,6 +1,19 @@
-const RemoveCartItemBtn = () => {
+import { useAppDispatch } from "../../../hooks/hooks";
+import { removeCartProduct } from "../../slices/cartSlice";
+
+type CartItemBtnTypes = {
+  id: string;
+};
+
+const RemoveCartItemBtn = ({ id }: CartItemBtnTypes) => {
+  const dispatch = useAppDispatch();
+  const handleRemoveItem = () => {
+    dispatch(removeCartProduct(id));
+  };
   return (
     <button
+      type="button"
+      onClick={handleRemoveItem}
       className="relative group w-[30px] h-[30px]
   before:absolute before:left-0 before:w-[14px] before:h-[2px] before:bg-black before:-rotate-45 before:hover:rotate-0
   before:transition-all before:ease-in before:duration-200
