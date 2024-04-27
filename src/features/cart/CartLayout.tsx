a70b60e60d607e88f4ecc02dff53029e17402d7b
Sandro <vipsandro11@gmail.com>
2024-04-27 23:21:14 +0400

    update CartLayout.tsx

--- src/features/cart/CartLayout.tsx
+++ src/features/cart/CartLayout.tsx
@@ -3,7 +3,7 @@ import CartTotal from "./cartTotal/CartTotal";
 
 const CartLayout = () => {
   return (
-    <div className="max-w-[75%] flex">
+    <div className="max-w-[75%] flex gap-7 min-h-screen">
       <CartTable />
       <CartTotal />
     </div>
