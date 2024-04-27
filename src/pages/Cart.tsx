34f97ab088fd88538d8c50c3c4f695f4741b1e66
Sandro <vipsandro11@gmail.com>
2024-04-27 21:43:35 +0400

    update Cart.tsx

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -5,9 +5,12 @@ const Cart = () => {
   return (
     <section>
       <CartHeader />
-      <div className="flex section-center section-x ">
+      <div
+        style={{ marginBottom: "40px" }}
+        className="flex section-center section-x"
+      >
         <CartLayout />
-        <div className="w-full max-w-[25%] bg-red-900 ml-7">test</div>
+        <div className="w-full bg-red-900 ml-7 flex-1">test</div>
       </div>
     </section>
   );
