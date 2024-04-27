ee76cbeb9db9a882858dcb92ead023f16b01fc04
Sandro <vipsandro11@gmail.com>
2024-04-27 20:52:05 +0400

    update LoadingSpinnerSmall.tsx

--- src/features/productPages/singleProduct/productTopContent/rightSide/addToCart/LoadingSpinnerSmall.tsx
+++ src/features/productPages/singleProduct/productTopContent/rightSide/addToCart/LoadingSpinnerSmall.tsx
@@ -3,7 +3,7 @@ const LoadingSpinnerSmall = () => {
     <div role="status" className="flex items-center justify-center">
       <svg
         aria-hidden="true"
-        className="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="w-5 h-5 text-gray-200 animate-spin fill-white"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
