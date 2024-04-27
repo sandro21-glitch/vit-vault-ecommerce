3efcbd0b05215b229a667e6ab21bf8ef51e1425f
Sandro <vipsandro11@gmail.com>
2024-04-27 20:52:00 +0400

    update TableBody.tsx

--- src/features/cart/cartTable/TableBody.tsx
+++ src/features/cart/cartTable/TableBody.tsx
@@ -3,8 +3,9 @@ import SingleCartProduct from "./SingleCartProduct";
 
 const TableBody = () => {
   const { cartProducts } = useAppSelector((store) => store.cart);
+  if (cartProducts.length < 1) return null;
   return (
-    <tbody className="w-full">
+    <tbody className="w-full ">
       {cartProducts.map((product) => {
         return <SingleCartProduct key={product.id} product={product} />;
       })}
