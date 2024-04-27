1993032f26298397a851527391e5379462dfc7ce
Sandro <vipsandro11@gmail.com>
2024-04-27 21:46:54 +0400

    update CartTable.tsx

--- src/features/cart/cartTable/CartTable.tsx
+++ src/features/cart/cartTable/CartTable.tsx
@@ -1,11 +1,13 @@
+import { useAppSelector } from "../../../hooks/hooks";
 import CartActions from "../cartActions/CartActions";
 import TableBody from "./TableBody";
 import TableHeader from "./TableHeader";
 
 const CartTable = () => {
+  const { cartProducts } = useAppSelector((store) => store.cart);
   return (
     <div className="flex-1 max-w-[60%]">
-      <table className="text-[14px] w-full border-b-2 mb-[40px]">
+      <table className={`text-[14px] w-full ${cartProducts.length < 1 ? 'border-none' : 'border-b-2 '} mb-[40px]`}>
         <TableHeader />
         <TableBody />
       </table>
