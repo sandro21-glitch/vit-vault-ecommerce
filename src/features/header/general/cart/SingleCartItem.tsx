b04a7d4057c203bd26f761292f87e4b07cb180e1
Sandro <vipsandro11@gmail.com>
2024-04-28 19:21:01 +0400

    update SingleCartItem.tsx

--- src/features/header/general/cart/SingleCartItem.tsx
+++ src/features/header/general/cart/SingleCartItem.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { CartProductTypes } from "../../../slices/cartSlice";
 import CheckDiscount from "../../../../ui/CheckDiscount";
 import RemoveCartItem from "./RemoveCartItem";
+import { formatPath } from "../../../../utils/formatPath";
 
 type SingleCartItemTypes = {
   item: CartProductTypes;
@@ -11,8 +12,8 @@ const SingleCartItem = ({ item }: SingleCartItemTypes) => {
   const { name, price, quantity, image, id } = item;
   return (
     <li className="py-3 border-b-2 flex relative">
-      <Link to={"/"} className="mr-2">
-        <img src={image} alt={name} className="min-w-[65px] max-w-[65px]" />
+      <Link to={`/product/${formatPath(name)}`} className="mr-2" state={{ id }}>
+        <img src={image} alt={name} className="min-w-[45px] max-w-[45px]" />
       </Link>
       <div className="flex flex-col">
         <h6 className="text-[14px]">{name}</h6>
