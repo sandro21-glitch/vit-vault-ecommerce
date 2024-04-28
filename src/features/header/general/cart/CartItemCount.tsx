827bd3c211013cb3f3c3dcfd761bd9cb163dd669
Sandro <vipsandro11@gmail.com>
2024-04-28 18:50:28 +0400

    update CartItemCount.tsx

--- src/features/header/general/cart/CartItemCount.tsx
+++ src/features/header/general/cart/CartItemCount.tsx
@@ -1,10 +1,14 @@
+import { useAppSelector } from "../../../../hooks/hooks";
+
 const CartItemCount = () => {
+  const { cartProducts } = useAppSelector((store) => store.cart);
+  const cartLenght = cartProducts.length;
   return (
     <div
       className="w-[1rem] h-[1rem] flex items-center justify-center
    rounded-full text-[.7rem] text-white bg-primaryGreen absolute left-5 top-0"
     >
-      1
+      {cartLenght}
     </div>
   );
 };
