2e38f321c7939e3fddfbfcb018805855b02fc6a6
Sandro <vipsandro11@gmail.com>
2024-04-28 18:47:13 +0400

    update CartModal.tsx

--- src/features/header/general/cart/CartModal.tsx
+++ src/features/header/general/cart/CartModal.tsx
@@ -1,8 +1,16 @@
+import { useAppSelector } from "../../../../hooks/hooks";
+import CartItemList from "./CartItemList";
+
 const CartModal = () => {
+  const { cartProducts } = useAppSelector((store) => store.cart);
   return (
     <div className="absolute top-[100%] py-5 right-0 w-[330px]  z-[9999] hidden group-hover:block">
       <div className="pt-[15px] bg-white shadow-mainShadow p-[20px]">
-        <p className="text-center">თქვენ კალათში პროდუქტები არ არის.</p>
+        {cartProducts.length < 1 ? (
+          <p className="text-center">თქვენ კალათში პროდუქტები არ არის.</p>
+        ) : (
+          <CartItemList cartProducts={cartProducts} />
+        )}
       </div>
     </div>
   );
