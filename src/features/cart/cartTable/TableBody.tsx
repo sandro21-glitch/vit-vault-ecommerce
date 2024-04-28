d0d5f7cfe5edb1ced5459c92a3d7f39899fc3d89
Sandro <vipsandro11@gmail.com>
2024-04-28 18:09:50 +0400

    update TableBody.tsx

--- src/features/cart/cartTable/TableBody.tsx
+++ src/features/cart/cartTable/TableBody.tsx
@@ -3,8 +3,7 @@ import SingleCartProduct from "./SingleCartProduct";
 
 const TableBody = () => {
   const { cartProducts } = useAppSelector((store) => store.cart);
-  if (cartProducts.length < 1)
-    return <div className="max-w-[65%] flex-1">test</div>;
+  if (cartProducts.length < 1) return null;
   return (
     <tbody>
       {cartProducts.map((product) => {
