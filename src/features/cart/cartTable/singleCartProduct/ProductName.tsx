a02a559a15d8f4ea4fdd5e90297b8ae7e1d25982
Sandro <vipsandro11@gmail.com>
2024-04-28 20:02:31 +0400

    feat: create ProductName.tsx

---
+++ src/features/cart/cartTable/singleCartProduct/ProductName.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { formatPath } from "../../../../utils/formatPath";
+
+type ProductNameTypes = {
+  id: string;
+  name: string;
+};
+const ProductName = ({ id, name }: ProductNameTypes) => {
+  return (
+    <td className="cursor-pointer">
+      <Link
+        to={`/product/${formatPath(name)}`}
+        state={{ id }}
+        className="hover:text-primaryGray transition-colors ease-in duration-150"
+      >
+        {name}
+      </Link>
+    </td>
+  );
+};
+
+export default ProductName;
