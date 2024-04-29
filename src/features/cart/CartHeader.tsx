b8b1cfd1188185d086ed3832e7cf2ee02083ca0e
Sandro <vipsandro11@gmail.com>
2024-04-29 14:42:18 +0400

    chore: :art: Updated CartHeader component to hide certain elements on smaller screen sizes for better responsiveness

--- src/features/cart/CartHeader.tsx
+++ src/features/cart/CartHeader.tsx
@@ -6,17 +6,17 @@ const CartHeader = () => {
         <ul className="flex justify-center items-center py-[15px] text-[22px]">
           <li className="flex items-center cursor-pointer text-white font-semibold">
             საყიდლების კალათა
-            <span className="mx-5 cursor-default">
+            <span className="mx-5 cursor-default hidden lg:inline">
               <FaLongArrowAltRight />
             </span>
           </li>
-          <li className="flex items-center cursor-pointer text-white font-semibold">
+          <li className="items-center cursor-pointer text-white font-semibold hidden lg:flex">
             გადახდა
             <span className="mx-5 cursor-default">
               <FaLongArrowAltRight />
             </span>
           </li>
-          <li className="cursor-pointer text-white font-semibold">
+          <li className="cursor-pointer text-white font-semibold hidden lg:block">
             გადახდის გვერდი
           </li>
         </ul>
