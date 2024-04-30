1220675aa0ee4a8c18c1e7a3f28565f5143570fd
Sandro <vipsandro11@gmail.com>
2024-04-30 19:16:03 +0400

    update Cart.tsx

--- src/features/header/general/Cart.tsx
+++ src/features/header/general/Cart.tsx
@@ -2,14 +2,17 @@ import { MdOutlineShoppingBag } from "react-icons/md";
 import CartOItemCount from "./cart/CartItemCount";
 import CartModal from "./cart/CartModal";
 import TotalCartSum from "./cart/TotalCartSum";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   return (
-    <li className="flex items-center cursor-pointer relative font-sans group">
-      <MdOutlineShoppingBag size={30} />
-      <TotalCartSum />
-      <CartOItemCount />
-      <CartModal />
+    <li className="cursor-pointer relative font-sans group">
+      <Link to="/cart" className="flex items-center">
+        <MdOutlineShoppingBag size={30} />
+        <TotalCartSum />
+        <CartOItemCount />
+        <CartModal />
+      </Link>
     </li>
   );
 };
