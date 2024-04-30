8fcd941446facfccf0c1f804bff9c458af6821f0
Sandro <vipsandro11@gmail.com>
2024-04-30 17:47:54 +0400

    update Cart.tsx

--- src/features/header/general/Cart.tsx
+++ src/features/header/general/Cart.tsx
@@ -1,16 +1,13 @@
 import { MdOutlineShoppingBag } from "react-icons/md";
 import CartOItemCount from "./cart/CartItemCount";
 import CartModal from "./cart/CartModal";
-import { formatToGeorgianLari } from "../../../utils/formatPrice";
-import { useAppSelector } from "../../../hooks/hooks";
+import TotalCartSum from "./cart/TotalCartSum";
 
 const Cart = () => {
-  const { totalSum } = useAppSelector((store) => store.cart);
-
   return (
     <li className="flex items-center cursor-pointer relative font-sans group">
       <MdOutlineShoppingBag size={30} />
-      <span className="ml-3 font-semibold text-[#333] text-[14px]">{formatToGeorgianLari(totalSum)}</span>
+      <TotalCartSum />
       <CartOItemCount />
       <CartModal />
     </li>
