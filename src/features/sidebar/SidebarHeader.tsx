227644004f6b7ba0564a64424266c0b86f9714e8
Sandro <vipsandro11@gmail.com>
2024-04-30 21:58:32 +0400

    feat: create SidebarHeader.tsx

---
+++ src/features/sidebar/SidebarHeader.tsx
@@ -0,0 +1,10 @@
+const SidebarHeader = () => {
+  return (
+    <header className="flex justify-between items-center px-4 py-5 border-b">
+      <h4 className="font-semibold leading-7">საყიდლების კალათა</h4>
+      <button className="text-[14px]">დახურვა</button>
+    </header>
+  );
+};
+
+export default SidebarHeader;
