b45a69aa0e755302201374442219e53c2a3e8f47
Sandro <vipsandro11@gmail.com>
2024-04-30 18:59:27 +0400

    update ProductCard.tsx

--- src/ui/ProductCard.tsx
+++ src/ui/ProductCard.tsx
@@ -21,7 +21,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ categoryItem }) => {
     <li
       key={id}
       className="group pt-2 pb-5 flex flex-col justify-between items-center relative w-full h-[350px] bg-white hover:shadow-mainShadow
-       hover:translate-y-[-5px] transition-all ease-in duration-150 cursor-pointer"
+       hover:scale-105 hover:z-[99999] transition-all ease-in duration-300 cursor-pointer"
     >
       <Link
         to={`/product/${formatPath(name)}`}
@@ -42,9 +42,13 @@ const ProductCard: React.FC<ProductCardProps> = ({ categoryItem }) => {
           </div>
         )}
       </Link>
-      <div>
-        <div className="mb-1">{CheckDiscount({ price, discount })}</div>
-        <AddToCartBtn name={name} id={id} />
+      <div className="relative w-full">
+        <div className="flex items-center justify-center">{CheckDiscount({ price, discount })}</div>
+        <div className="absolute py-5 invisible group-hover:visible shadow-mainShadowB
+         flex items-center justify-center opacity-0 group-hover:opacity-100
+          top-7 left-0 right-0 z-[999999] w-full bg-white transition-all ease-in duration-150">
+          <AddToCartBtn name={name} id={id} />
+        </div>
       </div>
     </li>
   );
