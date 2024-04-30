3154d1584c8109d9f6daa1be5a23b5a9caf30168
Sandro <vipsandro11@gmail.com>
2024-04-30 22:07:36 +0400

    update SidebarCart.tsx

--- src/pages/SidebarCart.tsx
+++ src/pages/SidebarCart.tsx
@@ -6,7 +6,7 @@ const SidebarCart = () => {
   return (
     <div className="fixed  inset-0 z-[9999999]">
       <div className="fixed inset-0 bg-black opacity-60 z-[-1]"></div>
-      <aside className="w-[340px] bg-white float-end h-screen">
+      <aside className="w-[300px] lg:w-[340px] bg-white float-end h-screen">
         <SidebarHeader />
         <SidebarList />
       </aside>
