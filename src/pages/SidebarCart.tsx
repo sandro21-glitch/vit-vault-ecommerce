757f05b0546cd1ed9bc0f9c2d0a4fdc83a781048
Sandro <vipsandro11@gmail.com>
2024-05-01 18:18:29 +0400

    style: Add transition effect for SidebarCart visibility

--- src/pages/SidebarCart.tsx
+++ src/pages/SidebarCart.tsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
 import SidebarHeader from "../features/sidebar/SidebarHeader";
 import SidebarList from "../features/sidebar/SidebarList";
+import { useAppSelector } from "../hooks/hooks";
 
 const SidebarCart = () => {
-  // 340px
+  const { sidebar } = useAppSelector((store) => store.modals);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (sidebar) {
+      // set isVisible to true after a short delay to allow for transition
+      const timer = setTimeout(() => setIsVisible(true), 100);
+      return () => clearTimeout(timer);
+    } else {
+      // set isVisible to false after the transition duration to allow for animation
+      const timer = setTimeout(() => setIsVisible(false), 200);
+      return () => clearTimeout(timer);
+    }
+  }, [sidebar]);
+  if (!sidebar) return null;
   return (
-    <div className="fixed  inset-0 z-[9999999]">
+    <div
+      className={`fixed inset-0 ${
+        isVisible ? "opacity-100 z-[9999999]" : "opacity-0 z-[-9999]"
+      } transition-all ease-in duration-200`}
+    >
       <div className="fixed inset-0 bg-black opacity-60 z-[-1]"></div>
-      <aside className="w-[300px] lg:w-[340px] bg-white float-end h-screen">
+      <aside
+        className={`w-[300px] lg:w-[340px] bg-white float-end h-screen
+          ${isVisible ? "translate-x-0" : "translate-x-[200%]"}
+          transition-all ease-in duration-200
+        `}
+      >
         <SidebarHeader />
         <SidebarList />
       </aside>
