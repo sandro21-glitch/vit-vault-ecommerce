d30994d11957c16828e222224ac8f1f235255631
Sandro <vipsandro11@gmail.com>
2024-05-01 17:44:46 +0400

    update SidebarHeader.tsx

--- src/features/sidebar/SidebarHeader.tsx
+++ src/features/sidebar/SidebarHeader.tsx
@@ -1,16 +1,11 @@
-import CloseButton from "../../ui/CloseButton";
+import CloseSidebar from "./CloseSidebar";
 
 const SidebarHeader = () => {
-  const handleCloseSidebar = () => {
-    console.log("closed");
-  };
+
   return (
-    <header className="flex justify-between items-center px-4 py-5 border-b">
+    <header className="flex justify-between items-center p-5 border-b">
       <h4 className="font-semibold leading-7">საყიდლების კალათა</h4>
-      <div className="flex items-center gap-2">
-        <p className="text-[14px]">დახურვა</p>
-        <CloseButton onClick={handleCloseSidebar} />
-      </div>
+      <CloseSidebar />
     </header>
   );
 };
