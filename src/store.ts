7910eb114708c314ad0d780b69b86a7b8c3d5173
Sandro <vipsandro11@gmail.com>
2024-05-01 18:17:31 +0400

    Update: Added modalsReducer to store configuration

--- src/store.ts
+++ src/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./features/slices/productsSlice";
 import filtersReducer from "./features/slices/filtersSlice";
 import cartReducer from "./features/slices/cartSlice";
+import modalsReducer from "./features/slices/modalSlice";
 export const store = configureStore({
   reducer: {
     product: productReducer,
     filters: filtersReducer,
     cart: cartReducer,
+    modals: modalsReducer,
   },
 });
 
