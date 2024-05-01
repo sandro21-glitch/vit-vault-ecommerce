8ec74585930509f37df5b517eb4bf50531c10ca1
Sandro <vipsandro11@gmail.com>
2024-05-01 17:44:44 +0400

    update SingleSidebarListItem.tsx

--- src/features/sidebar/SingleSidebarListItem.tsx
+++ src/features/sidebar/SingleSidebarListItem.tsx
@@ -1,35 +1,46 @@
 import { Link } from "react-router-dom";
 import CheckDiscount from "../../ui/CheckDiscount";
 import { checkDiscount } from "../../utils/checkDiscount";
-import { CartProductTypes } from "../slices/cartSlice";
+import { CartProductTypes, removeCartProduct } from "../slices/cartSlice";
+import CloseButton from "../../ui/CloseButton";
+import { useAppDispatch } from "../../hooks/hooks";
 
 type SingleSidebarItemTypes = {
   product: CartProductTypes;
 };
 
 const SingleSidebarListItem = ({ product }: SingleSidebarItemTypes) => {
-  const { discount, image, name, price, quantity } = product;
+  const { discount, image, name, price, quantity, id } = product;
 
+  const dispatch = useAppDispatch();
+  const handleRemoveItem = () => {
+    dispatch(removeCartProduct(id));
+  };
   return (
-    <li className="flex px-4 py-5 border-b">
+    <li className="relative">
       <Link to="/">
-        <div className="mr-5">
-          <img
-            src={image}
-            alt={name}
-            loading="lazy"
-            className="max-w-[65px] min-w-[65px]"
-          />
-        </div>
-        <div>
-          <h6 className="text-[14px]">{name}</h6>
-          <div className="flex text-primaryGray font-medium">
-            <span>{quantity}</span>
-            <span className="ml-1">x</span>
-            <CheckDiscount
-              discount={0}
-              price={checkDiscount(price, discount)}
+        <div className="flex px-4 py-5 border-b">
+          <div className="mr-5">
+            <img
+              src={image}
+              alt={name}
+              loading="lazy"
+              className="max-w-[45px] min-w-[45px]"
             />
+          </div>
+          <div>
+            <h6 className="text-[14px]">{name}</h6>
+            <div className="flex text-primaryGray font-medium">
+              <span>{quantity}</span>
+              <span className="ml-1">x</span>
+              <CheckDiscount
+                discount={0}
+                price={checkDiscount(price, discount)}
+              />
+            </div>
+          </div>
+          <div className="absolute right-0 top-2">
+            <CloseButton onClick={handleRemoveItem} />
           </div>
         </div>
       </Link>
