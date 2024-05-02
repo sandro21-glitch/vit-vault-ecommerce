23d56bb2fe2ed3c3584dfcd50b2f517552b6aeae
Sandro <vipsandro11@gmail.com>
2024-05-02 18:48:45 +0400

    feat: create Checkout.tsx

---
+++ src/features/cart/cartTotal/Checkout.tsx
@@ -0,0 +1,9 @@
+const Checkout = () => {
+  return (
+    <button className="py-3 w-full bg-secondaryGreen text-white font-semibold mt-3 hover:bg-primaryGreen transition-all ease-in duration-150">
+      შეკვეთის გაფორმება
+    </button>
+  );
+};
+
+export default Checkout;
