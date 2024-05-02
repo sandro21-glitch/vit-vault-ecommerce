d930589191b1c998e4af75c986ba2c7aea6120a3
Sandro <vipsandro11@gmail.com>
2024-05-02 18:48:40 +0400

    update CartTotal.tsx

--- src/features/cart/cartTotal/CartTotal.tsx
+++ src/features/cart/cartTotal/CartTotal.tsx
@@ -1,3 +1,6 @@
+import CartTotalList from "./CartTotalList";
+import Checkout from "./Checkout";
+
 const CartTotal = () => {
   return (
     <div className="w-full lg:max-w-[40%]">
@@ -5,35 +8,8 @@ const CartTotal = () => {
         <h3 className="text-[25px] font-medium text-wrap mb-5">
           მთლიანი ღირებულება
         </h3>
-        <ul>
-          <li className="py-3 border-b text-[14px]">
-            <div className="flex items-center justify-between">
-              <p className="text-black ">ჯამი</p>
-              <p>168 ₾</p>
-            </div>
-          </li>
-          <li className="py-3 border-b text-[14px]">
-            <div className="flex items-center justify-between">
-              <p className="text-black ">მიწოდება</p>
-              <p className="text-right text-[15px] text-secondaryGreen font-semibold cursor-pointer">
-                მიწოდების <br /> ღირებულების <br /> დაანგარიშება
-              </p>
-            </div>
-          </li>
-          <li className="py-3">
-            <div className="flex items-center justify-between">
-              <p className="text-[16px] text-black font-semibold">
-                მთლიანი ჯამი
-              </p>
-              <p className="text-secondaryGreen font-semibold  text-[20px]">
-                168₾
-              </p>
-            </div>
-          </li>
-        </ul>
-        <button className="py-3 w-full bg-secondaryGreen text-white font-semibold mt-3 hover:bg-primaryGreen transition-all ease-in duration-150">
-          შეკვეთის გაფორმება
-        </button>
+        <CartTotalList />
+        <Checkout />
       </div>
     </div>
   );
