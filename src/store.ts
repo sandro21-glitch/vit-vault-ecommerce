578c4bfa27c6e218a904f37507725387b16bfbe1
Sandro <vipsandro11@gmail.com>
2024-05-03 14:55:34 +0400

    update store.ts

--- src/store.ts
+++ src/store.ts
@@ -3,12 +3,14 @@ import productReducer from "./features/slices/productsSlice";
 import filtersReducer from "./features/slices/filtersSlice";
 import cartReducer from "./features/slices/cartSlice";
 import modalsReducer from "./features/slices/modalSlice";
+import articlesReducer from "./features/slices/articlesSlice";
 export const store = configureStore({
   reducer: {
     product: productReducer,
     filters: filtersReducer,
     cart: cartReducer,
     modals: modalsReducer,
+    articles: articlesReducer,
   },
 });
 
