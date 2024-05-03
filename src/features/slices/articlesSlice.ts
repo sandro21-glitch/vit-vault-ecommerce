a7ec5e37d20821dc75538f0f0c3c0cefeee13c4d
Sandro <vipsandro11@gmail.com>
2024-05-03 15:21:46 +0400

    update articlesSlice.ts

--- src/features/slices/articlesSlice.ts
+++ src/features/slices/articlesSlice.ts
@@ -1,29 +1,58 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import articlesData from "../../data/articles.json";
+export interface SubInfo {
+  infoTitle: string;
+  answer: string;
+}
 
 export interface ArticlesData {
   id: string;
   title: string;
-  subTitle: string;
+  subTitle: string | null;
   image: string;
-  info: string;
-  subInfos: {
-    infoTitle: string;
-    answer: string;
-  }[];
+  info: string | null;
+  subInfos: SubInfo[] | null;
 }
 
+export const fetchArticlesData = createAsyncThunk(
+  "articles/fetchArticlesData",
+  async () => {
+    try {
+      const articles: ArticlesData[] = articlesData.articles;
+      return articles;
+    } catch (error) {
+      console.error("Error fetching articles:", error);
+      throw error;
+    }
+  }
+);
 export interface ArticlesState {
   articlesData: ArticlesData[] | null;
+  status: string;
 }
 
 const initialState: ArticlesState = {
   articlesData: null,
+  status: "idle",
 };
 
 export const articlesSlice = createSlice({
   name: "articles",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchArticlesData.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchArticlesData.fulfilled, (state, action) => {
+        state.articlesData = action.payload;
+        state.status = "succeeded";
+      })
+      .addCase(fetchArticlesData.rejected, (state) => {
+        state.status = "error";
+      });
+  },
 });
 
 export const {} = articlesSlice.actions;
