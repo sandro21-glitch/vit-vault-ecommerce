490f395fa3218232464b2f9e1fce88361a140604
Sandro <vipsandro11@gmail.com>
2024-05-07 20:01:27 +0400

    feat: create ArticlesLayout.tsx

---
+++ src/features/articles/ArticlesLayout.tsx
@@ -0,0 +1,11 @@
+import ArticlesList from "./ArticlesList";
+
+const ArticlesLayout = () => {
+  return (
+    <article>
+      <ArticlesList />
+    </article>
+  );
+};
+
+export default ArticlesLayout;
