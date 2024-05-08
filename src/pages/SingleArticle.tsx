b72ba9f2e0772ed6efd438e70c7adbdf319bcb73
Sandro <vipsandro11@gmail.com>
2024-05-08 22:06:42 +0400

    feat: Add redirection to homepage when selected article is not found

--- src/pages/SingleArticle.tsx
+++ src/pages/SingleArticle.tsx
@@ -1,20 +1,30 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import SingleArticlePageItem from "../features/articles/singleArticlePage/SingleArticlePageItem";
 import SectionHeader from "../ui/SectionHeader";
 import { useAppSelector } from "../hooks/hooks";
+import { useEffect } from "react";
 
 const SingleArticle = () => {
   const { state } = useLocation();
   const { articlesData } = useAppSelector((store) => store.articles);
   const selectedArticle = articlesData?.find((article) => article.id === state);
-  return (
-    <section>
-      <SectionHeader path={`${selectedArticle?.title}`} />
-      <article className="section-center section-x">
-        <SingleArticlePageItem selectedArticle={selectedArticle} />
-      </article>
-    </section>
-  );
+  const navigate = useNavigate();
+
+  if (selectedArticle) {
+    return (
+      <section>
+        <SectionHeader path={`${selectedArticle?.title}`} />
+        <article className="section-center section-x">
+          <SingleArticlePageItem selectedArticle={selectedArticle} />
+        </article>
+      </section>
+    );
+  } else {
+    useEffect(() => {
+      navigate("/");
+    }, []);
+    return null;
+  }
 };
 
 export default SingleArticle;
