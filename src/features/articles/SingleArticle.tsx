54268f0f2797156b2d53cd4de375aacc5c88d8a2
Sandro <vipsandro11@gmail.com>
2024-05-08 14:04:28 +0400

    update SingleArticle.tsx

--- src/features/articles/SingleArticle.tsx
+++ src/features/articles/SingleArticle.tsx
@@ -1,4 +1,5 @@
 import { ArticlesData } from "../slices/articlesSlice";
+import ImageTitle from "./ImageTitle";
 
 type SingleArticleTypes = {
   item: ArticlesData;
@@ -9,19 +10,20 @@ const SingleArticle = ({ item, index }: SingleArticleTypes) => {
   const isDivisible = index % 2 === 0;
   return (
     <li
-      className={`mb-10 flex items-center shadow-mainShadow ${
+      className={`mb-10 flex items-center gap-5 shadow-mainShadow p-5 rounded-md ${
         !isDivisible ? "flex-row-reverse" : "flex-row"
       } group `}
     >
-      <div className="overflow-hidden cursor-pointer flex-1 ">
+      <div className="overflow-hidden rounded-md cursor-pointer flex-1 relative">
         <img
           src={item.image}
           alt="article image"
-          className="w-full h-fit group-hover:scale-105 transition-all ease-in duration-300 "
+          className="w-full h-fit  max-h-[250px] object-cover group-hover:scale-105 transition-all ease-in duration-300 "
         />
+        <ImageTitle />
       </div>
-      <div className="w-full flex flex-col items-center flex-1 p-5">
-        <h3 className=" text-[23px]  cursor-pointer text-center font-medium mb-5 hover:text-secondaryGreen transition-all ease-in duration-150">
+      <div className="w-full flex flex-col items-center flex-1 ">
+        <h3 className=" text-[23px] leading-8 cursor-pointer text-center font-semibold mb-5 hover:text-secondaryGreen transition-all ease-in duration-150">
           {item.title}
         </h3>
         <p className="text-[16px] text-center mb-5 font-poppins">
