f8e99b40f75a11ec8f36ea32ee70af40752fd7b7
Sandro <vipsandro11@gmail.com>
2024-05-08 21:58:58 +0400

    update SubTitle.tsx

--- src/features/articles/singleArticlePage/parts/SubTitle.tsx
+++ src/features/articles/singleArticlePage/parts/SubTitle.tsx
@@ -1,8 +1,20 @@
+type SubTitleTypes = {
+  subTitle: string;
+};
 
-const SubTitle = () => {
+const SubTitle = ({ subTitle }: SubTitleTypes) => {
+  if (!subTitle) return null;
+  const subTitleParts = subTitle.split("-");
   return (
-    <div>SubTitle</div>
-  )
-}
+    <div>
+      {subTitleParts?.map((part, index) => (
+        <h2 key={index} className="text-start text-[25px] font-medium mb-10">
+          {part.trim()}
+          {index < subTitleParts.length - 1 && <br />}
+        </h2>
+      ))}
+    </div>
+  );
+};
 
-export default SubTitle
+export default SubTitle;
