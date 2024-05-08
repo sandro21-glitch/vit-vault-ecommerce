0bb9e0c458177ed66476133d1c7a239b158bd6d1
Sandro <vipsandro11@gmail.com>
2024-05-08 14:04:30 +0400

    feat: create ImageTitle.tsx

---
+++ src/features/articles/ImageTitle.tsx
@@ -0,0 +1,15 @@
+import "./imageTitle.css";
+const ImageTitle = () => {
+  return (
+    <div className="absolute top-0">
+      <div className="notification">
+        <div className="notiglow"></div>
+        <div className="notiborderglow"></div>
+        <div className="notititle">Welcome To Uiverse</div>
+        <div className="notibody">Contribute to Open Source UI Elements</div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageTitle;
