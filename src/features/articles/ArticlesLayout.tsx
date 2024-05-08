a47cf5a8cb31f93e7709be4402983d0b63e34ca4
Sandro <vipsandro11@gmail.com>
2024-05-08 16:45:21 +0400

    update ArticlesLayout.tsx

--- src/features/articles/ArticlesLayout.tsx
+++ src/features/articles/ArticlesLayout.tsx
@@ -1,9 +1,11 @@
+import ArticleWidgets from "../../ui/ArticleWidgets";
 import ArticlesList from "./ArticlesList";
 
 const ArticlesLayout = () => {
   return (
-    <article className="section-center section-x">
+    <article className="section-center section-x flex gap-10 flex-col lg:flex-row">
       <ArticlesList />
+      <ArticleWidgets />
     </article>
   );
 };
