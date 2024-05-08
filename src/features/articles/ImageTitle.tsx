81b8a066b66a2c8c3195734b7a1b0208adc4fff7
Sandro <vipsandro11@gmail.com>
2024-05-08 16:37:13 +0400

    update ImageTitle.tsx

--- src/features/articles/ImageTitle.tsx
+++ src/features/articles/ImageTitle.tsx
@@ -1,12 +1,11 @@
 import "./imageTitle.css";
 const ImageTitle = () => {
   return (
-    <div className="absolute top-0">
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[999]">
       <div className="notification">
         <div className="notiglow"></div>
         <div className="notiborderglow"></div>
-        <div className="notititle">Welcome To Uiverse</div>
-        <div className="notibody">Contribute to Open Source UI Elements</div>
+        <div className="notititle">პრაქტიკული მითითებები ცილის შესახებ</div>
       </div>
     </div>
   );
