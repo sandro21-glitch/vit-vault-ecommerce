e9153dbafebcc2599d53dc2ff27e27be9f65ec75
Sandro <vipsandro11@gmail.com>
2024-05-08 16:59:47 +0400

    update ArticleWidgets.tsx

--- src/ui/ArticleWidgets.tsx
+++ src/ui/ArticleWidgets.tsx
@@ -1,12 +1,19 @@
+import SingleWidget from "../features/articleWidgets/SingleWidget";
 import { useAppSelector } from "../hooks/hooks";
 
 const ArticleWidgets = () => {
   const { articlesData, status } = useAppSelector((store) => store.articles);
 
-  if(status === 'error') return <p>error</p>
-  if(status === 'loading') return <p>loading...</p>
-    
-  return <div className="">ArticleWidgets</div>;
+  if (status === "error") return <p>error</p>;
+  if (status === "loading") return <p>loading...</p>;
+  if (!articlesData) return <p>No articles available</p>;
+  return (
+    <div className="flex flex-col gap-5 max-w-[25%]">
+      {articlesData.map((article) => (
+        <SingleWidget key={article.id} article={article} />
+      ))}
+    </div>
+  );
 };
 
 export default ArticleWidgets;
