2c2884288655f2a1a9093821c1668847722ab6aa
Sandro <vipsandro11@gmail.com>
2024-05-08 20:23:17 +0400

    update NavList.tsx

--- src/features/header/navbar/desktop/nav/NavList.tsx
+++ src/features/header/navbar/desktop/nav/NavList.tsx
@@ -6,7 +6,12 @@ const NavList = () => {
     <ul className="flex items-center gap-5 w-full">
       {navItems.map((item) => {
         return (
-          <SingleNavItem key={item.id} name={item.name} icon={item.icon} />
+          <SingleNavItem
+            key={item.id}
+            name={item.name}
+            icon={item.icon}
+            path={item.path}
+          />
         );
       })}
     </ul>
