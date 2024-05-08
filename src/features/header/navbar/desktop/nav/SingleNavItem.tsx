4e55c712a33bd6a47eb3dc179815ec7713f4b31b
Sandro <vipsandro11@gmail.com>
2024-05-08 20:23:20 +0400

    update SingleNavItem.tsx

--- src/features/header/navbar/desktop/nav/SingleNavItem.tsx
+++ src/features/header/navbar/desktop/nav/SingleNavItem.tsx
@@ -1,12 +1,20 @@
+import { Link } from "react-router-dom";
+
 type SingleNavItemTypes = {
   name: string;
   icon: string;
+  path: string;
 };
-const SingleNavItem = ({ icon, name }: SingleNavItemTypes) => {
+const SingleNavItem = ({ icon, name, path }: SingleNavItemTypes) => {
   return (
-    <li className="flex items-center gap-1 cursor-pointer text-[14px] font-semibold hover:text-primaryGreen transition-colors ease-in duration-150">
-      <img src={icon} alt={name} className="w-[24px] h-[24px]" />
-      <span>{name}</span>
+    <li>
+      <Link
+        to={path}
+        className="flex items-center gap-1 cursor-pointer text-[14px] font-semibold hover:text-primaryGreen transition-colors ease-in duration-150"
+      >
+        <img src={icon} alt={name} className="w-[24px] h-[24px]" />
+        <span>{name}</span>
+      </Link>
     </li>
   );
 };
