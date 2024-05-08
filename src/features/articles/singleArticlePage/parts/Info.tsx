511f52b64ae7128d96644de45b9dc0830be54bab
Sandro <vipsandro11@gmail.com>
2024-05-08 21:59:03 +0400

    update Info.tsx

--- src/features/articles/singleArticlePage/parts/Info.tsx
+++ src/features/articles/singleArticlePage/parts/Info.tsx
@@ -1,8 +1,18 @@
+type InfoTypes = {
+  info: string;
+};
 
-const Info = () => {
+const Info = ({ info }: InfoTypes) => {
+  const infoParts = info.split(" - ");
   return (
-    <div>Info</div>
-  )
-}
+    <div>
+      {infoParts?.map((part, index) => (
+        <p key={index} className="font-poppins text-[20px] mb-10">
+          {part.trim()}
+        </p>
+      ))}
+    </div>
+  );
+};
 
-export default Info
+export default Info;
