59e46de37cc352311f679439453cc5e5e1679ba1
Sandro <vipsandro11@gmail.com>
2024-05-08 22:01:55 +0400

    update SubInfos.tsx

--- src/features/articles/singleArticlePage/parts/SubInfos.tsx
+++ src/features/articles/singleArticlePage/parts/SubInfos.tsx
@@ -7,13 +7,19 @@ const SubInfos = ({ subInfos }: SubInfoTypes) => {
   return (
     <ul>
       {subInfos.map((info, index) => {
+        const splitInfo = info.answer.split(" - ");
         return (
           <li key={index} className="mb-10">
             <div className="text-[2rem] text-black font-semibold">
               <span>{index + 1}. </span>
               <span>{info.infoTitle}</span>
             </div>
-            <p className="font-poppins text-[16px]">{info.answer}</p>
+            {splitInfo.map((part, index) => (
+              <p className="font-poppins text-[16px] mb-3">
+                {part.trim()}
+                {index < splitInfo.length - 1 && <br />}
+              </p>
+            ))}
           </li>
         );
       })}
