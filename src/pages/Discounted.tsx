2dd7dcca57c7ea24b54685c1b09527bff231a45c
Sandro <vipsandro11@gmail.com>
2024-05-11 20:36:49 +0400

    update Discounted.tsx

--- src/pages/Discounted.tsx
+++ src/pages/Discounted.tsx
@@ -1,9 +1,17 @@
-
+import DiscountedList from "../features/discounted/DiscountedList";
+import SectionHeader from "../ui/SectionHeader";
 
 const Discounted = () => {
-  return (
-    <div>Discounted</div>
-  )
-}
 
-export default Discounted
+  return (
+    <section>
+      <SectionHeader path="ფასდაკლებები" />
+      <article className="section-center section-x">
+        <header>11fefwefw</header>
+        <DiscountedList />
+      </article>
+    </section>
+  );
+};
+
+export default Discounted;
