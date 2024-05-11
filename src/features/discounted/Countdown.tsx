2d3929c329b4ab9a6eb9571ea5f57a2a5e7b849b
Sandro <vipsandro11@gmail.com>
2024-05-11 21:02:29 +0400

    update Countdown.tsx

--- src/features/discounted/Countdown.tsx
+++ src/features/discounted/Countdown.tsx
@@ -21,26 +21,24 @@ const Countdown = () => {
   const minutes = Math.floor((time % (60 * 60)) / 60);
   const seconds = time % 60;
 
-  
-
   return (
     <div className="mb-10 font-poppins">
       <div className="flex items-center justify-center ">
         <div className="border px-4 py-2 rounded-md flex items-center gap-10">
           <div className="flex flex-col items-center">
-            <p className="text-[1rem] uppercase">days</p>
+            <p className="text-[.8rem] uppercase">days</p>
             <p className="text-[3rem] font-bold text-secondaryGreen">{`${days}`.padStart(2, "0")}</p>
           </div>
           <div className="flex flex-col items-center">
-            <p className="text-[1rem] uppercase">hours</p>
+            <p className="text-[.8rem] uppercase">hours</p>
             <p className="text-[3rem] font-bold text-secondaryGreen">{`${hours}`.padStart(2, "0")}</p>
           </div>
           <div className="flex flex-col items-center">
-            <p className="text-[1rem] uppercase">min</p>
+            <p className="text-[.8rem] uppercase">min</p>
             <p className="text-[3rem] font-bold text-secondaryGreen">{`${minutes}`.padStart(2, "0")}</p>
           </div>
           <div className="flex flex-col items-center">
-            <p className="text-[1rem] uppercase">sec</p>
+            <p className="text-[.8rem] uppercase">sec</p>
             <p className="text-[3rem] font-bold text-secondaryGreen">{`${seconds}`.padStart(2, "0")}</p>
           </div>
         </div>
