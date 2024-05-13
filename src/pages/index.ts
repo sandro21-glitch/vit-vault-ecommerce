cfab018887c82fa590ddb7926969db51bbc50c2e
Sandro <vipsandro11@gmail.com>
2024-05-13 20:01:31 +0400

    feat: create src/pages/index.ts

---
+++ src/pages/index.ts
@@ -0,0 +1,9 @@
+export { default as Articles } from "./Articles";
+export { default as Cart } from "./Cart";
+export { default as Discounted } from "./Discounted";
+export { default as MainPage } from "./MainPage";
+export { default as ProductByCategory } from "./ProductByCategory";
+export { default as ProductByInput } from "./ProductByInput";
+export { default as SidebarCart } from "./SidebarCart";
+export { default as SingleArticle } from "./SingleArticle";
+export { default as SingleProduct } from "./SingleProduct";
