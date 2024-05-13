040f9cb035cc3672eaec10588aa54f757fef2ab7
Sandro <vipsandro11@gmail.com>
2024-05-13 21:31:24 +0400

    update UserAuthBtn.tsx

--- src/features/header/navbar/mobile/nav/UserAuthBtn.tsx
+++ src/features/header/navbar/mobile/nav/UserAuthBtn.tsx
@@ -1,9 +1,23 @@
 import { Link } from "react-router-dom";
 import { FaRegUser } from "react-icons/fa";
+import { useAppDispatch } from "../../../../../hooks/hooks";
+import { openLoginSidebar } from "../../../../slices/modalSlice";
+type UserAuthBtnTypes = {
+  setOpen: (open: boolean) => void;
+};
 
-const UserAuthBtn = () => {
+const UserAuthBtn = ({ setOpen }: UserAuthBtnTypes) => {
+  const dispatch = useAppDispatch();
+  const handleOpenAuthSidebar = () => {
+    setOpen(false);
+    dispatch(openLoginSidebar());
+  };
+
   return (
-    <li className="border-b list-none cursor-pointer text-[14px] font-semibold text-primaryGray">
+    <li
+      onClick={handleOpenAuthSidebar}
+      className="border-b list-none cursor-pointer text-[14px] font-semibold text-primaryGray"
+    >
       <Link to="/discount" className="p-[14px] inline-flex items-center gap-2">
         <FaRegUser />
         <span>შესვლა / რეგისტრაცია</span>
