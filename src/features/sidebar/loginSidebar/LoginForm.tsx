f56db9b7b6538298dc7362cdfcd3399b40259d10
Sandro <vipsandro11@gmail.com>
2024-05-13 21:58:24 +0400

    feat: create LoginForm.tsx

---
+++ src/features/sidebar/loginSidebar/LoginForm.tsx
@@ -0,0 +1,40 @@
+const LoginForm = () => {
+  return (
+    <form className="p-5 font-poppins">
+      <div className="flex flex-col">
+        <label htmlFor="name" className="text-right text-[18px] text-primaryGray">
+          სახელი, ან ელფოსტა
+        </label>
+        <input type="text" id="name" className="border-2 outline-none p-2" />
+      </div>
+      <div className="flex flex-col mt-5">
+        <label htmlFor="password" className="text-right text-[18px] text-primaryGray">
+          პაროლი
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="border-2 outline-none p-2"
+        />
+      </div>
+      <button
+        className="mt-5 w-full bg-secondaryGreen py-2 text-white text-[15px] lg:text-[18px]
+       font-bold tracking-widest hover:bg-primaryGreen transition-all ease-in duration-150"
+      >
+        შესვლა
+      </button>
+      <div className="flex items-center gap-1 mt-5">
+        <input type="checkbox" id="save" />
+        <label
+          htmlFor="save"
+          className="cursor-pointer text-[17px] text-primaryGray"
+        >
+          დამახსოვრება
+        </label>
+      </div>
+      <p className="text-secondaryGreen mt-5">დაგავიწყდათ პაროლი?</p>
+    </form>
+  );
+};
+
+export default LoginForm;
