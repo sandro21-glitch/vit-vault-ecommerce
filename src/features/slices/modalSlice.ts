49a445debad6499ce3e2aecc5832cb06e0f3c438
Sandro <vipsandro11@gmail.com>
2024-05-13 21:22:34 +0400

    update modalSlice.ts

--- src/features/slices/modalSlice.ts
+++ src/features/slices/modalSlice.ts
@@ -2,10 +2,12 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export interface ModalsState {
   sidebar: boolean;
+  loginSidebar: boolean;
 }
 
 const initialState: ModalsState = {
   sidebar: false,
+  loginSidebar: false,
 };
 
 export const modalsSlice = createSlice({
@@ -15,12 +17,23 @@ export const modalsSlice = createSlice({
     openSidebar: (state) => {
       state.sidebar = true;
     },
+    openLoginSidebar: (state) => {
+      state.loginSidebar = true;
+    },
     closeSidebar: (state) => {
       state.sidebar = false;
+    },
+    closeLoginSidebar: (state) => {
+      state.loginSidebar = false;
     },
   },
 });
 
-export const { openSidebar, closeSidebar } = modalsSlice.actions;
+export const {
+  openSidebar,
+  closeSidebar,
+  openLoginSidebar,
+  closeLoginSidebar,
+} = modalsSlice.actions;
 
 export default modalsSlice.reducer;
