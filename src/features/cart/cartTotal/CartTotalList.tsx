531264658256778e94d1404c16eebe247f45b5ca
Sandro <vipsandro11@gmail.com>
2024-05-14 14:23:36 +0400

    update CartTotalList.tsx

--- src/features/cart/cartTotal/CartTotalList.tsx
+++ src/features/cart/cartTotal/CartTotalList.tsx
@@ -2,7 +2,7 @@ import { useAppSelector } from "../../../hooks/hooks";
 import { formatToGeorgianLari } from "../../../utils/formatPrice";
 
 const CartTotalList = () => {
-  const { totalSum } = useAppSelector((store) => store.cart);
+  const { totalSum, totalDiscount } = useAppSelector((store) => store.cart);
 
   return (
     <ul>
@@ -14,9 +14,11 @@ const CartTotalList = () => {
       </li>
       <li className="py-3 border-b text-[14px]">
         <div className="flex items-center justify-between">
-          <p className="text-black ">მიწოდება</p>
-          <p className="text-right text-[15px] text-secondaryGreen font-semibold cursor-pointer">
-            მიწოდების <br /> ღირებულების <br /> დაანგარიშება
+          <p className="text-[16px] text-black font-semibold">
+            საერთო ფასდაკლება
+          </p>
+          <p className="text-secondaryGreen font-semibold  text-[20px]">
+            {formatToGeorgianLari(totalDiscount)}
           </p>
         </div>
       </li>
