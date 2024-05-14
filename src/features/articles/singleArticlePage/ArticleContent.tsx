28980119d0838a477119008a1aad85b836ca464b
Sandro <vipsandro11@gmail.com>
2024-05-14 14:34:25 +0400

    update ArticleContent.tsx

--- src/features/articles/singleArticlePage/ArticleContent.tsx
+++ src/features/articles/singleArticlePage/ArticleContent.tsx
@@ -13,9 +13,9 @@ const ArticleContent = ({ selectedArticle }: ArticleContentTypes) => {
   const { image, info, subInfos, subTitle, title } = selectedArticle;
 
   return (
-    <div className="max-w-[75%]">
+    <div className="w-full lg:max-w-[75%]">
       <Title title={title} />
-      <img src={image} alt="article image" loading="lazy" className="mb-10" />
+      <img src={image} alt="article image" loading="lazy" className="mb-10 w-full" />
       {subTitle && <SubTitle subTitle={subTitle} />}
       {info && <Info info={info} />}
       {subInfos && <SubInfos subInfos={subInfos} />}
