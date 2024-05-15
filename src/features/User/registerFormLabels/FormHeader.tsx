457f4457670d0f6b13e7722eee8489111e201220
Sandro <vipsandro11@gmail.com>
2024-05-15 22:47:05 +0400

    feat: create FormHeader.tsx

---
+++ src/features/User/registerFormLabels/FormHeader.tsx
@@ -0,0 +1,14 @@
+
+
+const FormHeader = () => {
+  return (
+    <h4
+      className="mb-10 relative after:absolute after:-bottom-2 after:left-0 after:right-0
+   after:bg-secondaryGreen after:h-[3px] after:w-full"
+    >
+      მომხმარებელი
+    </h4>
+  );
+};
+
+export default FormHeader;
