c536b79ccdd91e87fdc4056b8cb8e1c20cd9d4db
Sandro <vipsandro11@gmail.com>
2024-05-15 20:49:45 +0400

    update Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,58 +1,14 @@
-import { useState } from "react";
-import { UserData, registerUser } from "../features/slices/userSlice";
-import { useAppDispatch } from "../hooks/hooks";
+import RegisterForm from "../features/User/RegisterForm";
+import SectionHeader from "../ui/SectionHeader";
 
 const Register = () => {
-  const dispatch = useAppDispatch();
-  const [userData, setUserData] = useState<UserData>({
-    email: "",
-    password: "",
-  });
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserData((prevUserData) => ({
-      ...prevUserData,
-      email: e.target.value,
-    }));
-  };
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserData((prevUserData) => ({
-      ...prevUserData,
-      password: e.target.value,
-    }));
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    dispatch(registerUser(userData));
-    console.log(userData);
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="mail">eMail</label>
-        <input
-          type="email"
-          id="mail"
-          className="border"
-          value={userData.email}
-          onChange={handleEmailChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="pass">Password</label>
-        <input
-          type="password"
-          id="pass"
-          className="border"
-          value={userData.password}
-          onChange={handlePasswordChange}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <section>
+      <SectionHeader path="რეგისტრაცია" />
+      <article className="section-center section-x min-h-[65vh]">
+        <RegisterForm />
+      </article>
+    </section>
   );
 };
 
