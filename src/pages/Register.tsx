33006b2cd759e9b1c43301b86781c22a53444b12
Sandro <vipsandro11@gmail.com>
2024-05-15 20:17:36 +0400

    update Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,59 @@
-
+import { useState } from "react";
+import { UserData, registerUser } from "../features/slices/userSlice";
+import { useAppDispatch } from "../hooks/hooks";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const dispatch = useAppDispatch();
+  const [userData, setUserData] = useState<UserData>({
+    email: "",
+    password: "",
+  });
 
-export default Register
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserData((prevUserData) => ({
+      ...prevUserData,
+      email: e.target.value,
+    }));
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserData((prevUserData) => ({
+      ...prevUserData,
+      password: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(registerUser(userData));
+    console.log(userData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="mail">eMail</label>
+        <input
+          type="email"
+          id="mail"
+          className="border"
+          value={userData.email}
+          onChange={handleEmailChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="pass">Password</label>
+        <input
+          type="password"
+          id="pass"
+          className="border"
+          value={userData.password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Register;
