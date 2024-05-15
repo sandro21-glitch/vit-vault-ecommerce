eaa36447cfd368a0b519982a96e7262fba37d1dd
Sandro <vipsandro11@gmail.com>
2024-05-15 22:52:44 +0400

    update TermsCheckbox.tsx

--- src/features/User/registerFormLabels/TermsCheckbox.tsx
+++ src/features/User/registerFormLabels/TermsCheckbox.tsx
@@ -1,10 +1,10 @@
 const TermsCheckbox = () => {
   return (
     <div className="flex items-center mb-5">
-      <input type="checkbox" id="terms" className="scale-[1.5] mr-2" />
+      <input type="checkbox" id="terms" className="scale-[1.1] lg:scale-[1.5] mr-2" />
       <label
         htmlFor="terms"
-        className="text-[18px] font-semibold  cursor-pointer"
+        className="text-[14px] md:text-[16px] lg:text-[18px] font-semibold  cursor-pointer"
       >
         ვეთანხმები{" "}
         <span className="text-secondaryGreen hover:text-primaryGreen transition-colors ease-in duration-150">
