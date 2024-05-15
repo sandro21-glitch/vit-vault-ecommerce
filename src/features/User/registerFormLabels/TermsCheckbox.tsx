4bbdc3fbce2e7fb6b964bd75053093d88e9be108
Sandro <vipsandro11@gmail.com>
2024-05-15 22:47:12 +0400

    feat: create TermsCheckbox.tsx

---
+++ src/features/User/registerFormLabels/TermsCheckbox.tsx
@@ -0,0 +1,18 @@
+const TermsCheckbox = () => {
+  return (
+    <div className="flex items-center mb-5">
+      <input type="checkbox" id="terms" className="scale-[1.5] mr-2" />
+      <label
+        htmlFor="terms"
+        className="text-[18px] font-semibold  cursor-pointer"
+      >
+        ვეთანხმები{" "}
+        <span className="text-secondaryGreen hover:text-primaryGreen transition-colors ease-in duration-150">
+          წესებს და პირობებს
+        </span>
+      </label>
+    </div>
+  );
+};
+
+export default TermsCheckbox;
