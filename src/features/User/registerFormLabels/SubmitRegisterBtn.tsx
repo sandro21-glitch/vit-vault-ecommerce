da24766b2b3638fa386a3d7f606d6cc1aea40d8a
Sandro <vipsandro11@gmail.com>
2024-05-15 22:47:10 +0400

    feat: create SubmitRegisterBtn.tsx

---
+++ src/features/User/registerFormLabels/SubmitRegisterBtn.tsx
@@ -0,0 +1,12 @@
+const SubmitRegisterBtn = () => {
+  return (
+    <button
+      type="submit"
+      className="w-full bg-secondaryGreen text-white py-3 text-[18px] font-semibold tracking-widest"
+    >
+      რეგისტრაცია
+    </button>
+  );
+};
+
+export default SubmitRegisterBtn;
