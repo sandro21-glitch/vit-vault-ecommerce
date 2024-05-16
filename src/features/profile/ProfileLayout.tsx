6258e46c8f9bc8e0a02eed1b40553c8c3553a982
Sandro <vipsandro11@gmail.com>
2024-05-16 14:48:33 +0400

    update ProfileLayout.tsx

--- src/features/profile/ProfileLayout.tsx
+++ src/features/profile/ProfileLayout.tsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import AccountSidebar from "./sidebar/AccountSidebar";
 
 const ProfileLayout = () => {
+  const [selectedTab, setSelectedTab] = useState("orders");
+
   return (
     <div className="py-20">
-      <AccountSidebar />
+      <AccountSidebar
+        setSelectedTab={setSelectedTab}
+        selectedTab={selectedTab}
+      />
     </div>
   );
 };
