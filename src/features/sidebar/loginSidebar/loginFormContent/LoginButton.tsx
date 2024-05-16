73aae0be13e0978c324b78d268508eb288832514
Sandro <vipsandro11@gmail.com>
2024-05-16 15:49:11 +0400

    feat: create LoginButton.tsx

---
+++ src/features/sidebar/loginSidebar/loginFormContent/LoginButton.tsx
@@ -0,0 +1,13 @@
+const LoginButton = () => {
+  return (
+    <button
+      type="submit"
+      className="mt-5 w-full bg-secondaryGreen py-2 text-white text-[15px] lg:text-[18px]
+   font-bold tracking-widest hover:bg-primaryGreen transition-all ease-in duration-150"
+    >
+      შესვლა
+    </button>
+  );
+};
+
+export default LoginButton;
