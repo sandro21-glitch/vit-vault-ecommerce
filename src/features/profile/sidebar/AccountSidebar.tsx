042faf4aba34bacdc83b6b0f60582f16c2fc5194
Sandro <vipsandro11@gmail.com>
2024-05-16 15:15:46 +0400

    update AccountSidebar.tsx

--- src/features/profile/sidebar/AccountSidebar.tsx
+++ src/features/profile/sidebar/AccountSidebar.tsx
@@ -7,7 +7,7 @@ type SidebarListProps = {
 
 const AccountSidebar = ({ setSelectedTab, selectedTab }: SidebarListProps) => {
   return (
-    <div className="max-w-[25%] pr-5 border-r h-full">
+    <div className="min-w-[25%] max-w-[25%] pr-5 border-r h-full">
       <h4 className="border-b pb-3 pl-2">ჩემი ანგარიში</h4>
       <SidebarList setSelectedTab={setSelectedTab} selectedTab={selectedTab} />
     </div>
