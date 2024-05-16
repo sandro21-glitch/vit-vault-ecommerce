aabcdd59361893b4b49245f2bc3e295c4ecb467c
Sandro <vipsandro11@gmail.com>
2024-05-16 15:49:16 +0400

    feat: create LoginPasswordInput.tsx

---
+++ src/features/sidebar/loginSidebar/loginFormContent/LoginPasswordInput.tsx
@@ -0,0 +1,26 @@
+type LoginPasswordTypes = {
+  userData: { email: string; password: string };
+  setUserData: (userData: { email: string; password: string }) => void;
+};
+
+const LoginPasswordInput = ({ setUserData, userData }: LoginPasswordTypes) => {
+  return (
+    <div className="flex flex-col mt-5">
+      <label
+        htmlFor="password"
+        className="text-right text-[18px] text-primaryGray"
+      >
+        პაროლი
+      </label>
+      <input
+        type="password"
+        id="password"
+        className="border-2 outline-none p-2"
+        value={userData.password}
+        onChange={(e) => setUserData({ ...userData, password: e.target.value })}
+      />
+    </div>
+  );
+};
+
+export default LoginPasswordInput;
