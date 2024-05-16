f1edd459df5fa76c003450c4c58a329e98371e5d
Sandro <vipsandro11@gmail.com>
2024-05-16 15:50:33 +0400

    feat: :sparkles: Add user authentication to Profile component
    - Imported useNavigate from react-router-dom for navigation - Implemented redirect to homepage if user is not authenticated - Used useAppSelector to access user state from Redux store - Added useEffect hook to handle redirection based on user authentication status

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import ProfileLayout from "../features/profile/ProfileLayout";
+import { useAppSelector } from "../hooks/hooks";
 import SectionHeader from "../ui/SectionHeader";
+import { useEffect } from "react";
 
 const Profile = () => {
+  const { user } = useAppSelector((store) => store.user);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+  }, [user, navigate]);
   return (
     <section>
       <SectionHeader path="ჩემი ანგარიში" />
