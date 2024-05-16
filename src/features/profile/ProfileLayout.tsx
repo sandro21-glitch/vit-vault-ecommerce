068d1f390afdf49030aeaf99cfca22d0ac0619c3
Sandro <vipsandro11@gmail.com>
2024-05-16 14:38:34 +0400

    feat: create ProfileLayout.tsx

---
+++ src/features/profile/ProfileLayout.tsx
@@ -0,0 +1,11 @@
+import AccountSidebar from "./sidebar/AccountSidebar";
+
+const ProfileLayout = () => {
+  return (
+    <div className="py-20">
+      <AccountSidebar />
+    </div>
+  );
+};
+
+export default ProfileLayout;
