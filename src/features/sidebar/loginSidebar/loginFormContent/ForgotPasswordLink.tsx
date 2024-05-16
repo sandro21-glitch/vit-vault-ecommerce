12acfdef4415c44388c26ba421dce2137abef9cb
Sandro <vipsandro11@gmail.com>
2024-05-16 15:49:09 +0400

    feat: create ForgotPasswordLink.tsx

---
+++ src/features/sidebar/loginSidebar/loginFormContent/ForgotPasswordLink.tsx
@@ -0,0 +1,9 @@
+const ForgotPasswordLink = () => {
+  return (
+    <p className="text-secondaryGreen mt-5 hover:underline cursor-pointer text-[20px]">
+      დაგავიწყდათ პაროლი?
+    </p>
+  );
+};
+
+export default ForgotPasswordLink;
