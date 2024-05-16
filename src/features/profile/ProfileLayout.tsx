cf1fc1280635b1c78f941f2b04a7cd9ce2803f90
Sandro <vipsandro11@gmail.com>
2024-05-16 15:15:40 +0400

    update ProfileLayout.tsx

--- src/features/profile/ProfileLayout.tsx
+++ src/features/profile/ProfileLayout.tsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import AccountSidebar from "./sidebar/AccountSidebar";
+import Orders from "./content/orders/Orders";
+import Address from "./content/address/Address";
+import AccountDetails from "./content/accountDetails/AccountDetails";
 
 const ProfileLayout = () => {
   const [selectedTab, setSelectedTab] = useState("orders");
 
   return (
-    <div className="py-20">
-      <AccountSidebar setSelectedTab={setSelectedTab} selectedTab={selectedTab} />
+    <div className="py-20 flex gap-5">
+      <AccountSidebar
+        setSelectedTab={setSelectedTab}
+        selectedTab={selectedTab}
+      />
+      <div className="w-[75%]">
+        {selectedTab === "orders" && <Orders />}
+        {selectedTab === "addresses" && <Address />}
+        {selectedTab === "accountDetails" && <AccountDetails />}
+      </div>
     </div>
   );
 };
