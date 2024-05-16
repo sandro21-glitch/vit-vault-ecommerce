95b071f6045f4c6074684687f2620c60f5881903
Sandro <vipsandro11@gmail.com>
2024-05-16 15:49:02 +0400

    refactor: Refactor LoginForm component to use modularized components

--- src/features/sidebar/loginSidebar/LoginForm.tsx
+++ src/features/sidebar/loginSidebar/LoginForm.tsx
@@ -1,46 +1,31 @@
+import { useState } from "react";
+import ForgotPasswordLink from "./loginFormContent/ForgotPasswordLink";
+import LoginButton from "./loginFormContent/LoginButton";
+import LoginNameInput from "./loginFormContent/LoginNameInput";
+import LoginPasswordInput from "./loginFormContent/LoginPasswordInput";
+import SaveCheckbox from "./loginFormContent/SaveCheckbox";
+import { UserData, loginUser } from "../../slices/userSlice";
+import { useAppDispatch } from "../../../hooks/hooks";
+
 const LoginForm = () => {
+  const dispatch = useAppDispatch();
+  const [userData, setUserData] = useState<UserData>({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmitLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(loginUser(userData));
+  };
+
   return (
-    <form className="p-5 font-poppins border-b">
-      <div className="flex flex-col">
-        <label
-          htmlFor="name"
-          className="text-right text-[18px] text-primaryGray"
-        >
-          სახელი, ან ელფოსტა
-        </label>
-        <input type="text" id="name" className="border-2 outline-none p-2" />
-      </div>
-      <div className="flex flex-col mt-5">
-        <label
-          htmlFor="password"
-          className="text-right text-[18px] text-primaryGray"
-        >
-          პაროლი
-        </label>
-        <input
-          type="password"
-          id="password"
-          className="border-2 outline-none p-2"
-        />
-      </div>
-      <button
-        className="mt-5 w-full bg-secondaryGreen py-2 text-white text-[15px] lg:text-[18px]
-       font-bold tracking-widest hover:bg-primaryGreen transition-all ease-in duration-150"
-      >
-        შესვლა
-      </button>
-      <div className="flex items-center gap-1 mt-5">
-        <input type="checkbox" id="save" />
-        <label
-          htmlFor="save"
-          className="cursor-pointer text-[17px] text-primaryGray"
-        >
-          დამახსოვრება
-        </label>
-      </div>
-      <p className="text-secondaryGreen mt-5 hover:underline cursor-pointer text-[20px]">
-        დაგავიწყდათ პაროლი?
-      </p>
+    <form onSubmit={handleSubmitLogin} className="p-5 font-poppins border-b">
+      <LoginNameInput userData={userData} setUserData={setUserData} />
+      <LoginPasswordInput userData={userData} setUserData={setUserData} />
+      <LoginButton />
+      <SaveCheckbox />
+      <ForgotPasswordLink />
     </form>
   );
 };
