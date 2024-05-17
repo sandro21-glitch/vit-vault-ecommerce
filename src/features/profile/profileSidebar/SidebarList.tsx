392ea0dcc2da0ec7d29161c540821587c2279111
Sandro <vipsandro11@gmail.com>
2024-05-17 19:24:40 +0400

    feat: Updated SidebarList component to handle user logout

--- src/features/profile/profileSidebar/SidebarList.tsx
+++ src/features/profile/profileSidebar/SidebarList.tsx
@@ -1,3 +1,7 @@
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../../hooks/hooks";
+import { signOutUser } from "../../slices/userSlice";
+
 type SidebarListProps = {
   selectedTab: string;
   setSelectedTab: (tab: string) => void;
@@ -7,7 +11,16 @@ const SidebarList = ({ setSelectedTab, selectedTab }: SidebarListProps) => {
   const handleTabClick = (tab: string) => {
     setSelectedTab(tab);
   };
-
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const handleSignOutUser = async () => {
+    try {
+      await dispatch(signOutUser());
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <ul className="mt-2">
       <li
@@ -40,7 +53,7 @@ const SidebarList = ({ setSelectedTab, selectedTab }: SidebarListProps) => {
         className={`p-2 cursor-pointer transition-all ease-in duration-150 font-semibold ${
           selectedTab === "logout" ? "bg-gray-200" : "hover:bg-veryLightGray"
         }`}
-        onClick={() => handleTabClick("logout")}
+        onClick={handleSignOutUser}
       >
         გამოსვლა
       </li>
