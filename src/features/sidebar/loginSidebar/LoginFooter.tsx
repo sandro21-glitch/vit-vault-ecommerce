5653f7f2b36ac4c284e93b49c920fbeee5fdb204
Sandro <vipsandro11@gmail.com>
2024-05-18 20:56:49 +0400

    update LoginFooter.tsx

--- src/features/sidebar/loginSidebar/LoginFooter.tsx
+++ src/features/sidebar/loginSidebar/LoginFooter.tsx
@@ -1,7 +1,10 @@
 import { LuUser2 } from "react-icons/lu";
 import { Link } from "react-router-dom";
+import { useAppDispatch } from "../../../hooks/hooks";
+import { closeLoginSidebar } from "../../slices/modalSlice";
 
 const LoginFooter = () => {
+  const dispatch = useAppDispatch();
   return (
     <div className="p-5 border-b font-poppins">
       <p className="flex justify-center text-gray-200 mb-5">
@@ -10,7 +13,10 @@ const LoginFooter = () => {
       <p className="text-center mb-1 text-black font-semibold text-[18px]">
         ჯერ არ გაქვთ ანგარიში?
       </p>
-      <p className="flex justify-center text-[18px] text-black font-semibold hover:text-primaryGray transition-colors ease-in duration-150">
+      <p
+        onClick={() => dispatch(closeLoginSidebar())}
+        className="flex justify-center text-[18px] text-black font-semibold hover:text-primaryGray transition-colors ease-in duration-150"
+      >
         <Link to="/register" className="border-b border-secondaryGreen">
           რეგისტრაცია
         </Link>
