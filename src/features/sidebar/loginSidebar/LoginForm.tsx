5111def287192e3b0c16bc766bc1e6a73efcd10c
Sandro <vipsandro11@gmail.com>
2024-05-19 17:31:26 +0400

    feat: :sparkles: Enhance login form with error handling and local error state
    - Updated the login form to display error messages locally and from Redux state. - Implemented useEffect to handle Redux error state changes and display alerts. - Added clearError dispatch to reset Redux error state after displaying local error.

--- src/features/sidebar/loginSidebar/LoginForm.tsx
+++ src/features/sidebar/loginSidebar/LoginForm.tsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ForgotPasswordLink from "./loginFormContent/ForgotPasswordLink";
 import LoginButton from "./loginFormContent/LoginButton";
 import LoginNameInput from "./loginFormContent/LoginNameInput";
 import LoginPasswordInput from "./loginFormContent/LoginPasswordInput";
 import SaveCheckbox from "./loginFormContent/SaveCheckbox";
-import { UserData, loginUser } from "../../slices/userSlice";
-import { useAppDispatch } from "../../../hooks/hooks";
+import { UserData, clearError, loginUser } from "../../slices/userSlice";
+import { useAppDispatch, useAppSelector } from "../../../hooks/hooks";
 import { closeLoginSidebar } from "../../slices/modalSlice";
 import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
+  const { error } = useAppSelector((store) => store.user);
   const storedInputMail = localStorage.getItem("userEmail");
+  const [localError, setLocalError] = useState<string | null>(null);
   const [userData, setUserData] = useState<UserData>({
     email: storedInputMail ? JSON.parse(storedInputMail) : "",
     password: "",
@@ -22,7 +24,7 @@ const LoginForm = () => {
   const handleSubmitLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      await dispatch(loginUser(userData));
+      await dispatch(loginUser(userData)).unwrap();
       dispatch(closeLoginSidebar());
       navigate("/profile");
       if (isChecked) {
@@ -30,13 +32,23 @@ const LoginForm = () => {
       }
     } catch (error) {
       console.error("Login failed:", error);
+      setLocalError(error as string);
     }
   };
+
+  useEffect(() => {
+    if (error) {
+      setLocalError(error);
+      alert(error);
+      dispatch(clearError()); 
+    }
+  }, [error, dispatch]);
 
   return (
     <form onSubmit={handleSubmitLogin} className="p-5 font-poppins border-b">
       <LoginNameInput userData={userData} setUserData={setUserData} />
       <LoginPasswordInput userData={userData} setUserData={setUserData} />
+      {localError && <p className="text-red-600 text-[14px] mt-1">{localError}</p>}
       <LoginButton />
       <SaveCheckbox setIsChecked={setIsChecked} isChecked={isChecked} />
       <ForgotPasswordLink />
