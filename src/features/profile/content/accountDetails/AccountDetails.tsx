05a356a8b523aebaa3402c3f2ed6b15ae8937d06
Sandro <vipsandro11@gmail.com>
2024-05-23 00:09:43 +0400

    refactor: :sparkles: Update AccountDetails component to integrate Redux state management
    - Removed unnecessary import statements - Integrated Redux hooks for accessing user data and dispatching actions - Updated useState initial state to populate form fields with user data - Implemented handleSubmitUserData function to dispatch update action - Added toast notification for successful data update - Added form submission handler for updating user data - Adjusted button styling for better UI consistency

--- src/features/profile/content/accountDetails/AccountDetails.tsx
+++ src/features/profile/content/accountDetails/AccountDetails.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import UserEmailSection from "./UserEmailSection";
 import UserNameSection from "./UserNameSection";
+import { useAppDispatch, useAppSelector } from "../../../../hooks/hooks";
+import { updateUserData } from "../../../slices/userSlice";
+import toast from "react-hot-toast";
 
 export interface UserFormTypes {
   userName: string;
@@ -8,13 +11,29 @@ export interface UserFormTypes {
 }
 
 const AccountDetails = () => {
+  const user = useAppSelector((state) => state.user.user);
   const [accountInfo, setAccountInfo] = useState<UserFormTypes>({
-    userName: "",
-    userSurname: "",
+    userName: user?.name ? user.name : "",
+    userSurname: user?.surname ? user.surname : "",
   });
+  const dispatch = useAppDispatch();
+
+  const handleSubmitUserData = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (user) {
+      dispatch(
+        updateUserData({
+          uid: user.uid,
+          name: accountInfo.userName,
+          surname: accountInfo.userSurname,
+        })
+      );
+      toast.success("მომხმარებლის მონაცემები წარმატებით განახლდა!");
+    }
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmitUserData}>
       <div>
         <UserNameSection
           accountInfo={accountInfo}
@@ -22,6 +41,13 @@ const AccountDetails = () => {
         />
         <UserEmailSection />
       </div>
+      <button
+        type="submit"
+        className="bg-secondaryGreen py-3 px-5 text-white mt-7 font-poppins tracking-wide
+         font-bold text-[14px] hover:bg-primaryGreen transition-all ease-in duration-150"
+      >
+        ცვლილებების შენახვა
+      </button>
     </form>
   );
 };
