e674ee5a5fd443f1e1abbc99e362b6e78d5d60f1
Sandro <vipsandro11@gmail.com>
2024-05-23 19:30:00 +0400

    feat: Add functionality to clear cart and show success toast

--- src/features/cart/cartActions/ClearCart.tsx
+++ src/features/cart/cartActions/ClearCart.tsx
@@ -1,6 +1,20 @@
+import toast from "react-hot-toast";
+import { useAppDispatch } from "../../../hooks/hooks";
+import { clearCart } from "../../slices/cartSlice";
+
 const ClearCart = () => {
+  const dispatch = useAppDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart());
+    toast.success("თქვენი კალათა გასუფთავდა");
+  };
+
   return (
-    <button className="bg-veryLightGray hover:bg-gray-200 transition-colors ease-in duration-150 py-[12px] px-[20px] font-semibold text-black text-[13px] leading-[18px]">
+    <button
+      type="button"
+      onClick={handleClearCart}
+      className="bg-veryLightGray hover:bg-gray-200 transition-colors ease-in duration-150 py-[12px] px-[20px] font-semibold text-black text-[13px] leading-[18px]"
+    >
       კალათის გასუფთავება
     </button>
   );
