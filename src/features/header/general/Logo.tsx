c0b62573c5c4e1faa95facf893d5ea4695cf52c9
Sandro <vipsandro11@gmail.com>
2024-05-24 23:28:41 +0400

    update Logo.tsx

--- src/features/header/general/Logo.tsx
+++ src/features/header/general/Logo.tsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
 import logo from "/logo-no-background.svg";
+import logoMobile from "/logo-mobile.svg";
 
 const Logo = () => {
   return (
     <li className="cursor-pointer">
       <Link to="/">
-        <img src={logo} alt="logo" className="max-w-[250px] max-h-[95px]" />
+        <img
+          src={logo}
+          alt="logo"
+          className="max-w-[250px] max-h-[95px] md:block hidden"
+        />
+        <img
+          src={logoMobile}
+          alt="logo"
+          className="max-w-[150px] md:hidden block"
+        />
       </Link>
     </li>
   );
