7d7417d7340651bc57d9e83861fdce8fd1c573ab
Sandro <vipsandro11@gmail.com>
2024-05-25 20:38:29 +0400

    update NameInputs.tsx

--- src/features/cart/payment/billingAndDelivery/formInputs/NameInputs.tsx
+++ src/features/cart/payment/billingAndDelivery/formInputs/NameInputs.tsx
@@ -8,23 +8,29 @@ interface NameInputTypes {
 
 const NameInputs = ({ formData, handleChange }: NameInputTypes) => {
   return (
-    <div className="flex">
+    <div className="flex gap-5">
       <div className="flex flex-col mb-5">
-        <label htmlFor="name" className="text-end">სახელი</label>
+        <label htmlFor="name" className="text-end">
+          სახელი
+        </label>
         <Input
           id="name"
           name="firstName"
           value={formData.firstName}
           onChange={handleChange}
+          className="w-full"
         />
       </div>
       <div className="flex flex-col mb-5">
-        <label htmlFor="surname" className="text-end">გვარი</label>
+        <label htmlFor="surname" className="text-end">
+          გვარი
+        </label>
         <Input
           id="surname"
           name="lastName"
           value={formData.lastName}
           onChange={handleChange}
+          className="w-full"
         />
       </div>
     </div>
