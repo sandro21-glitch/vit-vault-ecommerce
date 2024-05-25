95736494a90cfb08ef7e7f21d47dbb917eff04ee
Sandro <vipsandro11@gmail.com>
2024-05-25 19:09:56 +0400

    change path

--- src/features/cart/cartTotal/Checkout.tsx
+++
@@ -1,9 +0,0 @@
-const Checkout = () => {
-  return (
-    <button className="py-3 w-full bg-secondaryGreen text-white font-semibold mt-3 hover:bg-primaryGreen transition-all ease-in duration-150">
-      შეკვეთის გაფორმება
-    </button>
-  );
-};
-
-export default Checkout;
