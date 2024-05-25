edd8844e4f5277b0a8a1708086eb49f5dcd8b12c
Sandro <vipsandro11@gmail.com>
2024-05-25 20:27:09 +0400

    update Input.tsx

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React from 'react';
 
 interface TextInputProps {
   id: string;
-  type: string;
+  type?: string;
+  name?: string;
   className?: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = ({ value, className, id, onChange, type }: TextInputProps) => {
+const Input: React.FC<TextInputProps> = ({ id, type = 'text', name, className, value, onChange }) => {
   return (
     <input
       type={type}
       id={id}
+      name={name}
       className={`${className} border-[3px] outline-none p-2`}
       value={value}
       onChange={onChange}
