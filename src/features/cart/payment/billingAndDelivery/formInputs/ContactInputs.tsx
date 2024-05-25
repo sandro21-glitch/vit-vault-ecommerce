3a76cb9b02739fb1b660bb16a078441bfc94368d
Sandro <vipsandro11@gmail.com>
2024-05-25 20:37:02 +0400

    update ContactInputs.tsx

--- src/features/cart/payment/billingAndDelivery/formInputs/ContactInputs.tsx
+++ src/features/cart/payment/billingAndDelivery/formInputs/ContactInputs.tsx
@@ -1,9 +1,34 @@
+import { PaymentFormData } from "../../../../../types/formTypes";
+import Input from "../../../../../ui/Input";
 
-
-const ContactInputs = () => {
-  return (
-    <div>ContactInputs</div>
-  )
+interface ContactInputs {
+  formData: PaymentFormData;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default ContactInputs
+const ContactInputs = ({ formData, handleChange }: ContactInputs) => {
+  return (
+    <div>
+      <div className="flex flex-col mb-5">
+        <label htmlFor="email" className="text-end">ელფოსტის მისამართი</label>
+        <Input
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="flex flex-col mb-5">
+        <label htmlFor="mobile" className="text-end">ტელეფონი</label>
+        <Input
+          id="mobile"
+          name="mobile"
+          value={formData.mobile}
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ContactInputs;
