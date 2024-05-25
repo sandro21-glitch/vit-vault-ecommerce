a2dc578243d1bca64c1fbc3053c0b9dbfb5ec249
Sandro <vipsandro11@gmail.com>
2024-05-25 20:37:04 +0400

    update NameInputs.tsx

--- src/features/cart/payment/billingAndDelivery/formInputs/NameInputs.tsx
+++ src/features/cart/payment/billingAndDelivery/formInputs/NameInputs.tsx
@@ -8,9 +8,9 @@ interface NameInputTypes {
 
 const NameInputs = ({ formData, handleChange }: NameInputTypes) => {
   return (
-    <div>
-      <div>
-        <label htmlFor="name">სახელი</label>
+    <div className="flex">
+      <div className="flex flex-col mb-5">
+        <label htmlFor="name" className="text-end">სახელი</label>
         <Input
           id="name"
           name="firstName"
@@ -18,8 +18,8 @@ const NameInputs = ({ formData, handleChange }: NameInputTypes) => {
           onChange={handleChange}
         />
       </div>
-      <div>
-        <label htmlFor="surname">გვარი</label>
+      <div className="flex flex-col mb-5">
+        <label htmlFor="surname" className="text-end">გვარი</label>
         <Input
           id="surname"
           name="lastName"
