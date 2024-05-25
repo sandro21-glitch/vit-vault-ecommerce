f3f843c786ff023969de82d5ba85a4ee200d30b0
Sandro <vipsandro11@gmail.com>
2024-05-25 20:55:04 +0400

    update AddressInputs.tsx

--- src/features/cart/payment/billingAndDelivery/formInputs/AddressInputs.tsx
+++ src/features/cart/payment/billingAndDelivery/formInputs/AddressInputs.tsx
@@ -3,24 +3,34 @@ import Input from "../../../../../ui/Input";
 
 type AddressInputTypes = {
   formData: PaymentFormData;
-  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleChange: (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => void;
 };
 
 const AddressInputs = ({ formData, handleChange }: AddressInputTypes) => {
   return (
     <div>
       <div className="flex flex-col mb-5">
-        <label htmlFor="country" className="text-end">ქვეყანა</label>
-        <Input
+        <label htmlFor="country" className="text-end">
+          ქვეყანა
+        </label>
+        <select
           id="country"
           value={formData.country}
           onChange={handleChange}
+          className="w-full border-[3px] outline-none px-2 py-3"
           name="country"
-          className="w-full"
-        />
+        >
+          <option value="საქართველო" defaultChecked>საქართველო</option>
+        </select>
       </div>
       <div className="flex flex-col mb-5">
-        <label htmlFor="street" className="text-end">ქუჩის მისამართი</label>
+        <label htmlFor="street" className="text-end">
+          ქუჩის მისამართი
+        </label>
         <Input
           id="street"
           value={formData.street}
