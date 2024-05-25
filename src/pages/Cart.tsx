600203f949421b7a5a6da6ad7d63475b5d0fcb5b
Sandro <vipsandro11@gmail.com>
2024-05-25 20:26:41 +0400

    update Cart.tsx

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,16 +1,21 @@
-import CartHeader from "../features/cart/CartHeader";
-import CartLayout from "../features/cart/CartLayout";
+import CartHeader from "../features/cart/cartLayout/CartHeader";
+import CartLayout from "../features/cart/cartLayout/CartLayout";
 import ArticleWidgets from "../ui/ArticleWidgets";
 
 const Cart = () => {
   return (
     <section>
       <CartHeader />
+      {/* cart */}
       <div
         style={{ marginBottom: "40px" }}
         className="flex flex-col lg:flex-row section-center section-x md:gap-7"
       >
         <CartLayout />
+        {/* <div className="w-full flex">
+          <div className="w-1/2">1</div>
+          <div className="w-1/2">2</div>
+        </div> */}
         <ArticleWidgets />
       </div>
     </section>
