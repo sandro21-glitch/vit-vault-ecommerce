67b32a536e682216fbd2506f17886f435a2a4c9e
Sandro <vipsandro11@gmail.com>
2024-05-26 18:23:21 +0400

    update Order.tsx

--- src/features/cart/payment/order/Order.tsx
+++ src/features/cart/payment/order/Order.tsx
@@ -1,9 +1,14 @@
-
+import OrderedProducts from "./orderContent/OrderedProducts";
 
 const Order = () => {
   return (
-    <div className="w-1/2">Order</div>
-  )
-}
+    <div className="w-1/2 bg-[#F8F8F8]">
+      <h3 className="text-[22px] text-center font-semibold mb-10 pt-10">
+        თქვენი შეკვეთა
+      </h3>
+      <OrderedProducts />
+    </div>
+  );
+};
 
-export default Order
+export default Order;
