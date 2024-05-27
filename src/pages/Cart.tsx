d755ee08495fe27cc2ab200d465c9e137ca3d116
Sandro <vipsandro11@gmail.com>
2024-05-27 22:53:39 +0400

    update Cart.tsx

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,19 +1,22 @@
+import { useState } from "react";
 import CartHeader from "../features/cart/cartLayout/CartHeader";
 import CartLayout from "../features/cart/cartLayout/CartLayout";
-// import Payment from "../features/cart/payment/Payment";
+import Payment from "../features/cart/payment/Payment";
 import ArticleWidgets from "../ui/ArticleWidgets";
 
 const Cart = () => {
+  const [cartPage, setCartPage] = useState<string>("cart");
+
   return (
     <section>
-      <CartHeader />
+      <CartHeader cartPage={cartPage} />
       {/* cart */}
       <div
         style={{ marginBottom: "40px" }}
         className="flex flex-col lg:flex-row section-center section-x md:gap-7"
       >
-        <CartLayout />
-        {/* <Payment /> */}
+        {cartPage === "cart" && <CartLayout setCartPage={setCartPage} />}
+        {cartPage === "payment" && <Payment />}
         <ArticleWidgets />
       </div>
     </section>
