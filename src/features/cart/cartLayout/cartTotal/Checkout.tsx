7677f418c13f688e8c85c0a911aa8b2633046765
Sandro <vipsandro11@gmail.com>
2024-05-27 20:28:39 +0400

    update Checkout.tsx

--- src/features/cart/cartLayout/cartTotal/Checkout.tsx
+++ src/features/cart/cartLayout/cartTotal/Checkout.tsx
@@ -1,6 +1,29 @@
+import { useAppDispatch, useAppSelector } from "../../../../hooks/hooks";
+import { addOrders } from "../../../slices/orderSlice";
+
 const Checkout = () => {
+  const { cartProducts } = useAppSelector((store) => store.cart);
+  const { orders } = useAppSelector((store) => store.order);
+  const dispatch = useAppDispatch();
+  console.log(orders);
+
+  const orderedProduct = cartProducts.map((product) => ({
+    name: product.name,
+    price: product.price,
+    amount: product.quantity,
+    id: product.id,
+  }));
+
+  const handleAddOrder = () => {
+    dispatch(addOrders(orderedProduct));
+  };
+
   return (
-    <button className="py-3 w-full bg-secondaryGreen text-white font-semibold mt-3 hover:bg-primaryGreen transition-all ease-in duration-150">
+    <button
+      type="button"
+      onClick={handleAddOrder}
+      className="py-3 w-full bg-secondaryGreen text-white font-semibold mt-3 hover:bg-primaryGreen transition-all ease-in duration-150"
+    >
       შეკვეთის გაფორმება
     </button>
   );
