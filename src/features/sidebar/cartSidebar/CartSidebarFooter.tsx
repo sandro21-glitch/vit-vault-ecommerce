d4e061b001e14b8ef15f05631dc8a4800e3b975e
Sandro <vipsandro11@gmail.com>
2024-05-27 23:05:37 +0400

    feat: create CartSidebarFooter.tsx

---
+++ src/features/sidebar/cartSidebar/CartSidebarFooter.tsx
@@ -0,0 +1,18 @@
+const CartSidebarFooter = () => {
+  return (
+    <footer className="absolute bottom-0 border-t-2 px-4 py-10 w-full">
+      <div className="flex justify-between w-full items-center mb-10">
+        <p className="text-[22px] text-black font-semibold">ჯამი:</p>
+        <p className="text-[22px] text-secondaryGreen font-semibold ">22</p>
+      </div>
+      <button
+        className="w-full bg-veryLightGray text-primaryGray
+       hover:text-black py-3 transition-colors ease-in duration-150"
+      >
+        კალათის ნახვა
+      </button>
+    </footer>
+  );
+};
+
+export default CartSidebarFooter;
