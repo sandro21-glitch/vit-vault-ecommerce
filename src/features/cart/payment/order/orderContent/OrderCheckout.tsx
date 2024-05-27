cd32960158c838d3b243c24fde7b9775dfbbe2cb
Sandro <vipsandro11@gmail.com>
2024-05-27 17:53:13 +0400

    feat: create OrderCheckout.tsx

---
+++ src/features/cart/payment/order/orderContent/OrderCheckout.tsx
@@ -0,0 +1,20 @@
+const OrderCheckout = () => {
+  return (
+    <div>
+      <div className="flex items-center gap-1 mb-5">
+        <input type="radio" name="" id="" checked />
+        <label htmlFor="" className="cursor-pointer">
+          ადგილზე გადახდა კურიერთან
+        </label>
+      </div>
+      <button
+        type="submit"
+        className="bg-secondaryGreen w-full py-3 text-white font-bold font-poppins text-[18px] hover:bg-primaryGreen transition-colors ease-in duration-150"
+      >
+        გადახდა
+      </button>
+    </div>
+  );
+};
+
+export default OrderCheckout;
