2dfbc5559158bb767e73d2fb5bd480b5f61d4bfe
Sandro <vipsandro11@gmail.com>
2024-05-27 20:16:10 +0400

    update orderSlice.ts

--- src/features/slices/orderSlice.ts
+++ src/features/slices/orderSlice.ts
@@ -1,11 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface OrderedProducts {
+  name: string;
+  price: number;
+  amount: number;
+}
+
 export interface OrderState {
- 
+  orders: OrderedProducts[];
+  totalSum: number;
 }
 
 const initialState: OrderState = {
-
+  orders: [],
+  totalSum: 0,
 };
 
 export const orderSlice = createSlice({
@@ -14,8 +22,6 @@ export const orderSlice = createSlice({
   reducers: {},
 });
 
-export const {
-
-} = orderSlice.actions;
+export const {} = orderSlice.actions;
 
 export default orderSlice.reducer;
