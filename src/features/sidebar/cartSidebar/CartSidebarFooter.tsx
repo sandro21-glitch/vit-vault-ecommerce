1b266d05d3244d35eaae9407ea37a8f3e3b22494
Sandro <vipsandro11@gmail.com>
2024-05-27 23:08:41 +0400

    update CartSidebarFooter.tsx

--- src/features/sidebar/cartSidebar/CartSidebarFooter.tsx
+++ src/features/sidebar/cartSidebar/CartSidebarFooter.tsx
@@ -1,4 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../../hooks/hooks";
+import { closeSidebar } from "../../slices/modalSlice";
+
 const CartSidebarFooter = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const handleNavigate = () => {
+    navigate("/cart");
+    dispatch(closeSidebar());
+  };
+
   return (
     <footer className="absolute bottom-0 border-t-2 px-4 py-10 w-full">
       <div className="flex justify-between w-full items-center mb-10">
@@ -6,6 +17,7 @@ const CartSidebarFooter = () => {
         <p className="text-[22px] text-secondaryGreen font-semibold ">22</p>
       </div>
       <button
+        onClick={handleNavigate}
         className="w-full bg-veryLightGray text-primaryGray
        hover:text-black py-3 transition-colors ease-in duration-150"
       >
