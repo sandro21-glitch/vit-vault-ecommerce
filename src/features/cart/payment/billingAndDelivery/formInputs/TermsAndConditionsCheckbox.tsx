4e91708946850cf4cddd49e35263f3d3f56b85d8
Sandro <vipsandro11@gmail.com>
2024-05-28 00:12:49 +0400

    update TermsAndConditionsCheckbox.tsx

--- src/features/cart/payment/billingAndDelivery/formInputs/TermsAndConditionsCheckbox.tsx
+++ src/features/cart/payment/billingAndDelivery/formInputs/TermsAndConditionsCheckbox.tsx
@@ -1,13 +1,23 @@
-const TermsAndConditionsCheckbox = () => {
+type TermTypes = {
+  terms: boolean;
+  setTerms: (isChecked: boolean) => void;
+};
+
+const TermsAndConditionsCheckbox = ({ setTerms, terms }: TermTypes) => {
   return (
     <div className="flex items-center justify-center gap-1">
       <input
         type="checkbox"
         name="check"
         id="check"
+        checked={terms}
+        onChange={() => setTerms(!terms)}
         className="cursor-pointer"
       />
-      <label htmlFor="check" className="cursor-pointer text-[18px] text-[#464646]">
+      <label
+        htmlFor="check"
+        className="cursor-pointer text-[18px] text-[#464646]"
+      >
         ვეთანხმები{" "}
         <span className="text-secondaryGreen">წესებს და პირობებს</span>
       </label>
