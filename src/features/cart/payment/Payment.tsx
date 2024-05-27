2d1ac0cf4298032395425d37d795b77c0e3555cb
Sandro <vipsandro11@gmail.com>
2024-05-28 00:12:42 +0400

    update Payment.tsx

--- src/features/cart/payment/Payment.tsx
+++ src/features/cart/payment/Payment.tsx
@@ -6,13 +6,14 @@ import { useAppSelector } from "../../../hooks/hooks";
 
 const Payment = () => {
   const { user } = useAppSelector((state) => state.user);
+  const [terms, setTerms] = useState<boolean>(false);
   const [formData, setFormData] = useState<PaymentFormData>({
     firstName: user?.name ? user.name : "",
     lastName: user?.surname ? user.surname : "",
     country: "საქართველო",
     street: "",
     city: "",
-    email: user?.email ? user.email :  "",
+    email: user?.email ? user.email : "",
     mobile: "",
   });
 
@@ -30,7 +31,12 @@ const Payment = () => {
 
   return (
     <form className="w-full lg:max-w-[75%] flex flex-col lg:flex-row gap-7 mb-10 md:mb-0 ">
-      <BillingAndDelivery handleChange={handleChange} formData={formData} />
+      <BillingAndDelivery
+        handleChange={handleChange}
+        formData={formData}
+        terms={terms}
+        setTerms={setTerms}
+      />
       <Order />
     </form>
   );
