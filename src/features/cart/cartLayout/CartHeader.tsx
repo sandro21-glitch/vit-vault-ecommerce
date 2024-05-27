d8cd33da127a6dd732d8635835b938daa4ff5eb3
Sandro <vipsandro11@gmail.com>
2024-05-27 22:53:29 +0400

    update CartHeader.tsx

--- src/features/cart/cartLayout/CartHeader.tsx
+++ src/features/cart/cartLayout/CartHeader.tsx
@@ -1,18 +1,31 @@
 import { FaLongArrowAltRight } from "react-icons/fa";
-const CartHeader = () => {
+
+type CartHeaderTypes = {
+  cartPage: string;
+};
+
+const CartHeader = ({ cartPage }: CartHeaderTypes) => {
   return (
-    <header className=" bg-secondaryGreen mb-[40px]">
+    <header className=" bg-black mb-[40px]">
       <div className=" section-center section-x">
         <ul className="flex justify-center items-center py-[15px] text-[22px]">
-          <li className="flex items-center cursor-pointer text-white font-semibold">
+          <li
+            className={`flex items-center cursor-pointer ${
+              cartPage === "cart" ? "text-secondaryGreen" : "text-white"
+            } font-semibold`}
+          >
             საყიდლების კალათა
-            <span className="mx-5 cursor-default hidden lg:inline">
+            <span className="mx-5 cursor-default hidden lg:inline text-white">
               <FaLongArrowAltRight />
             </span>
           </li>
-          <li className="items-center cursor-pointer text-white font-semibold hidden lg:flex">
+          <li
+            className={`items-center cursor-pointer ${
+              cartPage === "payment" ? "text-secondaryGreen" : "text-white"
+            } font-semibold hidden lg:flex`}
+          >
             გადახდა
-            <span className="mx-5 cursor-default">
+            <span className="mx-5 cursor-default text-white">
               <FaLongArrowAltRight />
             </span>
           </li>
