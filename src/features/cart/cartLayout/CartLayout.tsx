88e316272f70d985f3d0c1d1110c0f3c1ef71b40
Sandro <vipsandro11@gmail.com>
2024-05-27 22:53:32 +0400

    update CartLayout.tsx

--- src/features/cart/cartLayout/CartLayout.tsx
+++ src/features/cart/cartLayout/CartLayout.tsx
@@ -1,11 +1,15 @@
 import CartTable from "./cartTable/CartTable";
-import CartTotal from "./cartTotal/CartTotal"; 
+import CartTotal from "./cartTotal/CartTotal";
 
-const CartLayout = () => {
+type CartLayoutTypes = {
+  setCartPage: (page: string) => void;
+};
+
+const CartLayout = ({ setCartPage }: CartLayoutTypes) => {
   return (
     <div className="w-full lg:max-w-[75%] flex flex-col lg:flex-row gap-7 mb-10 md:mb-0">
       <CartTable />
-      <CartTotal />
+      <CartTotal setCartPage={setCartPage} />
     </div>
   );
 };
