24cb1a70610ba1a694477594c3a4f778641352f3
Sandro <vipsandro11@gmail.com>
2024-05-28 14:33:34 +0400

    update OrderTable.tsx

--- src/features/cart/payment/order/orderContent/OrderTable.tsx
+++ src/features/cart/payment/order/orderContent/OrderTable.tsx
@@ -1,3 +1,5 @@
+import OrderTableFooter from "./orderTableContent/OrderTableFooter";
+
 const OrderTable = () => {
   return (
     <div className="bg-white p-3">
@@ -13,20 +15,7 @@ const OrderTable = () => {
           <p className="w-full text-end text-inherit">4.00₾ x 1</p>
         </li>
       </ul>
-      <footer>
-        <div className="flex justify-between border-b-2 mb-3 pb-3 cursor-pointer">
-          <p className="text-black font-semibold">ჯამი</p>
-          <p className="text-secondaryGreen font-semibold">7₾</p>
-        </div>
-        <div className="flex justify-between border-b-2 mb-3 pb-3 cursor-pointer">
-          <p className="text-black font-semibold">მიწოდება</p>
-          <p className="text-secondaryGreen font-semibold">4₾</p>
-        </div>
-        <div className="flex justify-between border-b-2 mb-3 pb-3 cursor-pointer">
-          <p className="text-[20px] font-semibold text-black">მთლიანი ჯამი</p>
-          <p className="text-[20px] font-semibold text-secondaryGreen">11₾</p>
-        </div>
-      </footer>
+    <OrderTableFooter />
     </div>
   );
 };
