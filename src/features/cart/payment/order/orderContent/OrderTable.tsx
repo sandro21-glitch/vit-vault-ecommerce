72fe805312c7f2207f32965d1806cade8071913f
Sandro <vipsandro11@gmail.com>
2024-05-28 14:34:12 +0400

    update OrderTable.tsx

--- src/features/cart/payment/order/orderContent/OrderTable.tsx
+++ src/features/cart/payment/order/orderContent/OrderTable.tsx
@@ -1,12 +1,10 @@
 import OrderTableFooter from "./orderTableContent/OrderTableFooter";
+import OrderTableHeader from "./orderTableContent/OrderTableHeader";
 
 const OrderTable = () => {
   return (
     <div className="bg-white p-3">
-      <header className="flex justify-between border-b-2 mb-3 pb-3">
-        <p className="font-semibold text-[20px] text-black">პროდუქტი</p>
-        <p className="font-semibold text-[20px] text-black">ჯამი</p>
-      </header>
+    <OrderTableHeader />
       <ul>
         <li className="flex justify-between border-b-2 mb-3 pb-3 cursor-pointer text-black hover:text-[rgba(51,51,51,.7)] transition-colors ease-in duration-150">
           <p className="w-full max-w-1/2 text-inherit">
