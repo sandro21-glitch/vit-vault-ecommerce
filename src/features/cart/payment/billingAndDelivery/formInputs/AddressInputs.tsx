92928405725dc7e5c7646b9bac395c72613914ed
Sandro <vipsandro11@gmail.com>
2024-05-28 23:11:34 +0400

    update AddressInputs.tsx

--- src/features/cart/payment/billingAndDelivery/formInputs/AddressInputs.tsx
+++ src/features/cart/payment/billingAndDelivery/formInputs/AddressInputs.tsx
@@ -1,3 +1,4 @@
+import { TbCodeAsterix } from "react-icons/tb";
 import { PaymentFormData } from "../../../../../types/formTypes";
 import Input from "../../../../../ui/Input";
 
@@ -13,7 +14,7 @@ type AddressInputTypes = {
 const AddressInputs = ({ formData, handleChange }: AddressInputTypes) => {
   return (
     <div>
-      <div className="flex flex-col mb-5">
+      <div className="flex flex-col mb-5 relative">
         <label
           htmlFor="country"
           className="text-end text-[18px] text-[#464646] mb-1"
@@ -31,8 +32,12 @@ const AddressInputs = ({ formData, handleChange }: AddressInputTypes) => {
             საქართველო
           </option>
         </select>
+        <TbCodeAsterix
+          className="absolute right-[-15px] text-red-600 cursor-help"
+          title="სავალდებულო ველი"
+        />
       </div>
-      <div className="flex flex-col mb-5">
+      <div className="flex flex-col mb-5 relative">
         <label
           htmlFor="street"
           className="text-end text-[18px] text-[#464646] mb-1"
@@ -45,6 +50,10 @@ const AddressInputs = ({ formData, handleChange }: AddressInputTypes) => {
           onChange={handleChange}
           name="street"
           className="w-full"
+        />
+        <TbCodeAsterix
+          className="absolute right-[-15px] text-red-600 cursor-help"
+          title="სავალდებულო ველი"
         />
       </div>
     </div>
