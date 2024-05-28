5d0ed89c0e28beb10196a451e6c4eacbb96df204
Sandro <vipsandro11@gmail.com>
2024-05-28 14:34:15 +0400

    feat: create OrderTableHeader.tsx

---
+++ src/features/cart/payment/order/orderContent/orderTableContent/OrderTableHeader.tsx
@@ -0,0 +1,12 @@
+{}
+
+const OrderTableHeader = () => {
+  return (
+    <header className="flex justify-between border-b-2 mb-3 pb-3">
+    <p className="font-semibold text-[20px] text-black">პროდუქტი</p>
+    <p className="font-semibold text-[20px] text-black">ჯამი</p>
+  </header>
+  )
+}
+
+export default OrderTableHeader
