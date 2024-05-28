6df709cd078624862726d90bf67a831955b43d64
Sandro <vipsandro11@gmail.com>
2024-05-28 15:21:10 +0400

    update Cart.tsx

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CartHeader from "../features/cart/cartLayout/CartHeader";
 import CartLayout from "../features/cart/cartLayout/CartLayout";
 import Payment from "../features/cart/payment/Payment";
 import ArticleWidgets from "../ui/ArticleWidgets";
+import { useAppDispatch } from "../hooks/hooks";
+import { clearOrders } from "../features/slices/orderSlice";
 
 const Cart = () => {
   const [cartPage, setCartPage] = useState<string>("cart");
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    if (cartPage !== "payment") {
+      dispatch(clearOrders());
+    }
+  }, [cartPage, dispatch]);
 
   return (
     <section>
