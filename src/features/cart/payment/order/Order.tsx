c9ebfc3f4d7c352966e89da92eef4ec92ec18d3c
Sandro <vipsandro11@gmail.com>
2024-05-28 16:04:44 +0400

    update Order.tsx

--- src/features/cart/payment/order/Order.tsx
+++ src/features/cart/payment/order/Order.tsx
@@ -3,7 +3,7 @@ import OrderTable from "./orderContent/OrderTable";
 
 const Order = () => {
   return (
-    <div className="w-1/2 bg-[#F8F8F8] p-10">
+    <div className="w-full lg:w-1/2 bg-[#F8F8F8] p-10">
       <h3 className="text-[22px] text-center font-semibold mb-10 ">
         თქვენი შეკვეთა
       </h3>
