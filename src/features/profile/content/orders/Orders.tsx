f4a3913853105a2faad5703f93702e7aed2cdd16
Sandro <vipsandro11@gmail.com>
2024-05-29 18:54:13 +0400

    update Orders.tsx

--- src/features/profile/content/orders/Orders.tsx
+++ src/features/profile/content/orders/Orders.tsx
@@ -1,9 +1,11 @@
 import OrderListHeader from "./OrderListHeader";
+import ShippedOrderList from "./ShippedOrderList";
 
 const Orders = () => {
   return (
     <div className="w-full">
       <OrderListHeader />
+      <ShippedOrderList />
     </div>
   );
 };
