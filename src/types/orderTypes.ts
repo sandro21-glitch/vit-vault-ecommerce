facac7022c4f2348b8faba0b96bc4359351ce3c7
Sandro <vipsandro11@gmail.com>
2024-05-29 18:36:17 +0400

    update orderTypes.ts

--- src/types/orderTypes.ts
+++ src/types/orderTypes.ts
@@ -1,7 +1,15 @@
+import { PaymentFormData } from "./formTypes";
+
 export interface OrderedProducts {
   name: string;
   price: number;
   discount: number;
   amount: number;
   id: string;
+}
+export interface ShippingData {
+  formData: PaymentFormData;
+  newProduct: OrderedProducts[];
+  shippingDate: string;
+  orderId: string;
 }
