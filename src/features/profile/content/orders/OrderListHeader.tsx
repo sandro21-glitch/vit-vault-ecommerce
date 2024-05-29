163d6775392f30d26fc4611024a6c30f31bb0d4b
Sandro <vipsandro11@gmail.com>
2024-05-29 18:54:11 +0400

    update OrderListHeader.tsx

--- src/features/profile/content/orders/OrderListHeader.tsx
+++ src/features/profile/content/orders/OrderListHeader.tsx
@@ -1,6 +1,6 @@
 const OrderListHeader = () => {
   return (
-    <ul className="grid grid-cols-5 border-b py-3 text-center text-[14px] lg:text-[16px]">
+    <ul className="grid grid-cols-5 border-b py-3 text-[14px] lg:text-[16px]">
       <li>შეკვეთა</li>
       <li>თარიღი</li>
       <li>სტატუსი</li>
