d6aea6c961409a2f513a969e08c9a3ccf3382bfc
Sandro <vipsandro11@gmail.com>
2024-05-29 18:59:22 +0400

    update SingleShippedOrder.tsx

--- src/features/profile/content/orders/SingleShippedOrder.tsx
+++ src/features/profile/content/orders/SingleShippedOrder.tsx
@@ -15,13 +15,20 @@ const SingleShippedOrder = ({
   }, 0);
 
   return (
-    <ul className="grid grid-cols-5">
-      <li className="font-semibold cursor-pointer">#{orderId.slice(0, 7)}</li>
-      <li className="text-wrap whitespace-break-spaces max-w-[50%]">
+    <ul className="grid grid-cols-5 font-poppins mb-5 py-5 border-b border-dotted">
+      <li className="font-semibold cursor-pointer ">#{orderId.slice(0, 7)}</li>
+      <li className="text-wrap whitespace-break-spaces max-w-[50%] text-primaryGray font-medium">
         {shippingDate}
       </li>
-      <li>გადახდის მოლოდინში</li>
-      <li>{formatToGeorgianLari(orderSum)}</li>
+      <li className="max-w-[50%] text-wrap text-primaryGray font-medium">
+        გადახდის მოლოდინში
+      </li>
+      <li className="text-primaryGray font-medium">
+        სულ{" "}
+        <span className="font-semibold text-secondaryGreen">
+          {formatToGeorgianLari(orderSum)}
+        </span>
+      </li>
       <li className="flex items-center justify-end">
         <button
           className="bg-secondaryGreen text-white px-4 py-2 font-semibold
