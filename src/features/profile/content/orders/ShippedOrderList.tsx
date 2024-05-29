41a3037fcb94b837e67deb58b3ce7017a7909800
Sandro <vipsandro11@gmail.com>
2024-05-29 18:59:20 +0400

    update ShippedOrderList.tsx

--- src/features/profile/content/orders/ShippedOrderList.tsx
+++ src/features/profile/content/orders/ShippedOrderList.tsx
@@ -4,7 +4,7 @@ import SingleShippedOrder from "./SingleShippedOrder";
 const ShippedOrderList = () => {
   const { shippedOrders } = useAppSelector((store) => store.order);
   return (
-    <div className="mt-5 ">
+    <div className="mt-5">
         {shippedOrders.map((order) => {
             return <SingleShippedOrder key={order.orderId} order={order} />
         })}
