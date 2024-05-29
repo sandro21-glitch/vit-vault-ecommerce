5e0d7b6239ca2f2c715c1332da076fdcdd5be621
Sandro <vipsandro11@gmail.com>
2024-05-29 19:44:38 +0400

    update LoggedInUser.tsx

--- src/features/header/general/LoggedInUser.tsx
+++ src/features/header/general/LoggedInUser.tsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
 
-const LoggedInUser = () => {
+type LoggedUserTypes = {
+  setOpen?: (open: boolean) => void;
+};
+
+const LoggedInUser = ({ setOpen }: LoggedUserTypes) => {
+  const handleOpenAuthSidebar = () => {
+    setOpen && setOpen(false);
+  };
   return (
-    <li>
+    <li onClick={handleOpenAuthSidebar}>
       <Link
         to="/profile"
-        className="text-[#808080] font-bold text-[13px] cursor-pointer"
+        className="text-[#808080] font-bold text-[13px] cursor-pointer p-[14px] md:p-0
+         inline-flex items-center gap-2"
       >
         ჩემი ანგარიში
       </Link>
