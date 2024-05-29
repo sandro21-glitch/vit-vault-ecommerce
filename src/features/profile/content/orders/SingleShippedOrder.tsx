4681e1b7ebd871ca3da18f72f7c130e902a8fca8
Sandro <vipsandro11@gmail.com>
2024-05-29 18:54:18 +0400

    feat: create SingleShippedOrder.tsx

---
+++ src/features/profile/content/orders/SingleShippedOrder.tsx
@@ -0,0 +1,37 @@
+import { ShippingData } from "../../../../types/orderTypes";
+import { formatToGeorgianLari } from "../../../../utils/formatPrice";
+
+type SingleShipingTypes = {
+  order: ShippingData;
+};
+
+const SingleShippedOrder = ({
+  order: { orderId, shippingDate, newProduct },
+}: SingleShipingTypes) => {
+  const orderSum = newProduct.reduce((acc, cur) => {
+    const discountedPrice = cur.price - (cur.price * cur.discount) / 100;
+    const productTotal = cur.amount * discountedPrice;
+    return acc + productTotal;
+  }, 0);
+
+  return (
+    <ul className="grid grid-cols-5">
+      <li className="font-semibold cursor-pointer">#{orderId.slice(0, 7)}</li>
+      <li className="text-wrap whitespace-break-spaces max-w-[50%]">
+        {shippingDate}
+      </li>
+      <li>გადახდის მოლოდინში</li>
+      <li>{formatToGeorgianLari(orderSum)}</li>
+      <li className="flex items-center justify-end">
+        <button
+          className="bg-secondaryGreen text-white px-4 py-2 font-semibold
+         text-[14px] hover:bg-primaryGreen transition-colors ease-in duration-150"
+        >
+          ნახვა
+        </button>
+      </li>
+    </ul>
+  );
+};
+
+export default SingleShippedOrder;
