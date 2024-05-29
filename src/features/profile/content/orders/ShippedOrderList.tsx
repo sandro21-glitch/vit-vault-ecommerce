8d0866154533fe01448d3fd5be513f691f0f1e2f
Sandro <vipsandro11@gmail.com>
2024-05-29 18:54:15 +0400

    feat: create ShippedOrderList.tsx

---
+++ src/features/profile/content/orders/ShippedOrderList.tsx
@@ -0,0 +1,16 @@
+import { useAppSelector } from "../../../../hooks/hooks";
+import SingleShippedOrder from "./SingleShippedOrder";
+
+const ShippedOrderList = () => {
+  const { shippedOrders } = useAppSelector((store) => store.order);
+  return (
+    <div className="mt-5 ">
+        {shippedOrders.map((order) => {
+            return <SingleShippedOrder key={order.orderId} order={order} />
+        })}
+
+    </div>
+  );
+};
+
+export default ShippedOrderList;
