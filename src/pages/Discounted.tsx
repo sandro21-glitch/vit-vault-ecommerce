3290e7fcc86d0a9748ce1a23bfcff7357492ef88
Sandro <vipsandro11@gmail.com>
2024-05-30 16:19:34 +0400

    update Discounted.tsx

--- src/pages/Discounted.tsx
+++ src/pages/Discounted.tsx
@@ -1,16 +1,13 @@
 import Countdown from "../features/productPages/discounted/Countdown";
 import DiscountedList from "../features/productPages/discounted/DiscountedList";
-import SectionHeader from "../ui/SectionHeader";
+import PageLayout from "../ui/PageLayout";
 
 const Discounted = () => {
   return (
-    <section>
-      <SectionHeader path="ფასდაკლებები" />
-      <article className="section-center section-x">
-        <Countdown />
-        <DiscountedList />
-      </article>
-    </section>
+    <PageLayout title="ფასდაკლებები">
+      <Countdown />
+      <DiscountedList />
+    </PageLayout>
   );
 };
 
