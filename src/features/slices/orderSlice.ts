30d0621a3fceabdc8e5a9dfabf3c9acde813be2b
Sandro <vipsandro11@gmail.com>
2024-05-30 13:23:10 +0400

    feat: :sparkles: Add async thunks for pushing and fetching shipped orders from Firebase
    - Added `pushShippedOrdersToFirebase` async thunk to push shipped orders to Firebase based on user ID. - Added `fetchShippedOrdersFromFirebase` async thunk to fetch shipped orders from Firebase based on user ID. - Integrated async thunks with Redux state using `createAsyncThunk` and `extraReducers` in `orderSlice`. - Introduced `Status` enum to track the loading state of async actions. - Updated `OrderState` to include `status` for better state management. - Ensured proper state updates in `extraReducers` for async thunks.

--- src/features/slices/orderSlice.ts
+++ src/features/slices/orderSlice.ts
@@ -1,16 +1,57 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { OrderedProducts, ShippingData } from "../../types/orderTypes";
+import { child, get, push, ref, set } from "firebase/database";
+import { database } from "../../config/firebase";
+
+// Async Thunks
+export const pushShippedOrdersToFirebase = createAsyncThunk(
+  "orders/pushShippedOrders",
+  async (
+    { userId, shippingData }: { userId: string; shippingData: ShippingData },
+    _
+  ) => {
+    const userShippedOrdersRef = ref(database, `users/${userId}/shippedOrders`);
+    const newOrderRef = push(userShippedOrdersRef);
+    await set(newOrderRef, shippingData);
+    return shippingData;
+  }
+);
+
+export const fetchShippedOrdersFromFirebase = createAsyncThunk(
+  "orders/fetchShippedOrders",
+  async (userId: string, _) => {
+    const dbRef = ref(database);
+    const userShippedOrdersRef = child(dbRef, `users/${userId}/shippedOrders`);
+    const snapshot = await get(userShippedOrdersRef);
+    if (snapshot.exists()) {
+      const data = snapshot.val();
+      const shippedOrders = Object.values(data) as ShippingData[];
+      return shippedOrders;
+    } else {
+      return [];
+    }
+  }
+);
+// enum status
+enum Status {
+  Idle = "idle",
+  Loading = "loading",
+  Succeeded = "succeeded",
+  Failed = "failed",
+}
 
 export interface OrderState {
   orders: OrderedProducts[];
   shippedOrders: ShippingData[];
   totalSum: number;
+  status: Status;
 }
 
 const initialState: OrderState = {
   orders: [],
   shippedOrders: [],
   totalSum: 0,
+  status: Status.Idle,
 };
 
 export const orderSlice = createSlice({
@@ -26,6 +67,28 @@ export const orderSlice = createSlice({
     clearOrders: (state) => {
       state.orders = [];
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(
+      pushShippedOrdersToFirebase.fulfilled,
+      (state, action: PayloadAction<ShippingData>) => {
+        state.shippedOrders.push(action.payload);
+      }
+    );
+    builder
+      .addCase(
+        fetchShippedOrdersFromFirebase.fulfilled,
+        (state, action: PayloadAction<ShippingData[]>) => {
+          state.shippedOrders = action.payload;
+          state.status = Status.Succeeded;
+        }
+      )
+      .addCase(fetchShippedOrdersFromFirebase.pending, (state) => {
+        state.status = Status.Loading;
+      })
+      .addCase(fetchShippedOrdersFromFirebase.rejected, (state) => {
+        state.status = Status.Failed;
+      });
   },
 });
 
