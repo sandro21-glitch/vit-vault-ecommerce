26a175682a86bbe0f69f81d0a997c71a119477ee
Sandro <vipsandro11@gmail.com>
2024-05-30 16:19:37 +0400

    feat: create OrderDetails.tsx

---
+++ src/pages/OrderDetails.tsx
@@ -0,0 +1,12 @@
+import OrderDetailsContent from "../features/orderDetails/OrderDetailsContent";
+import PageLayout from "../ui/PageLayout";
+
+const OrderDetails = () => {
+  return (
+    <PageLayout title="შეკვეთის დეტალები">
+      <OrderDetailsContent />
+    </PageLayout>
+  );
+};
+
+export default OrderDetails;
