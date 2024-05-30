2a73dbd81ce590be7d187e090ca0b9d9aca8a020
Sandro <vipsandro11@gmail.com>
2024-05-30 16:26:59 +0400

    update OrderDetailsContent.tsx

--- src/features/orderDetails/OrderDetailsContent.tsx
+++ src/features/orderDetails/OrderDetailsContent.tsx
@@ -1,8 +1,38 @@
-
 const OrderDetailsContent = () => {
   return (
-    <div>OrderDetailsContent</div>
-  )
-}
+    <div className="mb-52 mt-10">
+      <ul>
+        <li className="flex justify-between border-b mb-5 pb-5">
+          <p>პროდუქტი</p>
+          <p>მთლიანი ჯამი</p>
+        </li>
+        <li className="flex justify-between border-b mb-5 pb-5">
+          <p>product</p>
+          <p>3l</p>
+        </li>
+        <li className="flex justify-between border-b mb-5 pb-5">
+          <p>jami</p>
+          <p>3l</p>
+        </li>
+        <li className="flex justify-between border-b mb-5 pb-5">
+          <p>miwodeba</p>
+          <p>fosta</p>
+        </li>
+        <li className="flex justify-between border-b mb-5 pb-5">
+          <p>miwodebis safasuri</p>
+          <p>10l</p>
+        </li>
+        <li className="flex justify-between border-b mb-5 pb-5">
+          <p>gadaxdis metodi</p>
+          <p>UniPay</p>
+        </li>
+        <li className="flex justify-between border-b mb-5 pb-5">
+          <p>mtliani jami</p>
+          <p>11l</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default OrderDetailsContent
+export default OrderDetailsContent;
