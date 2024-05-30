eb6c2cbe835851b17fd5321265e8676ff45d8a06
Sandro <vipsandro11@gmail.com>
2024-05-30 13:27:24 +0400

    update ShippedOrderList.tsx

--- src/features/profile/content/orders/ShippedOrderList.tsx
+++ src/features/profile/content/orders/ShippedOrderList.tsx
@@ -2,18 +2,22 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../../hooks/hooks";
 import SingleShippedOrder from "./SingleShippedOrder";
 import { fetchShippedOrdersFromFirebase } from "../../../slices/orderSlice";
+import SmallSpinner from "../../../../ui/SmallSpinner";
 
 const ShippedOrderList = () => {
   const { shippedOrders, status } = useAppSelector((store) => store.order);
   const { user } = useAppSelector((store) => store.user);
   const dispatch = useAppDispatch();
+
   useEffect(() => {
     if (user?.uid) {
       dispatch(fetchShippedOrdersFromFirebase(user?.uid));
     }
   }, [dispatch, user?.uid]);
-  if (status === "loading") return <p>Loading</p>;
-  if (status === "failed") return <p>Error</p>;
+
+  if (status === "loading") return <SmallSpinner />;
+  if (status === "failed") return <p className="text-center">Error</p>;
+
   return (
     <div className="mt-5">
       {shippedOrders.map((order) => {
