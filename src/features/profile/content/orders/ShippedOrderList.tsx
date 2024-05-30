f2a6c703a173c0d03afb25ed8d8847c5698b516e
Sandro <vipsandro11@gmail.com>
2024-05-30 13:22:42 +0400

    feat: :sparkles: Add fetching of shipped orders from Firebase based on user ID
    - Integrated `useEffect` to dispatch `fetchShippedOrdersFromFirebase` when the user ID is available. - Updated `ShippedOrderList` component to include loading and error states. - Improved code readability and ensured proper state management by adding status checks. - Leveraged `useAppDispatch` and `useAppSelector` for Redux store interactions.

--- src/features/profile/content/orders/ShippedOrderList.tsx
+++ src/features/profile/content/orders/ShippedOrderList.tsx
@@ -1,8 +1,19 @@
-import { useAppSelector } from "../../../../hooks/hooks";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../../hooks/hooks";
 import SingleShippedOrder from "./SingleShippedOrder";
+import { fetchShippedOrdersFromFirebase } from "../../../slices/orderSlice";
 
 const ShippedOrderList = () => {
-  const { shippedOrders } = useAppSelector((store) => store.order);
+  const { shippedOrders, status } = useAppSelector((store) => store.order);
+  const { user } = useAppSelector((store) => store.user);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    if (user?.uid) {
+      dispatch(fetchShippedOrdersFromFirebase(user?.uid));
+    }
+  }, [dispatch, user?.uid]);
+  if (status === "loading") return <p>Loading</p>;
+  if (status === "failed") return <p>Error</p>;
   return (
     <div className="mt-5">
       {shippedOrders.map((order) => {
