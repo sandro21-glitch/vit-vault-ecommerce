34a49310bca5b6a057ffcb52e48fe50c36eb5309
Sandro <vipsandro11@gmail.com>
2024-05-30 14:22:08 +0400

    feat: create PendingPayment.tsx

---
+++ src/features/profile/content/orders/shippedOrderContent/PendingPayment.tsx
@@ -0,0 +1,9 @@
+const PendingPayment = () => {
+  return (
+    <li className="max-w-[50%] text-wrap text-primaryGray font-medium">
+      გადახდის მოლოდინში
+    </li>
+  );
+};
+
+export default PendingPayment;
