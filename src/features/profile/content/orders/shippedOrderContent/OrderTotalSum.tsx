2e8887c8a5cfda4cd483d607bf528aca0b7cde6b
Sandro <vipsandro11@gmail.com>
2024-05-30 14:22:06 +0400

    feat: create OrderTotalSum.tsx

---
+++ src/features/profile/content/orders/shippedOrderContent/OrderTotalSum.tsx
@@ -0,0 +1,18 @@
+import { formatToGeorgianLari } from "../../../../../utils/formatPrice";
+
+type OrderTotalSumProps = {
+  orderSum: number;
+};
+
+const OrderTotalSum = ({ orderSum }: OrderTotalSumProps) => {
+  return (
+    <li className="text-primaryGray font-medium">
+      სულ{" "}
+      <span className="font-semibold text-secondaryGreen">
+        {formatToGeorgianLari(orderSum)}
+      </span>
+    </li>
+  );
+};
+
+export default OrderTotalSum;
