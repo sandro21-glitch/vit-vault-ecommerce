a9c959a4ea3c17c473cb11c99365e546d82f6b37
Sandro <vipsandro11@gmail.com>
2024-05-31 19:51:31 +0400

    update OrderTotalPrice.tsx

--- src/features/orderDetails/OrderTotalPrice.tsx
+++ src/features/orderDetails/OrderTotalPrice.tsx
@@ -1,8 +1,16 @@
-const OrderTotalPrice = () => {
+import { formatToGeorgianLari } from "../../utils/formatPrice";
+
+type OrderTotalPriceTypes = {
+  totalOrderPrice: number;
+};
+
+const OrderTotalPrice = ({ totalOrderPrice }: OrderTotalPriceTypes) => {
   return (
     <div className="flex justify-between border-b mb-5 pb-5">
-      <p>მთლიანი ჯამი</p>
-      <p>11l</p>
+      <p className="text-black font-semibold">მთლიანი ჯამი</p>
+      <p className="text-primaryGray font-semibold">
+        {formatToGeorgianLari(totalOrderPrice + 10)}
+      </p>
     </div>
   );
 };
