b0f8dfd56814a1e74ed8c1cd1ecaf59d5db64104
Sandro <vipsandro11@gmail.com>
2024-05-31 19:27:35 +0400

    feat: create OrderProductList.tsx

---
+++ src/features/orderDetails/OrderProductList.tsx
@@ -0,0 +1,20 @@
+import { ShippingData } from "../../types/orderTypes";
+import SingleOrderProduct from "./SingleOrderProduct";
+
+type OrderProductListProps = {
+  orderInfo: ShippingData | undefined;
+};
+
+const OrderProductList = ({ orderInfo }: OrderProductListProps) => {
+  return (
+    <ul>
+      {orderInfo?.newProduct.map((product) => {
+        return (
+          <SingleOrderProduct key={product.id} product={product} />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default OrderProductList;
