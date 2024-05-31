5f049bfdb2b89a1a95a621d66ac5dc6082de1f85
Sandro <vipsandro11@gmail.com>
2024-05-31 20:02:44 +0400

    update PendingPayment.tsx

--- src/features/profile/content/orders/shippedOrderContent/PendingPayment.tsx
+++ src/features/profile/content/orders/shippedOrderContent/PendingPayment.tsx
@@ -1,6 +1,6 @@
 const PendingPayment = () => {
   return (
-    <li className="max-w-[50%] text-wrap text-primaryGray font-medium">
+    <li className="max-w-[50%] text-wrap text-primaryGray font-medium md:text-[14px] text-[13px]">
       გადახდის მოლოდინში
     </li>
   );
