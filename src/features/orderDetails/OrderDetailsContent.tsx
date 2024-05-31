e3d6719fb8467d5ca3b791dcb9b990b0b5eeaa0d
Sandro <vipsandro11@gmail.com>
2024-05-31 19:27:31 +0400

    update OrderDetailsContent.tsx

--- src/features/orderDetails/OrderDetailsContent.tsx
+++ src/features/orderDetails/OrderDetailsContent.tsx
@@ -1,36 +1,31 @@
+import { useParams } from "react-router-dom";
+import { useAppSelector } from "../../hooks/hooks";
+import OrderProductList from "./OrderProductList";
+import DetailsHeader from "./DetailsHeader";
+import OrderSum from "./OrderSum";
+import OrderDelivery from "./OrderDelivery";
+import OrderTotalPrice from "./OrderTotalPrice";
+import OrderPayment from "./OrderPayment";
+import DeliveryFee from "./DeliveryFee";
+
 const OrderDetailsContent = () => {
+  const { shippedOrders } = useAppSelector((store) => store.order);
+  const { orderId } = useParams();
+
+  if (!orderId) return null;
+  const orderInfo = shippedOrders.find((order) => order.orderId === orderId);
+
   return (
     <div className="mb-52 mt-10">
-      <ul>
-        <li className="flex justify-between border-b mb-5 pb-5">
-          <p>პროდუქტი</p>
-          <p>მთლიანი ჯამი</p>
-        </li>
-        <li className="flex justify-between border-b mb-5 pb-5">
-          <p>product</p>
-          <p>3l</p>
-        </li>
-        <li className="flex justify-between border-b mb-5 pb-5">
-          <p>jami</p>
-          <p>3l</p>
-        </li>
-        <li className="flex justify-between border-b mb-5 pb-5">
-          <p>miwodeba</p>
-          <p>fosta</p>
-        </li>
-        <li className="flex justify-between border-b mb-5 pb-5">
-          <p>miwodebis safasuri</p>
-          <p>10l</p>
-        </li>
-        <li className="flex justify-between border-b mb-5 pb-5">
-          <p>gadaxdis metodi</p>
-          <p>UniPay</p>
-        </li>
-        <li className="flex justify-between border-b mb-5 pb-5">
-          <p>mtliani jami</p>
-          <p>11l</p>
-        </li>
-      </ul>
+      <div>
+        <DetailsHeader />
+        <OrderProductList orderInfo={orderInfo} />
+        <OrderSum />
+        <OrderDelivery />
+        <OrderPayment />
+        <DeliveryFee />
+        <OrderTotalPrice />
+      </div>
     </div>
   );
 };
