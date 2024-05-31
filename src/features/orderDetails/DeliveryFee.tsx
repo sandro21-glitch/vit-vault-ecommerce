803ac4f293a0fdabae5a4ca33934e71f09c2888e
Sandro <vipsandro11@gmail.com>
2024-05-31 19:27:25 +0400

    feat: create DeliveryFee.tsx

---
+++ src/features/orderDetails/DeliveryFee.tsx
@@ -0,0 +1,12 @@
+
+
+const DeliveryFee = () => {
+  return (
+    <div className="flex justify-between border-b mb-5 pb-5">
+    <p>მიწოდების საფასური</p>
+    <p>10ლ</p>
+  </div>
+  )
+}
+
+export default DeliveryFee
