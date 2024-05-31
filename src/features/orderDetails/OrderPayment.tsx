670b32b82c3aa9920dea280d52ed30447db9246a
Sandro <vipsandro11@gmail.com>
2024-05-31 19:51:25 +0400

    update OrderPayment.tsx

--- src/features/orderDetails/OrderPayment.tsx
+++ src/features/orderDetails/OrderPayment.tsx
@@ -1,8 +1,8 @@
 const OrderPayment = () => {
   return (
     <div className="flex justify-between border-b mb-5 pb-5">
-      <p>გადახდის მეთოდი</p>
-      <p>ადგილზე გადახდა</p>
+      <p className="text-black font-semibold">გადახდის მეთოდი</p>
+      <p className="text-primaryGray font-semibold">ადგილზე გადახდა</p>
     </div>
   );
 };
