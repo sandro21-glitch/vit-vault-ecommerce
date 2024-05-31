23978de7e60a9f839cf2f11407cf481b881b2df5
Sandro <vipsandro11@gmail.com>
2024-05-31 19:27:27 +0400

    feat: create DetailsHeader.tsx

---
+++ src/features/orderDetails/DetailsHeader.tsx
@@ -0,0 +1,10 @@
+const DetailsHeader = () => {
+  return (
+    <header className="flex justify-between border-b mb-5 pb-5">
+      <p className="text-black font-semibold">პროდუქტი</p>
+      <p className="text-black font-semibold">მთლიანი ჯამი</p>
+    </header>
+  );
+};
+
+export default DetailsHeader;
