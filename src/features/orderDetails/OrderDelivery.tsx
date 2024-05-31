64fcf700f4f3147db76e4fe343d4cffacbb3bcaf
Sandro <vipsandro11@gmail.com>
2024-05-31 19:27:29 +0400

    feat: create OrderDelivery.tsx

---
+++ src/features/orderDetails/OrderDelivery.tsx
@@ -0,0 +1,11 @@
+
+const OrderDelivery = () => {
+  return (
+    <div className="flex justify-between border-b mb-5 pb-5">
+    <p>მიწოდება</p>
+    <p>ფოსტა</p>
+  </div>
+  )
+}
+
+export default OrderDelivery
