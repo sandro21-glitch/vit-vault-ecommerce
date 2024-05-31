9b280ec1647d0513c7b6c20c9372a2097c0641cf
Sandro <vipsandro11@gmail.com>
2024-05-31 19:27:37 +0400

    feat: create OrderSum.tsx

---
+++ src/features/orderDetails/OrderSum.tsx
@@ -0,0 +1,12 @@
+
+
+const OrderSum = () => {
+  return (
+    <div className="flex justify-between border-b mb-5 pb-5">
+          <p>ჯამი</p>
+          <p>3ლ</p>
+        </div>
+  )
+}
+
+export default OrderSum
