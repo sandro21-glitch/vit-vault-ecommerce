1fb1a959e33bfbb99a737acf6090edbe9da6d1f4
Sandro <vipsandro11@gmail.com>
2024-05-31 20:02:35 +0400

    update OrderDate.tsx

--- src/features/profile/content/orders/shippedOrderContent/OrderDate.tsx
+++ src/features/profile/content/orders/shippedOrderContent/OrderDate.tsx
@@ -4,7 +4,7 @@ type OrderDateProps = {
 
 const OrderDate = ({ shippingDate }: OrderDateProps) => {
   return (
-    <li className="text-wrap whitespace-break-spaces max-w-[50%] text-primaryGray font-medium">
+    <li className="text-wrap whitespace-break-spaces max-w-[50%] text-primaryGray font-medium md:text-[14px] text-[13px]">
       {shippingDate}
     </li>
   );
