51652cd58ca46a71c7a79c308f94ca7a2beea2b2
Sandro <vipsandro11@gmail.com>
2024-05-31 19:27:39 +0400

    feat: create OrderTotalPrice.tsx

---
+++ src/features/orderDetails/OrderTotalPrice.tsx
@@ -0,0 +1,10 @@
+const OrderTotalPrice = () => {
+  return (
+    <div className="flex justify-between border-b mb-5 pb-5">
+      <p>მთლიანი ჯამი</p>
+      <p>11l</p>
+    </div>
+  );
+};
+
+export default OrderTotalPrice;
