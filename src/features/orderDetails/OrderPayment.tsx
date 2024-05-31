a554c444f331b7fe7b32129257de70f99de6e460
Sandro <vipsandro11@gmail.com>
2024-05-31 19:27:33 +0400

    feat: create OrderPayment.tsx

---
+++ src/features/orderDetails/OrderPayment.tsx
@@ -0,0 +1,10 @@
+const OrderPayment = () => {
+  return (
+    <div className="flex justify-between border-b mb-5 pb-5">
+      <p>გადახდის მეთოდი</p>
+      <p>ადგილზე გადახდა</p>
+    </div>
+  );
+};
+
+export default OrderPayment;
