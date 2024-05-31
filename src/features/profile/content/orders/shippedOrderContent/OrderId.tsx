73c1c980668d04b97f9e76a1ae3629204ebaf0de
Sandro <vipsandro11@gmail.com>
2024-05-31 20:02:40 +0400

    update OrderId.tsx

--- src/features/profile/content/orders/shippedOrderContent/OrderId.tsx
+++ src/features/profile/content/orders/shippedOrderContent/OrderId.tsx
@@ -4,7 +4,7 @@ type OrderIdProps = {
 
 const OrderId = ({ orderId }: OrderIdProps) => {
   return (
-    <li className="font-semibold cursor-pointer ">#{orderId.slice(0, 7)}</li>
+    <li className="font-semibold cursor-pointer md:text-[14px] text-[13px]">#{orderId.slice(0, 7)}</li>
   );
 };
 
