11f8a1d614974c95eb7c22e0e012d61da88b2f76
Sandro <vipsandro11@gmail.com>
2024-05-31 19:51:27 +0400

    update OrderSum.tsx

--- src/features/orderDetails/OrderSum.tsx
+++ src/features/orderDetails/OrderSum.tsx
@@ -1,12 +1,18 @@
+import { formatToGeorgianLari } from "../../utils/formatPrice";
 
+type OrderSumTypes = {
+  totalOrderPrice: number;
+};
 
-const OrderSum = () => {
+const OrderSum = ({ totalOrderPrice }: OrderSumTypes) => {
   return (
     <div className="flex justify-between border-b mb-5 pb-5">
-          <p>ჯამი</p>
-          <p>3ლ</p>
-        </div>
-  )
-}
+      <p className="text-black font-semibold">ჯამი</p>
+      <p className="text-primaryGray font-semibold">
+        {formatToGeorgianLari(totalOrderPrice)}
+      </p>
+    </div>
+  );
+};
 
-export default OrderSum
+export default OrderSum;
