41bcd35cb06b5c8eeacc8f03dc96bb3df9ea92d7
Sandro <vipsandro11@gmail.com>
2024-05-31 19:51:35 +0400

    update SingleOrderProduct.tsx

--- src/features/orderDetails/SingleOrderProduct.tsx
+++ src/features/orderDetails/SingleOrderProduct.tsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
 import { OrderedProducts } from "../../types/orderTypes";
 import CheckDiscount from "../../ui/CheckDiscount";
+import { formatPath } from "../../utils/formatPath";
 
 type SingleOrderTypes = {
   product: OrderedProducts;
@@ -9,16 +11,21 @@ const SingleOrderProduct = ({
   product: { id, name, amount, discount, price },
 }: SingleOrderTypes) => {
   return (
-    <li
-      key={id}
-      className="flex justify-between border-b mb-5 pb-5 cursor-pointer"
-    >
-      <div className="flex items-center gap-2">
-        <p>{name}</p>
-        <span>✖</span>
-        <p className="font-semibold text-black">{amount}</p>
-      </div>
-      <CheckDiscount discount={discount} price={price} onlyDiscounted />
+    <li key={id}>
+      <Link
+        to={`/product/${formatPath(name)}`}
+        state={{ id }}
+        className="flex justify-between border-b mb-5 pb-5 cursor-pointer"
+      >
+        <div className="flex items-center gap-2">
+          <p className="hover:text-black transition-colors ease-in duration-150">
+            {name}
+          </p>
+          <span>✖</span>
+          <p className="font-semibold text-black">{amount}</p>
+        </div>
+        <CheckDiscount discount={discount} price={price} onlyDiscounted />
+      </Link>
     </li>
   );
 };
