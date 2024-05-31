0ed8867b0c32e2ae9349b361f220f99f4749bae4
Sandro <vipsandro11@gmail.com>
2024-05-31 19:51:17 +0400

    update DeliveryFee.tsx

--- src/features/orderDetails/DeliveryFee.tsx
+++ src/features/orderDetails/DeliveryFee.tsx
@@ -3,8 +3,8 @@
 const DeliveryFee = () => {
   return (
     <div className="flex justify-between border-b mb-5 pb-5">
-    <p>მიწოდების საფასური</p>
-    <p>10ლ</p>
+    <p className="text-black font-semibold">მიწოდების საფასური</p>
+    <p className="text-primaryGray font-semibold">10ლ</p>
   </div>
   )
 }
