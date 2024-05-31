bb4be7fec8f0877f126f178ccdafe3b81830c0a7
Sandro <vipsandro11@gmail.com>
2024-05-31 20:02:28 +0400

    update OrderListHeader.tsx

--- src/features/profile/content/orders/OrderListHeader.tsx
+++ src/features/profile/content/orders/OrderListHeader.tsx
@@ -1,10 +1,10 @@
 const OrderListHeader = () => {
   return (
-    <ul className="grid grid-cols-5 border-b py-3 text-[14px] lg:text-[16px]">
+    <ul className="grid grid-cols-4 md:grid-cols-5 border-b py-3 text-[14px] lg:text-[16px]">
       <li>შეკვეთა</li>
       <li>თარიღი</li>
       <li>სტატუსი</li>
-      <li>მთლიანი ჯამი</li>
+      <li className="hidden md:block">მთლიანი ჯამი</li>
       <li className="text-right">ქმედებები</li>
     </ul>
   );
