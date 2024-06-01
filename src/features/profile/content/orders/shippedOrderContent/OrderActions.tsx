25466dd912ec3eefdac3c517fdcb6415ce718a53
Sandro <vipsandro11@gmail.com>
2024-06-01 17:44:54 +0400

    update OrderActions.tsx

--- src/features/profile/content/orders/shippedOrderContent/OrderActions.tsx
+++ src/features/profile/content/orders/shippedOrderContent/OrderActions.tsx
@@ -1,18 +1,12 @@
 import { Link } from "react-router-dom";
-import CloseButton from "../../../../../ui/CloseButton";
 
 type OrderTypes = {
   orderId: string;
 };
 
 const OrderActions = ({ orderId }: OrderTypes) => {
-  const handleRemoveOrder = () => {
-    console.log(orderId);
-  };
-
   return (
     <li className="flex items-center justify-end">
-      <CloseButton onClick={handleRemoveOrder} />
       <Link
         to={`/order/${orderId}`}
         className="bg-secondaryGreen text-white px-2 md:px-4 py-1 md:py-2 font-semibold
