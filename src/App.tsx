a2d2e098781f4a61efa1c0c5d5bfe06f9cc0194f
Sandro <vipsandro11@gmail.com>
2024-06-01 18:25:53 +0400

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  HashRouter as BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "./hooks/hooks";
 import {
