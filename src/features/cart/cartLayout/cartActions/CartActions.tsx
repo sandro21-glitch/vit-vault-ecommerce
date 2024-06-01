54857fdf9baffdb5833b72af2cebe94d451f23f3
Sandro <vipsandro11@gmail.com>
2024-06-01 17:56:00 +0400

    update CartActions.tsx

--- src/features/cart/cartLayout/cartActions/CartActions.tsx
+++ src/features/cart/cartLayout/cartActions/CartActions.tsx
@@ -1,7 +1,10 @@
+import { useAppSelector } from "../../../../hooks/hooks";
 import ClearCart from "./ClearCart";
 import CouponCode from "./CouponCode";
 
 const CartActions = () => {
+  const { cartProducts } = useAppSelector((store) => store.cart);
+  if (cartProducts.length < 1) return null;
   return (
     <div className="flex flex-col items-start lg:items-end">
       <CouponCode />
