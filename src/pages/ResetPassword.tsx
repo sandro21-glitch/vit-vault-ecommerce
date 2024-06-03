341e9d4565b579770c87269a1975ce770493321e
Sandro <vipsandro11@gmail.com>
2024-06-03 19:06:20 +0400

    feat: create ResetPassword.tsx

---
+++ src/pages/ResetPassword.tsx
@@ -0,0 +1,12 @@
+import PasswordRecoveryForm from "../features/User/PasswordRecoveryForm";
+import PageLayout from "../ui/PageLayout";
+
+const ResetPassword = () => {
+  return (
+    <PageLayout title="ჩემი ანგარიში">
+      <PasswordRecoveryForm />
+    </PageLayout>
+  );
+};
+
+export default ResetPassword;
