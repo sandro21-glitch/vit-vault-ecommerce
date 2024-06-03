c817dad21e5fa3bfee1acc0778774ce4683690bd
Sandro <vipsandro11@gmail.com>
2024-06-03 19:45:47 +0400

    refactor: Update ForgotPasswordLink to use React Router Link

--- src/features/sidebar/loginSidebar/loginFormContent/ForgotPasswordLink.tsx
+++ src/features/sidebar/loginSidebar/loginFormContent/ForgotPasswordLink.tsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
+import { useAppDispatch } from "../../../../hooks/hooks";
+import { closeLoginSidebar } from "../../../slices/modalSlice";
+
 const ForgotPasswordLink = () => {
+  const dispatch = useAppDispatch();
+
+  const handleCloseSidebar = () => {
+    dispatch(closeLoginSidebar());
+  };
+
   return (
-    <p className="text-secondaryGreen mt-5 hover:underline cursor-pointer text-[20px]">
+    <Link
+      onClick={handleCloseSidebar}
+      to="/password-recovery"
+      className="text-secondaryGreen mt-5 hover:underline cursor-pointer text-[20px] inline-block"
+    >
       დაგავიწყდათ პაროლი?
-    </p>
+    </Link>
   );
 };
 
