8e1d65864e4b692515d76599a257eb7132987935
Sandro <vipsandro11@gmail.com>
2024-06-04 19:31:52 +0400

    update ContactInputs.tsx

--- src/features/cart/payment/billingAndDelivery/formInputs/ContactInputs.tsx
+++ src/features/cart/payment/billingAndDelivery/formInputs/ContactInputs.tsx
@@ -11,27 +11,38 @@ const ContactInputs = ({ formData, handleChange }: ContactInputs) => {
   return (
     <div>
       <div className="flex flex-col mb-5 relative">
-        <label htmlFor="email" className="text-end text-[18px] text-[#464646] mb-1">ელფოსტის მისამართი</label>
+        <label
+          htmlFor="email"
+          className="text-end text-[18px] text-[#464646] mb-1"
+        >
+          ელფოსტის მისამართი
+        </label>
         <Input
           id="email"
           name="email"
+          type="email"
           value={formData.email}
           onChange={handleChange}
         />
-         <TbCodeAsterix
+        <TbCodeAsterix
           className="absolute right-[-15px] text-red-600 cursor-help"
           title="სავალდებულო ველი"
         />
       </div>
       <div className="flex flex-col mb-5 relative ">
-        <label htmlFor="mobile" className="text-end text-[18px] text-[#464646] mb-1">ტელეფონი</label>
+        <label
+          htmlFor="mobile"
+          className="text-end text-[18px] text-[#464646] mb-1"
+        >
+          ტელეფონი
+        </label>
         <Input
           id="mobile"
           name="mobile"
           value={formData.mobile}
           onChange={handleChange}
         />
-         <TbCodeAsterix
+        <TbCodeAsterix
           className="absolute right-[-15px] text-red-600 cursor-help"
           title="სავალდებულო ველი"
         />
