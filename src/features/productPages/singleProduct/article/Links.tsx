4bb9554d440a646e35e1c3883fe1f9e037f53885
Sandro <vipsandro11@gmail.com>
2024-06-10 23:43:57 +0400

    update Links.tsx

--- src/features/productPages/singleProduct/article/Links.tsx
+++ src/features/productPages/singleProduct/article/Links.tsx
@@ -4,10 +4,16 @@ const Links = () => {
   return (
     <div className="flex gap-1">
       <a href="http://facebook.com" target="_blank" rel="noopener noreferrer">
-        <FaFacebookF size={15} className="text-primaryGray hover:text-black transition-colors ease-in duration-150" />
+        <FaFacebookF
+          size={15}
+          className="text-primaryGray hover:text-black transition-colors ease-in duration-150"
+        />
       </a>
       <a href="http://linkedin.com" target="_blank" rel="noopener noreferrer">
-        <FaLinkedinIn size={15} className="text-primaryGray hover:text-black transition-colors ease-in duration-150" />
+        <FaLinkedinIn
+          size={15}
+          className="text-primaryGray hover:text-black transition-colors ease-in duration-150"
+        />
       </a>
     </div>
   );
