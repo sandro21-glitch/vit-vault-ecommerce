2ea1d24a364338711e74239238a928a818c61476
Sandro <vipsandro11@gmail.com>
2024-06-10 23:43:47 +0400

    update DetailsHeader.tsx

--- src/features/orderDetails/DetailsHeader.tsx
+++ src/features/orderDetails/DetailsHeader.tsx
@@ -1,9 +1,9 @@
 const DetailsHeader = () => {
   return (
-    <header className="flex justify-between border-b mb-5 pb-5">
+    <div className="flex justify-between border-b mb-5 pb-5">
       <p className="text-black font-semibold">პროდუქტი</p>
       <p className="text-black font-semibold">მთლიანი ჯამი</p>
-    </header>
+    </div>
   );
 };
 
