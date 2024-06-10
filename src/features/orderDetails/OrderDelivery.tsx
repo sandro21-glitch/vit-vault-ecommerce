1019cb5127a31efe56be3c2be4523355e5f68269
Sandro <vipsandro11@gmail.com>
2024-06-10 23:43:49 +0400

    update OrderDelivery.tsx

--- src/features/orderDetails/OrderDelivery.tsx
+++ src/features/orderDetails/OrderDelivery.tsx
@@ -1,11 +1,10 @@
-
 const OrderDelivery = () => {
   return (
     <div className="flex justify-between border-b mb-5 pb-5">
-    <p className="text-black font-semibold">მიწოდება</p>
-    <p className="text-primaryGray font-semibold">ფოსტა</p>
-  </div>
-  )
-}
+      <p className="text-black font-semibold">მიწოდება</p>
+      <p className="text-primaryGray font-semibold">ფოსტა</p>
+    </div>
+  );
+};
 
-export default OrderDelivery
+export default OrderDelivery;
